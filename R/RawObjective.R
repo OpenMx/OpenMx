992d8bdc73bd2c1f3f10eb8d4703fc6521824352
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2008-05-07 20:24:27 +0000

    A non-working version of SimpleRaw demo is in the trunk of the repository.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@74 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ R/RawObjective.R
@@ -0,0 +1,57 @@
+setConstructorS3("RawObjective", function(expectedMean, expectedCov, observed) {
+
+   if (missing(expectedMean)) expectedMean <- NA;
+   if (missing(expectedCov)) expectedCov <- NA;
+   if (missing(observed)) observed <- NA;
+
+   extend(MxObjective(), "RawObjective",
+	.expectedMean=expectedMean,
+	.expectedCov=expectedCov,
+	.observed=observed);
+
+})
+
+setMethodS3("createMxJobClosureR", "RawObjective", function(objective, job, ...) {
+
+   model <- job$.model;
+   model$updateFreeVariablesList(); 
+
+   function() {
+
+      startValues <- model$getFreeVariables();
+      expectedMean <- objective$.expectedMean; # this is an MxAlgebra statement
+      expectedCov <- objective$.expectedCov;   # this is an MxAlgebra statement
+      observed <- objective$.observed;         # this is a matrix
+      startValues <- model$getFreeVariables();
+      expectedMean$translateAlgebra();
+      expectedCov$translateAlgebra();
+
+
+      missdmnorm <- function(x, mu, sigma) {
+         tsel <- !is.na(x);
+         if (length(x[tsel]) == 0) return(NA);
+         return(-2 * dmnorm(x[tsel], mu[tsel], sigma[tsel,tsel], log=TRUE));
+      }
+
+      objectiveFunction <- function(par) {
+         model$updateMatrices(par);
+         expectedMean$setDirtyBit(TRUE);
+         expectedCov$setDirtyBit(TRUE);
+         predictedMean <- expectedMean$evalTranslation();
+         predictedCov <- expectedCov$evalTranslation();
+         functionValue <- sum(apply(observed, 1, missdmnorm, mu=predictedMean,
+		sigma=predictedCov), na.rm=TRUE);
+         return(functionValue);
+      }
+
+      # Optimize
+      outNLM <- nlm(objectiveFunction, 
+   		startValues, 
+   		hessian=TRUE, 
+   		iterlim=1000, 
+   		print.level=2,
+   		typsize=abs(startValues) / 10);
+
+   }
+
+})
