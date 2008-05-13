7a5cca7a2628bfaee822a651931d018e5aebbb68
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2008-05-13 13:19:51 +0000

    A more illustrative example of the possible ways to call setParameters() and setValues().
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@88 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- demo/SimpleCovariance.R
+++ demo/SimpleCovariance.R
@@ -6,15 +6,18 @@ model$F <- FullMatrix(2, 2)
 model$I <- IdenMatrix(2, 2)
 model$cov <- MxAlgebra(model$F %&% (solve(model$I - model$A) %&% model$S))
 
+# Three equivalent ways to invoke the same method
 setValues(model$A,c(0,0,0.2,0))
-setValues(model$S,c(0.8,0,0,0.8))
-setValues(model$F,c(1,0,0,1))
+model$S$values <- c(0.8,0,0,0.8)
+model$F$setValues(c(1,0,0,1))
 
-setParameters(model$A, c(0,0,1,0))
+# It is possible to set elements using a vector, or
+# a matrix, or element-by-element.
+model$A$parameters <- c(0,0,1,0)
 model$S$parameters[1,2] <- model$S$parameters[2,1] <- "0";
 model$S$parameters[1,1] <- "banana";
 model$S$parameters[2,2] <- "apple";
-setParameters(model$F, matrix(0,2,2))
+model$F$parameters <- matrix(0,2,2)
 
 covMatrix <- matrix(c(0.77642931, 0.39590663, 0.39590663, 0.49115615),
 	nrow = 2, ncol = 2, byrow = TRUE)
