0cf2c0d6b52baeb2bc78c448f27cffb6567ee76a
skenny <skenny@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2008-08-06 06:47:48 +0000

    incorporates new S matrices into model objects
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@107 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- swift/mxModelGen.R
+++ swift/mxModelGen.R
@@ -2,88 +2,62 @@
 # and writes them out as mxmodel objects
 
 library(OpenMx)
-dyn.load("/home/ac/skenny/perm.so")
+dyn.load("permall.so")
 
 ## should be like R_SWIFT_ARGS=3 3
-
-## get number of connections and array size (will be converted to nxn)
-
-allinputs <- Sys.getenv("R_SWIFT_ARGS")
-
-print(allinputs)
-
+allinputs   <- Sys.getenv("R_SWIFT_ARGS")
 connections <- noquote(strsplit(allinputs," ")[[1]][1])
-
-print(connections)
-
-numcol        <- noquote(strsplit(allinputs," ")[[1]][2])
-
-print(numcol)
-
+numcol      <- noquote(strsplit(allinputs," ")[[1]][2])
+# need to set default here .75 if not given by user
+#initweight    <- noquote(strsplit(allinputs," ")[[1]][3])
+initweight <- 0.75
 rowcol = as.integer(numcol)
 size = rowcol*rowcol
-
-
-# creates the .adat files 
-
-.C("run_perm", connections=as.integer(connections), size=as.integer(size))
-
-# read files and generate associated ap.adat, s.dat and sp.dat files
-# will do this in the c code
+.C("run_perm", connections=as.integer(connections), size=as.integer(size), ncol=as.integer(rowcol))
 
 # read in the data files to generate models and write as objects
-
-# dot is wildcard here, will need to adjust to it gets all 3 dat's
-count = 0
+count = 1
 for (i in list.files(pattern=".adat")){
+   for(j in list.files(pattern=".sdat"))
+	{
 	model = MxModel()
-#	model$A <- FullMatrix(rowcol, rowcol, free=TRUE)
-#	model$S <- DiagMatrix(rowcol, rowcol) 
+	model$A <- FullMatrix(rowcol, rowcol, free=TRUE)
+	model$S <- DiagMatrix(rowcol, rowcol, free=TRUE) 
 	model$F <- IdenMatrix(rowcol, rowcol) # modifiable in future, for now no filter
 	model$I <- IdenMatrix(rowcol, rowcol)
 	model$cov <- MxAlgebra(model$F %&% (solve(model$I - model$A) %&% model$S))
 
-# hard-coding for now
-a = double(length = size)
-apar = double(length = size)
-xindex = array(c(1:rowcol))
-yindex = array(c(1:rowcol))
-	apar = matrix(c(read.table(i)), nrow = rowcol, ncol = rowcol)
-	cnt = rowcol+1.0
-for(x in xindex)
-{
- for(y in yindex)
-   {
-	if (apar[x,y] == 1){
-        apar[x,y] = cnt
-	cnt = cnt + 1}
+	xindex = array(c(1:rowcol))
+	yindex = array(c(1:rowcol))
 
-   }
+
+	abits = as.matrix(read.table(i))
+	sbits = as.matrix(read.table(j))
+
+	parcnt = 1.0
+	scnt = rowcol+1.0
+
+
+	for(x in xindex)
+	{
+	 for(y in yindex)
+	 {
+			if (abits[x,y] == 1){
+	        	model$A$parameters[x,y] = parcnt
+			model$A$values[x,y] = initweight
+			parcnt = parcnt + 1
+			}
+		else if (sbits[x,y] == 1){
+			model$S$parameters[x,y] = parcnt
+			model$S$values[x,y] = initweight
+			parcnt = parcnt + 1
+			}
+
 }
-print(apar)
-aval = matrix(c(read.table(i)), nrow = rowcol, ncol = rowcol)
-
-for(x in xindex)
-{
- for(y in yindex)
-   {
-	if (aval[x,y] == 1)
-        aval[x,y] = 0.75
-
-   }
 }
-print(aval)
-
-	
-	s = matrix(c(read.table("matrices/s.dat")), nrow = rowcol, ncol = rowcol)
-	spar = matrix(c(read.table("matrices/sp.dat")), nrow = rowcol, ncol = rowcol)
-	
-	model$A$values <- aval
-	model$A$parameters <- apar
-	model$S$values <- s
-	model$S$parameters <- spar
-	count = count+1
-print(model$S)
 	save.Object(model,file=paste(count,"_model.rdata", sep=""))
+	count = count + 1
+
+
 }
-system("tar -cf models.tar *rdata")
+}
