a1ef4db642b62ebc19a5615d91e44ca5ade184ed
skenny <skenny@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2008-08-12 22:59:02 +0000

    setting params for A and S
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@111 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- swift/mxModelGen.R
+++ swift/mxModelGen.R
@@ -13,13 +13,17 @@ numcol      <- noquote(strsplit(allinputs," ")[[1]][2])
 initweight <- 0.75
 rowcol = as.integer(numcol)
 size = rowcol*rowcol
-.C("run_perm", connections=as.integer(connections), size=as.integer(size), ncol=as.integer(rowcol))
+retval = .C("run_perm", connections=as.integer(connections), size=as.integer(size), ncol=as.integer(rowcol), total=as.integer(0))
+
+total_perms = retval$total
 
 # read in the data files to generate models and write as objects
-count = 1
-for (i in list.files(pattern=".adat")){
-   for(j in list.files(pattern=".sdat"))
-	{
+count = 0
+fnums = array(c(0:total_perms))
+for(i in fnums)
+{
+print("i is :")
+print(i)
 	model = MxModel()
 	model$A <- FullMatrix(rowcol, rowcol, free=TRUE)
 	model$S <- DiagMatrix(rowcol, rowcol, free=TRUE) 
@@ -30,34 +34,28 @@ for (i in list.files(pattern=".adat")){
 	xindex = array(c(1:rowcol))
 	yindex = array(c(1:rowcol))
 
-
-	abits = as.matrix(read.table(i))
-	sbits = as.matrix(read.table(j))
+	abits = as.matrix(read.table(paste(i,".adat",sep="")))
+	sbits = as.matrix(read.table(paste(i,".sdat",sep="")))
 
 	parcnt = 1.0
 	scnt = rowcol+1.0
-
 
 	for(x in xindex)
 	{
 	 for(y in yindex)
 	 {
-			if (abits[x,y] == 1){
-	        	model$A$parameters[x,y] = parcnt
-			model$A$values[x,y] = initweight
-			parcnt = parcnt + 1
-			}
-		else if (sbits[x,y] == 1){
-			model$S$parameters[x,y] = parcnt
+			if (sbits[x,y] == 1){
+	        	model$S$parameters[x,y] = parcnt
 			model$S$values[x,y] = initweight
 			parcnt = parcnt + 1
 			}
-
-}
-}
+		else if (abits[x,y] == 1){
+			model$A$parameters[x,y] = parcnt
+			model$A$values[x,y] = initweight
+			parcnt = parcnt + 1
+			}
+		}
+	}
 	save.Object(model,file=paste(count,"_model.rdata", sep=""))
 	count = count + 1
-
-
-}
 }
