6177df40f36888e855271eb9009f569d22248a75
skenny <skenny@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2008-08-14 18:07:11 +0000

    calculates total permutations to map outputs
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@113 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- swift/modelmapper.py
+++ swift/modelmapper.py
@@ -2,15 +2,35 @@
 
 import sys
 
-total = 0
+def fact(x): return reduce(lambda x, y: x*y, xrange(2, x+1))
+
+conn = 1
+size = 1
+total = 1
 argstr = sys.argv
+
+argfile = open("mapper.args", "w")
+
+
 i = 0
 for a in argstr:
-    if a == '-mods':
-        total = int(argstr[i+1])
+    argfile.write(a+" ")    
+    if a == '-conn':
+        conn = int(argstr[i+1])
+    elif a == '-size':
+        size = int(argstr[i+1])
     i += 1
+
+if (conn <2):
+    total = size
+
+else:
+    total = fact(size)/(fact(conn)*fact(size-conn))    
+
+
 for perm in range(0, total):
-    print("["+str(perm)+"]"+" "+str(perm)+"_model.rdata")
+    print("["+str(perm)+"]"+" "+str(conn)+"_"+str(perm)+"_model.rdata")            
+
 
     
 
