f15cf7a57e9a370e332557cf0245f42c8ea6d09c
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2008-10-08 18:21:18 +0000

    Some sort of working path model representation.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@135 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ R/MxPath.R
@@ -0,0 +1,56 @@
+MxPath <- function(from, to, all = FALSE, free = NULL, 
+	arrows = NULL, startVal = NULL, 
+	endVal = NULL, algebra = NULL,
+	name = NULL, label = NULL,
+	boundMax = NULL, boundMin = NULL,
+	ciUpper = NULL, ciLower = NULL) {
+	if(length(from) == 1 && length(to) == 1) {
+		return(MxCreatePath(from, to, free, 
+			arrows, startVal, endVal, 
+			algebra, name, label, boundMax, 
+			boundMin, ciUpper, ciLower))
+	} else {
+		from <- as.list(from)
+		to <- as.list(to)
+		if (all) {
+			from <- rep(from, each=length(to))	
+		}
+		result <- mapply(MxCreatePath, from, to,
+			free, arrows, startVal, endVal,
+				algebra, name, label, boundMax,
+				boundMin, ciUpper, ciLower, SIMPLIFY=FALSE)
+		return(result)			
+	}
+}
+
+MxCreatePath <- function(from, to, free = NULL, 
+	arrows = NULL, startVal = NULL, 
+	endVal = NULL, algebra = NULL,
+	name = NULL, label = NULL,
+	boundMax = NULL, boundMin = NULL,
+	ciUpper = NULL, ciLower = NULL) {
+	result <- list()
+	if (!is.null(arrows) && (arrows == 2) && (from > to)) {
+		temp <- from
+		from <- to
+		to <- temp	
+	}
+	result[['from']] <- from
+	result[['to']] <- to
+	result[['free']] <- free
+	result[['arrows']] <- arrows
+	result[['startVal']] <- startVal
+	result[['endVal']] <- endVal
+	result[['algebra']] <- algebra
+	result[['name']] <- name
+	result[['label']] <- label		
+	result[['boundMax']] <- boundMax
+	result[['boundMin']] <- boundMin	
+	result[['ciUpper']] <- ciUpper
+	result[['ciLower']] <- ciLower
+	return(result)
+}
+
+isMxPath <- function(value) {
+	return(is.list(value) && !is.null(value[['from']]) && !is.null(value[['to']]))
+}
---
+++ R/MxPathModel.R
@@ -0,0 +1,51 @@
+setClass(Class = "MxPathModel",
+	representation = representation(
+		paths = "data.frame",
+		latentVars = "list",
+		manifestVars = "list",
+		matrices = "list"))
+
+setMethod("initialize", "MxPathModel",
+	function(.Object, paths = list(), latentVars = list(),
+		manifestVars = list(), matrices = list()) {
+#		.Object <- mxAddPath(.Object, paths)
+		.Object@latentVars <- latentVars
+		.Object@manifestVars <- manifestVars
+		.Object@matrices <- matrices
+		return(.Object)
+	}
+)
+
+setGeneric("mxAddPath", function(.Object, paths) { return(standardGeneric("mxAddPath")) } )
+
+setMethod("mxAddPath", "MxPathModel", 
+	function(.Object, paths = list()) {
+		if (isMxPath(paths)) {
+			paths <- list(paths)
+		}
+		for(i in 1:length(paths)) {
+			.Object <- mxAddSinglePath(.Object, paths[[i]])
+		}
+	}
+)
+
+mxAddSinglePath <- function(.Object, path) {
+	if (nrow(.Object@paths) > 0) {
+		fromExists <- (.Object@paths['from'] == path[['from']])
+		toExists <- (.Object@paths['to'] == path[['to']])
+		replace <- fromExists && toExists
+		if (replace) {
+			ids <- names(path)			
+			for(i in 1:length(path)) {
+				id <- ids[[i]]
+				.Object@paths[fromExists & toExists,id] <- path[[id]]
+			}
+		} else {
+			.Object@paths <- merge(.Object@paths, data.frame(path), all=TRUE)
+		}
+	} else {
+		.Object@paths <- data.frame(path)
+	}
+	return(.Object)
+}
+
