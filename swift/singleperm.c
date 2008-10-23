b47e61e8de5a842c5996f68d14930b9a3470d2cb
skenny <skenny@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2008-10-23 19:31:54 +0000

    writes out data for only 1 model based on perm number
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@148 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- swift/singleperm.c
+++ swift/singleperm.c
@@ -14,12 +14,21 @@ with 'connections' conn
 #include <string.h>
 #include <math.h>
 
-static int count = 1;
+static int PERMCOUNT = 0;
 
 /*  Free memory used for data  */
 
 void FreeData(void) {
   return;
+}
+
+
+int factorial( int n )
+{
+  if ( n <= 1 )
+    return 1;
+  else
+    return  n * factorial( n-1 );
 }
 
 int find_n(int n, int idata[], int size)
@@ -90,27 +99,11 @@ void insert_zero(unsigned pos, int size, int connections, int idata[], int recen
     }
 }
 
-int get_total(int size, int connections)
-{
-  int top = (size-connections)+1;
-  int statop = top;
-  int total=0;
-  int j;
-
-  for(j=1;j<=statop;j++)
-    {
-      total += top*j;
-      top--;
-    }
-  printf("total is %i\n", total);
-  return total;
-}
-
 int find_last(int size, int idata[])
   {
     int j;
     int last;
-  int count = 0;
+  int ct = 0;
   for(j=0; j<size; j++)
     {
        if (idata[j] == 1)
@@ -125,48 +118,23 @@ void Perm(int size, int connections, int idata[], int recentdata[], int rowcol, 
   int k = 0;
   int symm = 0;
   int asymm = 0;
-  int total = get_total(size, connections);
   int mxdat_a[rowcol][rowcol];
   int mxdat_s[rowcol][rowcol];
+  int batch_counter = 0;
+  /*  int total = factorial(size)/(factorial(connections)*factorial(size-connections));*/
 
-  /* get initial matrix */
-  if(permnum == 0)
-    {
-   FILE *sdatfile, *adatfile;
-   char sname [10], aname [10];
-   const char sext[] = ".sdat";
-   const char aext[] = ".adat";
-   sprintf(sname, "%d%s", permnum,sext);
-   sprintf(aname, "%d%s", permnum,aext);
-   sdatfile = fopen(sname, "wt");
-   adatfile = fopen(aname, "wt");
-   int x,y;
-   for (x=0; x<rowcol; x++)
-     {
-       for(y=0; y<rowcol; y++)
-	 {
-	   fprintf(sdatfile, "%i ",mxdat_s[x][y]);
-	   fprintf(adatfile, "%i ",mxdat_a[x][y]);
-	 }
-       fputc('\n',sdatfile);
-       fputc('\n',adatfile);
-     }
-   fputc('\n', sdatfile);
-   fputc('\n', adatfile);
-   fclose(sdatfile);
-   fclose(adatfile);
-   count++;
-    }
+  printf(".....BEGINNING PERMS FOR %i connections.....\n",connections);
+  /*  printf("total is %i:\n",total);*/
 
    /* begin permutations */
 
   int last_shift_pos = size - connections;
 
-  while(find_n(1,idata,size) != last_shift_pos)
+  while(find_n(1,idata,size) < last_shift_pos)
     {
       int symm = 0;
       int asymm = 0;
-      insert_zero(find_last(size,idata), size, connections, idata, recentdata);
+
 
       /* load into grid */
       /* check for symmetry */
@@ -191,72 +159,159 @@ void Perm(int size, int connections, int idata[], int recentdata[], int rowcol, 
 		}
 	    }
 	}
-      if(permnum == count)
-	{
-	  FILE *sdatfile, *adatfile;
-	  char sname [10], aname [10];
-	  const char sext[] = ".sdat";
-	  const char aext[] = ".adat";
-	  sprintf(sname, "%d%s", count,sext);
-	  sprintf(aname, "%d%s", count,aext);
-	  sdatfile = fopen(sname, "wt");
-	  adatfile = fopen(aname, "wt");
-	  int x,y;
-	  for (x=0; x<rowcol; x++)
-	    {
-	      for(y=0; y<rowcol; y++)
-		{
-		  fprintf(sdatfile, "%i ",mxdat_s[x][y]);
-		  fprintf(adatfile, "%i ",mxdat_a[x][y]);
-		}
-	      fputc('\n',sdatfile);
-	      fputc('\n',adatfile);
-	    }
-	  fputc('\n', sdatfile);
-	  fputc('\n', adatfile);
-	  fclose(sdatfile);
-	  fclose(adatfile);
-	}
-   	  count++;
-	  k=0;
-    }
-}   
 
-int run_perm(int *con, int *sz, int *nc, int *total, int *pnum) {
+      if(permnum == PERMCOUNT)
+	    {
+	      printf("permnum %i PERMCOUNT %i\n",permnum,PERMCOUNT);
+	      FILE *sdatfile, *adatfile;
+	      char sname [10], aname [10];
+	      const char sext[] = ".sdat";
+	      const char aext[] = ".adat";
+	      sprintf(sname, "%d%s", permnum,sext);
+	      sprintf(aname, "%d%s", permnum,aext);
+	      sdatfile = fopen(sname, "wt");
+	      adatfile = fopen(aname, "wt");
+	      int x,y;
+	      for (x=0; x<rowcol; x++)
+		{
+		  for(y=0; y<rowcol; y++)
+		    {
+		      fprintf(sdatfile, "%i ",mxdat_s[x][y]);
+		      fprintf(adatfile, "%i ",mxdat_a[x][y]);
+		    }
+		  fputc('\n',sdatfile);
+		  fputc('\n',adatfile);
+		}
+	      fputc('\n', sdatfile);
+	      fputc('\n', adatfile);
+	      fclose(sdatfile);
+	      fclose(adatfile);
+	    }
+  else{
+      insert_zero(find_last(size,idata), size, connections, idata, recentdata);
+  }
+      PERMCOUNT++;
+      k=0;
+    }
+  if(find_n(1,idata,size) == last_shift_pos)
+    {
+
+      int symm = 0;
+      int asymm = 0;
+
+
+      /* load into grid */
+      /* check for symmetry */
+      while(k<size)
+	{
+	  for(m=0; m<rowcol; m++)
+	    {
+
+	      for(n=0; n<rowcol; n++)
+		{  
+		
+		  if((m == n) && (idata[k] == 1))
+		    {
+		      mxdat_s[m][n] = idata[k];
+		      mxdat_a[m][n] = 0;
+		    }
+		  else{
+		    mxdat_a[m][n] = idata[k];
+		    mxdat_s[m][n] = 0;
+		  }
+		    k++;
+		}
+	    }
+	}
+      if(permnum == PERMCOUNT)
+	{
+	      printf("permnum %i PERMCOUNT %i\n",permnum,PERMCOUNT);
+	      FILE *sdatfile, *adatfile;
+	      char sname [10], aname [10];
+	      const char sext[] = ".sdat";
+	      const char aext[] = ".adat";
+	      sprintf(sname, "%d%s", permnum,sext);
+	      sprintf(aname, "%d%s", permnum,aext);
+	      sdatfile = fopen(sname, "wt");
+	      adatfile = fopen(aname, "wt");
+	      int x,y;
+	      for (x=0; x<rowcol; x++)
+		{
+		  for(y=0; y<rowcol; y++)
+		    {
+		      fprintf(sdatfile, "%i ",mxdat_s[x][y]);
+		      fprintf(adatfile, "%i ",mxdat_a[x][y]);
+		    }
+		  fputc('\n',sdatfile);
+		  fputc('\n',adatfile);
+		}
+	      fputc('\n', sdatfile);
+	      fputc('\n', adatfile);
+	      fclose(sdatfile);
+	      fclose(adatfile);
+	}
+      PERMCOUNT++;
+    }
+}
+
+int run_perm(int *sz, int *nc, int *total, int *pnum) {
+
+  int conn,i,j;
   int numcol = nc[0];
-  int connections = con[0];
   int size = sz[0];
   int permnum = pnum[0];
   int idata[size];
   int recentdata[size];
 
-  int i,j;
-  for(i=0; i<size; i++)
+  for(conn=1;conn<=size;conn++)
     {
-      if(i<connections)
-	idata[i] = 1;
-      else
-	idata[i] = 0;
-    }
-  for(j=0; j<size; j++)
-    {
-      recentdata[j] = idata[j];
-    }
+      /* init matrices*/
 
-  printf("connections: %i\n",connections);
-  printf("size: %i\n",size);
- 
-  printf("Permutations of %u items:\n\n", connections);
-  Perm(size, connections, idata, recentdata, numcol, permnum);
-
-  printf("\n%d permutations in all.\n", count);
-  total[0] = count;
-  FreeData();
-  return 1;
+      for(i=0; i<size; i++)
+	{
+	  if(i<conn)
+	    idata[i] = 1;
+	  else
+	    idata[i] = 0;
+	}
+      for(j=0; j<size; j++)
+	{
+	  recentdata[j] = idata[j];
+	}
+      Perm(size, conn, idata, recentdata, numcol, permnum);
     }
+      printf("count is %i\n",PERMCOUNT);
+      return 0;
+}
 
 int main(int argc, char *argv[])
 {
-  printf("in main\n");
-  return 0;
+  int size, pnum, conn, ncol, i, j;
+  printf("enter size, permnum and number of columns:\n");
+  scanf("%i %i %i", &size, &pnum, &ncol);
+
+  int numcol = ncol;
+  int idata[size];
+  int recentdata[size];
+
+  for(conn=1;conn<=size;conn++)
+    {
+      /* init matrices*/
+
+      for(i=0; i<size; i++)
+	{
+	  if(i<conn)
+	    idata[i] = 1;
+	  else
+	    idata[i] = 0;
+	}
+      for(j=0; j<size; j++)
+	{
+	  recentdata[j] = idata[j];
+	}
+      Perm(size, conn, idata, recentdata, ncol, pnum);
+      printf("count is %i\n",PERMCOUNT);
+
+    }
+      return 0;
 }
