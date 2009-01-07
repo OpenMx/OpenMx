7be3b67926c7b0b7abdcc9bcd49afa052113d5bc
skenny <skenny@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-01-07 03:05:33 +0000

    does not generate models with negatve deg of freedom or with 2-way symmetric connections
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@188 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- swift/singleperm.c
+++ swift/singleperm.c
@@ -1,10 +1,12 @@
 /*
-permall.c 
+singleperm.c 
 
-creates all symmetric and 
-asymmetric permutations 
-of the matrix size 'size'
-with 'connections' conn
+generates S matrices for residuals and A matrices for 
+one-way symmetric connections for all possible models 
+of the given size with the given number of connections. 
+this can be called on it's own or by an R script.
+
+skenny@uchicago.edu 2009
 
 
 */
@@ -15,6 +17,10 @@ with 'connections' conn
 #include <math.h>
 
 static int PERMCOUNT = 0;
+static int ASSYMCOUNT = 0;
+static int STATCOUNT = 0;
+static int data[4] = {0,0,0,0};
+
 
 /*  Free memory used for data  */
 
@@ -58,9 +64,26 @@ int find_shift_elmt(int connections, int idata[], int size)
   return 0;
 }
 
+int get_stats(int connections, int idata[], int size, int recentdata[])
+{
+  int r = 0;
+  int ch = 0;
+  printf("[%3i] ", STATCOUNT);
+  while(r<size)
+    {
+      printf("%2i", idata[r]);
+      r++;
+    }
+  printf("\n");
+  STATCOUNT++;
+}
+
+
+
 /*  Insert data 0 at position 'pos'  */
 
 void insert_zero(unsigned pos, int size, int connections, int idata[], int recentdata[]) {
+
   int selmt_pos;
   if(idata[pos+1] != 0 || pos+1>=size){
     selmt_pos = find_shift_elmt(connections, idata, size);
@@ -114,30 +137,25 @@ int find_last(int size, int idata[])
 
 void Perm(int size, int connections, int idata[], int recentdata[], int rowcol, int permnum) {
 
-  int i,j,c,m,n,p,q,scount;
+  int i,j,c,m,n,p,q,r,s,scount;
   int k = 0;
-  int symm = 0;
+
   int asymm = 0;
   int mxdat_a[rowcol][rowcol];
   int mxdat_s[rowcol][rowcol];
-  int batch_counter = 0;
+  int zeros = size - connections;
+  printf("getting total, size is %i, connections is %i, zeros is %i\n", size, connections, zeros);
   /*  int total = factorial(size)/(factorial(connections)*factorial(size-connections));*/
-
-  printf(".....BEGINNING PERMS FOR %i connections.....\n",connections);
+  int last_shift_pos = size - connections;
   /*  printf("total is %i:\n",total);*/
 
    /* begin permutations */
 
-  int last_shift_pos = size - connections;
-
-  while(find_n(1,idata,size) < last_shift_pos)
+  while(find_n(1,idata,size) <= last_shift_pos)
     {
-      int symm = 0;
-      int asymm = 0;
-
-
+      /*      get_stats(connections, idata, size, recentdata);*/
       /* load into grid */
-      /* check for symmetry */
+
       while(k<size)
 	{
 	  for(m=0; m<rowcol; m++)
@@ -145,7 +163,7 @@ void Perm(int size, int connections, int idata[], int recentdata[], int rowcol, 
 
 	      for(n=0; n<rowcol; n++)
 		{  
-		
+		  /* check for diag symmetry */		
 		  if((m == n) && (idata[k] == 1))
 		    {
 		      mxdat_s[m][n] = idata[k];
@@ -158,9 +176,21 @@ void Perm(int size, int connections, int idata[], int recentdata[], int rowcol, 
 		    k++;
 		}
 	    }
-	}
 
-      if(permnum == PERMCOUNT)
+	}
+      /* check for remaining symmetry */
+      int symm = 0;
+      for(r=0; r<rowcol; r++)
+	{
+	  for(s=0; s<rowcol; s++)
+	    {
+	      if(mxdat_a[r][s] == 1 && mxdat_a[s][r] == 1)
+		symm = 1;
+	    }
+	}
+      if(!symm)
+	{
+	  if(permnum == ASSYMCOUNT)
 	    {
 	      printf("permnum %i PERMCOUNT %i\n",permnum,PERMCOUNT);
 	      FILE *sdatfile, *adatfile;
@@ -187,131 +217,75 @@ void Perm(int size, int connections, int idata[], int recentdata[], int rowcol, 
 	      fclose(sdatfile);
 	      fclose(adatfile);
 	    }
-  else{
-      insert_zero(find_last(size,idata), size, connections, idata, recentdata);
-  }
-      PERMCOUNT++;
+	  ASSYMCOUNT++;
+
+	}
       k=0;
-    }
-  if(find_n(1,idata,size) == last_shift_pos)
-    {
-
-      int symm = 0;
-      int asymm = 0;
-
-
-      /* load into grid */
-      /* check for symmetry */
-      while(k<size)
-	{
-	  for(m=0; m<rowcol; m++)
-	    {
-
-	      for(n=0; n<rowcol; n++)
-		{  
-		
-		  if((m == n) && (idata[k] == 1))
-		    {
-		      mxdat_s[m][n] = idata[k];
-		      mxdat_a[m][n] = 0;
-		    }
-		  else{
-		    mxdat_a[m][n] = idata[k];
-		    mxdat_s[m][n] = 0;
-		  }
-		    k++;
-		}
-	    }
-	}
-      if(permnum == PERMCOUNT)
-	{
-	      printf("permnum %i PERMCOUNT %i\n",permnum,PERMCOUNT);
-	      FILE *sdatfile, *adatfile;
-	      char sname [10], aname [10];
-	      const char sext[] = ".sdat";
-	      const char aext[] = ".adat";
-	      sprintf(sname, "%d%s", permnum,sext);
-	      sprintf(aname, "%d%s", permnum,aext);
-	      sdatfile = fopen(sname, "wt");
-	      adatfile = fopen(aname, "wt");
-	      int x,y;
-	      for (x=0; x<rowcol; x++)
-		{
-		  for(y=0; y<rowcol; y++)
-		    {
-		      fprintf(sdatfile, "%i ",mxdat_s[x][y]);
-		      fprintf(adatfile, "%i ",mxdat_a[x][y]);
-		    }
-		  fputc('\n',sdatfile);
-		  fputc('\n',adatfile);
-		}
-	      fputc('\n', sdatfile);
-	      fputc('\n', adatfile);
-	      fclose(sdatfile);
-	      fclose(adatfile);
-	}
       PERMCOUNT++;
+      if(find_n(1,idata,size) == last_shift_pos)
+	{
+	  printf("ASSYMCOUNT %i\n", ASSYMCOUNT);
+	  printf("PERMCOUNT %i\n", PERMCOUNT);
+	  break;
+	}
+	  insert_zero(find_last(size,idata), size, connections, idata, recentdata);
     }
+
 }
 
-int run_perm(int *sz, int *nc, int *total, int *pnum) {
+int run_perm(int *sz, int *nc, int *total, int *pnum, int *connections) {
 
-  int conn,i,j;
+  int i,j;
   int numcol = nc[0];
   int size = sz[0];
   int permnum = pnum[0];
+  int conn = connections[0];
   int idata[size];
   int recentdata[size];
+  /*  int max_con = floor(numcol*(numcol+1)/2);*/
 
-  for(conn=1;conn<=size;conn++)
+  for(i=0; i<size; i++)
     {
-      /* init matrices*/
-
-      for(i=0; i<size; i++)
-	{
-	  if(i<conn)
-	    idata[i] = 1;
-	  else
-	    idata[i] = 0;
-	}
-      for(j=0; j<size; j++)
-	{
-	  recentdata[j] = idata[j];
-	}
-      Perm(size, conn, idata, recentdata, numcol, permnum);
+      if(i<conn)
+	idata[i] = 1;
+      else
+	idata[i] = 0;
     }
-      printf("count is %i\n",PERMCOUNT);
-      return 0;
+  for(j=0; j<size; j++)
+    {
+      recentdata[j] = idata[j];
+    }
+  Perm(size, conn, idata, recentdata, numcol, permnum);
+  return 0;
+
+
 }
 
 int main(int argc, char *argv[])
 {
-  int size, pnum, conn, ncol, i, j;
-  printf("enter size, permnum and number of columns:\n");
-  scanf("%i %i %i", &size, &pnum, &ncol);
 
+  int pnum, ncol, conn;
+  printf("enter permnum, number of columns and conn:\n");
+  scanf("%i %i %i", &pnum, &ncol, &conn);
+  int size = ncol*ncol;
   int numcol = ncol;
   int idata[size];
   int recentdata[size];
+  /*  int max_con = floor(numcol*(numcol+1)/2);*/
 
-  for(conn=1;conn<=size;conn++)
+  int i,j;
+
+  for(i=0; i<size; i++)
     {
-      /* init matrices*/
-
-      for(i=0; i<size; i++)
-	{
-	  if(i<conn)
-	    idata[i] = 1;
-	  else
-	    idata[i] = 0;
-	}
-      for(j=0; j<size; j++)
-	{
-	  recentdata[j] = idata[j];
-	}
-      Perm(size, conn, idata, recentdata, ncol, pnum);
-      printf("count is %i\n",PERMCOUNT);
-
+      if(i<conn)
+	idata[i] = 1;
+      else
+	idata[i] = 0;
     }
-      return 0;
+  for(j=0; j<size; j++)
+    {
+      recentdata[j] = idata[j];
+    }
+  Perm(size, conn, idata, recentdata, ncol, pnum);
+  return 0;
 }
