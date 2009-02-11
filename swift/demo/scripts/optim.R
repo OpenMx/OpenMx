a93a6c6e73b2ea4b0d29c157b10086ca05fe85d0
skenny <skenny@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-02-11 17:50:44 +0000

    dir where swift can find R processing scripts
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@208 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ swift/demo/scripts/optim.R
@@ -0,0 +1,11 @@
+allinputs   <- Sys.getenv("R_SWIFT_ARGS")
+model_obj <- noquote(strsplit(allinputs," ")[[1]][1])
+
+library(OpenMx)
+library(R.oo)
+model = mxModel()
+model = load(model_obj)
+model = saveLoadReference
+model = mxJobRun(model)
+print(model_obj)
+save.Object(model,file=paste("results/",model_obj,sep=""))
