e772817a5888e3b2b8bfd5f3252088c1b0f5b4bf
sboker <sboker@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-02-18 13:48:25 +0000

    Better comments for MxPathDemo.R
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@219 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- demo/MxPathDemo.R
+++ demo/MxPathDemo.R
@@ -1,11 +1,19 @@
+# ----------------------------------
+# Read data from disk.
 
-#myData <- read.csv("myFile")
-#myManifest <- names(myData)
+# myData <- read.csv("myFile")
+# myManifest <- names(myData)
+
+# ----------------------------------
+# Define 100 indicators and five first level latent variables.
 
 myManifest <- sprintf("%02d", c(1:100))
 myLatent <- c("G1", "G2", "G3", "G4", "G5")
 
 ex2Model <- mxModel()
+
+# ----------------------------------
+# Attach five latent variables to the 100 indicators in simple factor structure.
 
 for (i in 1:5) {
     j <- i*20
@@ -17,6 +25,9 @@ for (i in 1:5) {
                             startVal=c(1,rep(0.75,19))))
 }
 
+# ----------------------------------
+# Allow the factors to freely covary.
+
 ex2Model <- mxModel(ex2Model, 
                  mxPath(from=myLatent,
                  		all=TRUE,
@@ -24,13 +35,27 @@ ex2Model <- mxModel(ex2Model,
                         free=TRUE, 
                         startVal=1))
                         
-ex3Model <- mxModel(ex2Model,
-           mxPath(from=myLatent, all=TRUE, free=FALSE, startVal=0), # Remove free covariances between latents
-           mxPath(from=myLatent, free=TRUE, startVal=1),
+# ----------------------------------
+# Convert the model to Open Mx (this step is needed?) and print it.
+
+ex2Model <- omxConvertPathModel(ex2Model)
+
+print(ex2Model)
+
+# ----------------------------------
+# Create a second model from the first model.
+# Remove the free factor covariances and create a second order common factor.
+
+ex3Model <- mxModel(ex2Model, latentVars = "G6",
+           mxPath(from=myLatent, all=TRUE, free=FALSE, startVal=0), # Remove free covs
+           mxPath(from=myLatent, free=TRUE, startVal=1), # Add back in free vars
            mxPath(from="G6", to=myLatent, 
-                  all=TRUE, free=TRUE, startVal=.75, arrows=1),
-           mxPath(from="G6", free=FALSE, startVal=1, arrows=2),
-           latentVars = "G6")
+                  all=TRUE, free=TRUE, startVal=.75, arrows=1), # Add 2nd order factor
+           mxPath(from="G6", free=FALSE, startVal=1, arrows=2) # Add free var
+           )
+
+# ----------------------------------
+# Convert the model to Open Mx (this step is needed?) and print it.
 
 ex3Model <- omxConvertPathModel(ex3Model)
 
