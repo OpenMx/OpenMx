5affb33d86ebccfe42281bda9c41b45d5b733d40
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-02-27 03:45:57 +0000

    Added LGCDemo to testsuite directory.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@262 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ inst/testsuite/LGCDemo.R
@@ -0,0 +1,82 @@
+#Latent Growth Curve - Matrix Specification
+#Needs mean structure components
+#Returning Error on mxJob: details below (R. Estabrook, 09Feb26)
+
+# Data
+wisc.cov<-matrix(c(33.73019, 25.46499, 30.88961, 40.51781,
+	25.46499, 37.28895, 33.82058, 47.40553,
+	30.88961, 33.82058, 53.57812, 62.25334,
+	40.51781, 47.40553, 62.25334, 113.74121),
+	nrow=4, ncol=4, byrow=TRUE)
+
+# Define the model
+model <- mxModel()
+model <- mxModel(model, mxMatrix("Full", name = "A", nrow = 6, ncol = 6))
+model <- mxModel(model, mxMatrix("Symm", name = "S", nrow = 6, ncol = 6))
+model <- mxModel(model, mxMatrix("Full", name = "F", nrow = 4, ncol = 6))
+
+# Specify "A" Matrix
+model[["A"]]@specification[6,2] <- "Basis Loading V2" #Latent Basis Slope Loading (Free)
+model[["A"]]@specification[6,3] <- "Basis Loading V4" #Latent Basis Slope Loading (Free)
+
+# Values for "A" Matrix
+model[["A"]]@values[5,1] <- 1 #Intercept Loading
+model[["A"]]@values[5,2] <- 1 #Intercept Loading
+model[["A"]]@values[5,3] <- 1 #Intercept Loading
+model[["A"]]@values[5,4] <- 1 #Intercept Loading
+model[["A"]]@values[6,4] <- 1 #Latent Basis Slope Loading (Fixed)
+
+model[["A"]]@values[6,2] <- .2 #Latent Basis Slope Loading (Free)
+model[["A"]]@values[6,3] <- .6 #Latent Basis Slope Loading (Free)
+
+# Specify "S" Matrix
+model[["S"]]@specification[1,1] <- "Manifest Residual"
+model[["S"]]@specification[2,2] <- "Manifest Residual"
+model[["S"]]@specification[3,3] <- "Manifest Residual"
+model[["S"]]@specification[4,4] <- "Manifest Residual"
+model[["S"]]@specification[5,5] <- "Latent Intercept Variance"
+model[["S"]]@specification[6,6] <- "Latent Slope Variance"
+model[["S"]]@specification[5,6] <- "Latent Covariance"
+
+# Values for "S" Matrix
+model[["S"]]@values[1,1] <- 11
+model[["S"]]@values[2,2] <- 11
+model[["S"]]@values[3,3] <- 11
+model[["S"]]@values[4,4] <- 11
+model[["S"]]@values[5,5] <- 20
+model[["S"]]@values[6,6] <- 24
+model[["S"]]@values[5,6] <- 14
+
+# Specify "F" Matrix
+
+# Values for "F" Matrix
+model[["F"]]@values[1,1] <- 1
+model[["F"]]@values[2,2] <- 1
+model[["F"]]@values[3,3] <- 1
+model[["F"]]@values[4,4] <- 1
+
+# Define the objective function
+objective <- mxRAMObjective("objective")
+
+# Define the observed covariance matrix
+covMatrix <- wisc.cov
+
+# Add the objective function and the data to the model
+model <- mxModel(model, objective, covMatrix)
+
+# Run the job
+# Currently returns "Error in if (uni) "U" else "N" : missing value where TRUE/FALSE needed"
+model <- mxJobRun(model)
+print(model@output)
+
+# Expected Results
+	# Manifest Residual			11.005 (0.771)
+	# Latent Intercept Variance	19.713 (0.394)
+	# Latent Slope Variance		24.140 (0.575)
+	# Latent Covariance			14.004 (3.050)
+	# Basis Coefficient: V2		0.234 (0.012)
+	# Basis Coefficient: V4		0.527 (0.011)
+# Expected Fit Statistics (when mean structure is included)
+	# Log Likelihood	-2491.070
+	# AIC				4998.139
+	# RMSEA				.116
