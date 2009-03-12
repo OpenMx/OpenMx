00ae11a6ff7477bbcf920724ad4a257577100d70
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-03-12 18:30:56 +0000

    Backing out the noStart argument to sfInit(), it was making some warning messages during snowfall initialization.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@276 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,5 @@
 #TODO: replace with .onLoad once namespaces are implemented
 .First.lib <- function(libname, pkgname) {
    library.dynam("OpenMx") 
-   sfInit(parallel = FALSE, nostart=TRUE)
+   sfInit(parallel = FALSE)
 }
