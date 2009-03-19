91462947b2b0b4370c5b3be112c9f5bee29f6576
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-03-19 15:15:27 +0000

    Forgot syntax for comments in old-Mx.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@286 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ models/failing/mxmanual61-b.R
@@ -0,0 +1,10 @@
+library(OpenMx)
+W <- mxMatrix("Symm", nrow = 3, ncol = 3, name = "W",
+	values = c(1.0, 0.4, 0.3, 0.9, 0.5, 1.1))
+H <- mxMatrix("Symm", nrow = 3, ncol = 3, name = "H",
+	values = c(1.2, 0.42, 0.3, 1.0, 0.47, 0.9))
+M <- mxMatrix("Full", nrow = 3, ncol = 3, name = "M",
+	values = c(0.4, 0.05, 0.22, 0.1, 0.3, 0.11, 0.2, 0.12, 0.5))
+D <- mxAlgebra(solve(W) %*% M %*% solve(H), name = "D")
+model <- mxModel(W, H, M, D)
+model <- mxJobRun(model)
