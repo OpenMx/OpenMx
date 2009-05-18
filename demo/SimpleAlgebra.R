cef73520c47f5b55c790f9d0ff4d0bf19f5a13e5
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-05-18 18:58:35 +0000

    Fixed the SimpleAlgebra demo to include a single free parameter.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@389 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- demo/SimpleAlgebra.R
+++ demo/SimpleAlgebra.R
@@ -2,50 +2,20 @@ library(OpenMx)
 
 O <- mxMatrix("Full", c(3), nrow=1, ncol=1, name="O")
 A <- mxMatrix("Full", c(1, 2), nrow=1, ncol=2, name="A")
-B <- mxMatrix("Full", c(3, 4), ncol=1, nrow=2, name="B")
-AlgA <- mxAlgebra(O, "AlgA")
-AlgAB <- mxAlgebra(A %*% B, "AlgAB")
-AlgdoubleMult <- mxAlgebra(A %*% B %*% O, "AlgdoubleMult")
+B <- mxMatrix("Full", c(3, 4), ncol=1, nrow=2, free = c(TRUE, FALSE), name="B")
+algebra <- mxAlgebra(A %*% B - O, "algebra")
+algSquared <- mxAlgebra(algebra %*% algebra, "algebraSquared")
 
 model <- mxModel()
-model <- mxModel(model, A)
-model <- mxModel(model, O)
+model <- mxModel(model, A, B, O)
+model <- mxModel(model, algebra, algSquared)
+model <- mxModel(model, mxAlgebraObjective("algebraSquared"))
 
-# Test 1: Algebra is just a matrix.
+# Test 1: Algebra is a multiply and subtract
 
-model <- mxModel(model, AlgA)
-model <- mxModel(model, mxAlgebraObjective("AlgA"))
-model <- mxRun(model)
+modelOut <- mxRun(model)
 
-outputA <- model[["AlgA"]]@result
-valA <- O[1,1]
-diffA <- (valA - outputA[1,1]) / valA
-diffA
-
-# Test 2: Algebra is a multiply.
-
-model <- mxModel(model, B)
-model <- mxModel(model, AlgAB)
-model <- mxModel(model, mxAlgebraObjective("AlgAB"))
-model <- mxRun(model)
-
-outputAB <- model[["AlgAB"]]@result
-AB <- (A@values %*% B@values)[1,1]
-diffAB <- (AB - outputAB[1,1]) / AB
-diffAB
-
-# Test 2: Algebra is two multiplies
-
-model <- mxModel(model, AlgdoubleMult)
-model <- mxModel(model, mxAlgebraObjective("AlgdoubleMult"))
-model <- mxRun(model)
-
-outputdoubleMult <- model[["AlgdoubleMult"]]@result
-doubleMult <- ((A@values%*%B@values) %*% O@values)[1,1]
-diffdoubleMult <- (doubleMult - outputdoubleMult[1,1]) / doubleMult
-diffdoubleMult
-
-c(valA, outputA[1,1], diffA)
-c(AB, outputAB[1,1],diffAB)
-c(doubleMult, outputdoubleMult[1,1],diffdoubleMult)
-
+objResult <- modelOut[["objective"]]@result
+original <- (model[['A']]@values %*% model[['B']]@values - model[['O']]@values) ^ 2
+new <- (modelOut[['A']]@values %*% modelOut[['B']]@values - modelOut[['O']]@values) ^ 2
+c(objResult, original, new)
