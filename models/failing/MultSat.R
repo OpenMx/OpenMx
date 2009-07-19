85419ba1f4fde3a8da741a860e437c563b3a229f
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-07-19 16:37:08 +0000

    Added manifestVars variable to MultSat.R model.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@505 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/failing/MultSat.R
+++ models/failing/MultSat.R
@@ -3,11 +3,12 @@ require(MASS)
 set.seed(200); rs=.5; xy <- mvrnorm (1000, c(0,0), matrix(c(1,rs,rs,1),2,2))
 testData <- xy
 summary(testData); cov(testData)
-dimnames(testData) <- list(NULL, c('X','Y'))
+manifestVars <- c('X', 'Y')
+dimnames(testData) <- list(NULL, manifestVars)
 
 # Currently back-end is not using 'M' matrix in RAM calculation
 multSatCovPatModel <- mxModel("multSatCovPat",
-	manifestVars = c("X", "Y"),
+	manifestVars = manifestVars,
 	mxPath(from="X", to="Y", arrows=2, free=T, values=.5, lbound=.01, labels="covXY"),
 	mxPath(from=c("X", "Y"), arrows=2, free=T, values=1, lbound=.01, labels=c("varX","varY")),
 	mxData(cov(testData), type="cov", numObs=1000, means=colMeans(testData)), type="RAM")
@@ -15,7 +16,7 @@ multSatCovPatFit <- mxRun(multSatCovPatModel)
 print(multSatCovPatFit[['S']]@values); print(multSatCovPatFit@objective)
 
 multSatRawPatModel <- mxModel("multSatRawPat",
-	manifestVars = c("X", "Y"),
+	manifestVars = manifestVars,
 	mxPath(from="X", to="Y", arrows=2, free=T, values=.5, lbound=.01, labels="covXY"),
 	mxPath(from=c("X", "Y"), arrows=2, free=T, values=1, lbound=.01, labels=c("varX","varY")),
 	mxData(testData, type="raw"), type="RAM")
@@ -34,9 +35,9 @@ print(multSatRawPatFit[['S']]@values); print(multSatRawPatFit@objective)
 
 multSatRawMatModel <- mxModel("multSatRawMat",
 	mxMatrix("Symm", nrow=2, ncol=2, free=T, values=c(1,.2,.2,1), name="expCov", 
-		dimnames = list(c('X','Y'), c('X','Y'))),
+		dimnames = list(manifestVars, manifestVars)),
 	mxMatrix("Full", nrow=1, ncol=2, free=T, values=c(0,0), name="expMean", 
-		dimnames = list(NULL, c('X', 'Y'))),
+		dimnames = list(NULL, manifestVars)),
 	mxFIMLObjective("expCov", "expMean"), mxData(testData, type="raw"))
 multSatRawMatFit <- mxRun(multSatRawMatModel)
 print(multSatRawMatFit[['expCov']]@values); print(multSatRawMatFit[['expMean']]@values)
