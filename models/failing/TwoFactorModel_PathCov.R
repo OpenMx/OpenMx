352ee5a7f26299a77a063587f742e222adb90537
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-07-29 03:13:22 +0000

    Fixing more typos in failing models
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@579 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/failing/TwoFactorModel_PathCov.R
+++ models/failing/TwoFactorModel_PathCov.R
@@ -23,7 +23,7 @@ model<-mxModel("Two Factor Model - Path",
             arrows=2,
             free=TRUE,
             values=c(1,1,1,1,1,1),
-            names=c("e1","e2","e3","e4","e5","e6")
+            labels=c("e1","e2","e3","e4","e5","e6")
             ), # residual variances
       mxPath(from=c("F1","F2"),
             arrows=2,
@@ -31,28 +31,28 @@ model<-mxModel("Two Factor Model - Path",
             free=TRUE,
             values=c(1, .5,
                     .5, 1),
-            names=c("varF1","cov","cov","varF2")
+            labels=c("varF1","cov","cov","varF2")
             ), # latent variance
       mxPath(from="F1",
             to=c("x1","x2","x3"),
             arrows=1,
             free=c(FALSE,TRUE,TRUE),
             values=c(1,1,1),
-            names=c("l1","l2","l3")
+            labels=c("l1","l2","l3")
             ), # factor loadings, factor 1
       mxPath(from="F2",
             to=c("x4","x5","x6"),
             arrows=1,
             free=c(FALSE,TRUE,TRUE),
             values=c(1,1,1),
-            names=c("l4","l5","l6")
+            labels=c("l4","l5","l6")
             ), # factor loadings, factor 1
       mxPath(from="one",
             to=c("x1","x2","x3","x4","x5","x6","F1","F2"),
             arrows=1,
             free=c(TRUE,TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,FALSE),
             values=c(1,1,1,1,1,1,0,0),
-            names=c("meanx1","meanx2","meanx3",
+            labels=c("meanx1","meanx2","meanx3",
                     "meanx4","meanx5","meanx6",
                     NA,NA)
             ) # means
@@ -62,21 +62,21 @@ model<-mxModel("Two Factor Model - Path",
 twoFactorPathCov<-mxRun(model)
 
 
-omxCheckCloseEnough(oneFactorFit@output$estimate[["l2"]], 0.999, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["l3"]], 0.959, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["l4"]], 1.028, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["l5"]], 1.008, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["l6"]], 1.021, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["varF1"]], 0.645, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["e1"]], 0.350, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["e2"]], 0.379, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["e3"]], 0.389, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["e4"]], 0.320, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["e5"]], 0.370, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["e6"]], 0.346, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["meanx1"]], 2.988, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["meanx2"]], 3.011, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["meanx3"]], 2.986, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["meanx4"]], 3.053, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["meanx5"]], 3.016, 0.01)
-omxCheckCloseEnough(oneFactorFit@output$estimate[["meanx6"]], 3.010, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["l2"]], 0.999, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["l3"]], 0.959, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["l4"]], 1.028, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["l5"]], 1.008, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["l6"]], 1.021, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["varF1"]], 0.645, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["e1"]], 0.350, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["e2"]], 0.379, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["e3"]], 0.389, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["e4"]], 0.320, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["e5"]], 0.370, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["e6"]], 0.346, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["meanx1"]], 2.988, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["meanx2"]], 3.011, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["meanx3"]], 2.986, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["meanx4"]], 3.053, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["meanx5"]], 3.016, 0.01)
+omxCheckCloseEnough(twoFactorPathCov@output$estimate[["meanx6"]], 3.010, 0.01)
--- models/failing/TwoFactorModel_PathRaw.R
+++ models/failing/TwoFactorModel_PathRaw.R
@@ -6,13 +6,13 @@ myFADataRaw<-myFADataRaw[,c("x1","x2","x3","y1","y2","y3")]
 model<-mxModel("Two Factor Model - Path", 
       type="RAM",
       mxData(myFADataRaw, type="raw"),
-      manifestVars=c("x1","x2","x3","x4","x5","x6"),
+      manifestVars=c("x1","x2","x3","y1","y2","y3"),
       latentVars=c("F1","F2"),
-      mxPath(from=c("x1","x2","x3","x4","x5","x6"),
+      mxPath(from=c("x1","x2","x3","y1","y2","y3"),
             arrows=2,
             free=TRUE,
             values=c(1,1,1,1,1,1),
-            names=c("e1","e2","e3","e4","e5","e6")
+            labels=c("e1","e2","e3","e4","e5","e6")
             ), # residual variances
       mxPath(from=c("F1","F2"),
             arrows=2,
@@ -20,28 +20,28 @@ model<-mxModel("Two Factor Model - Path",
             free=TRUE,
             values=c(1, .5,
                     .5, 1),
-            names=c("varF1","cov","cov","varF2")
+            labels=c("varF1","cov","cov","varF2")
             ), # latent variance
       mxPath(from="F1",
             to=c("x1","x2","x3"),
             arrows=1,
             free=c(FALSE,TRUE,TRUE),
             values=c(1,1,1),
-            names=c("l1","l2","l3")
+            labels=c("l1","l2","l3")
             ), # factor loadings, factor 1
       mxPath(from="F2",
-            to=c("x4","x5","x6"),
+            to=c("y1","y2","y3"),
             arrows=1,
             free=c(FALSE,TRUE,TRUE),
             values=c(1,1,1),
-            names=c("l4","l5","l6")
+            labels=c("l4","l5","l6")
             ), # factor loadings, factor 1
       mxPath(from="one",
-            to=c("x1","x2","x3","x4","x5","x6","F1","F2"),
+            to=c("x1","x2","x3","y1","y2","y3","F1","F2"),
             arrows=1,
             free=c(TRUE,TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,FALSE),
             values=c(1,1,1,1,1,1,0,0),
-            names=c("meanx1","meanx2","meanx3",
+            labels=c("meanx1","meanx2","meanx3",
                     "meanx4","meanx5","meanx6",
                     NA,NA)
             ) # means
