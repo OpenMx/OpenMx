0a6557e691aad15718861124e176f797e97c99e0
hmaes <hmaes@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-07-31 16:08:59 +0000

    updated bivCor.R
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@631 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ models/passing/bivCorM.R
@@ -0,0 +1,83 @@
+require(OpenMx)
+
+#Simulate Data
+require(MASS)
+set.seed(200)
+rs=.5
+xy <- mvrnorm (1000, c(0,0), matrix(c(1,rs,rs,1),2,2))
+testData <- xy
+selVars <- c('X','Y')
+dimnames(testData) <- list(NULL, selVars)
+summary(testData)
+cov(testData)
+
+#Fit Saturated Model with RawData and Matrices Input using Cholesky Decomposition
+bivCorModel <- mxModel("bivCor",
+    mxMatrix(
+        type="Full", 
+        nrow=2, 
+        ncol=2, 
+        free=c(T,T,F,T), 
+        values=c(1,.2,0,1), 
+        dimnames=list(selVars, selVars), 
+        name="Chol"
+    ), 
+    mxAlgebra(
+        expression=Chol %*% t(Chol), 
+        name="expCov", 
+        dimnames=list(selVars, selVars)
+    ), 
+    mxMatrix(
+        type="Full", 
+        nrow=1, 
+        ncol=2, 
+        free=T, 
+        values=c(0,0), 
+        dimnames=list(NULL, selVars), 
+        name="expMean"
+    ), 
+    mxData(
+        observed=testData, 
+        type="raw"
+    ), 
+    mxFIMLObjective(
+        covariance="expCov", 
+        means="expMean"
+    )
+    )
+
+bivCorFit <- mxRun(bivCorModel)
+EM <- mxEvaluate(expMean, bivCorFit)
+EC <- mxEvaluate(expCov, bivCorFit)
+LL <- mxEvaluate(objective, bivCorFit);
+
+
+#Test for Covariance=Zero
+bivCorModelSub <-mxModel(bivCorModel,
+    mxMatrix(
+        type="Full", 
+        nrow=2, 
+        ncol=2, 
+        free=c(T,F,F,T), 
+        values=c(1,0,0,1), 
+        dimnames=list(selVars, selVars),
+        name="Chol"
+        )
+    )
+bivCorFitSub <- mxRun(bivCorModelSub)
+EMs <- mxEvaluate(expMean, bivCorFitSub)
+ECs <- mxEvaluate(expCov, bivCorFitSub)
+LLs <- mxEvaluate(objective, bivCorFitSub);
+Chi= LLs-LL;
+LRT= rbind(LL,LLs,Chi); LRT
+
+
+#Mx answers hard-coded
+Mx.EM <- matrix(c(0.03211656, -0.004883885),1,2)
+Mx.EC <- matrix(c( 1.0092853, 0.4813504, 0.4813504, 0.9935390),2,2)
+Mx.LL <- 5415.772
+
+#Compare OpenMx results to Mx results (LL: likelihood; EC: expected covariance, EM: expected means)
+omxCheckCloseEnough(LL,Mx.LL,.001)
+omxCheckCloseEnough(EC,Mx.EC,.001)
+omxCheckCloseEnough(EM,Mx.EM,.001)
