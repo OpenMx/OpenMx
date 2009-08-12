d1e22a42a7fca555aecea48c52a0e68b1058dd98
sboker <sboker@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-08-12 01:54:56 +0000

    Added demo scripts and data file from website front page to the demo directory.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@698 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ demo/OneFactorMatrixDemo.R
@@ -0,0 +1,28 @@
+# ---------------------------------------------------------------------
+# Program: OneFactorPathDemo.R
+#  Author: Steve Boker
+#    Date: Thu Jul 30 13:33:08 EDT 2009
+#
+# This program is the OpenMx one factor path model demo for the front page
+#
+#
+# ---------------------------------------------------------------------
+# Revision History
+#    -- Thu Jul 30 13:33:11 EDT 2009
+#      Created OneFactorPathDemo.R.
+#
+# ---------------------------------------------------------------------
+
+require(OpenMx)
+demoData <- read.csv("demoOneFactor.csv", header=T)
+manifests <- names(demoData)
+latents <- c("G")
+factorModel <- mxModel("One Factor",
+    mxMatrix("Full", 5, 1, values=0.2, free=T, name="A"),
+    mxMatrix("Symm", 1, 1, values=1, free=F, name="L"),
+    mxMatrix("Diag", 5, 5, values=1, free=T, name="U"),
+    mxAlgebra(A %*% L %*% t(A) + U, name="R"),
+    mxMLObjective("R"),
+    mxData(cov(demoData), type="cov", numObs=500))
+summary(mxRun(factorModel))
+
---
+++ demo/OneFactorPathDemo.R
@@ -0,0 +1,27 @@
+# ---------------------------------------------------------------------
+# Program: OneFactorPathDemo.R
+#  Author: Steve Boker
+#    Date: Thu Jul 30 13:33:08 EDT 2009
+#
+# This program is the OpenMx one factor path model demo for the front page
+#
+#
+# ---------------------------------------------------------------------
+# Revision History
+#    -- Thu Jul 30 13:33:11 EDT 2009
+#      Created OneFactorPathDemo.R.
+#
+# ---------------------------------------------------------------------
+
+require(OpenMx)
+demoData <- read.csv("demoOneFactor.csv", header=T)
+manifests <- names(demoData)
+latents <- c("G")
+factorModel <- mxModel("One Factor", type="RAM",
+    manifestVars = manifests, latentVars = latents,
+    mxPath(from=latents, to=manifests),
+    mxPath(from=manifests, arrows=2),
+    mxPath(from=latents, arrows=2, free=F, values=1.0),
+    mxData(cov(demoData), type="cov", numObs=500))
+summary(mxRun(factorModel))
+
