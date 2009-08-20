117cb4594d4e3fd7870b8a2ec02c6300c43d9247
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-08-20 17:37:54 +0000

    Mx parser is now parsing commands one at a time, just like in mx.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@731 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- inst/tools/mxParser.py
+++ inst/tools/mxParser.py
@@ -3,6 +3,11 @@ import sys
 import re
 import string
 
+matrices = {}
+defines = {}
+title = None
+valBuffer = ""
+
 class MxMatrix:
     name = None
     type = None
@@ -13,28 +18,23 @@ class MxMatrix:
     values = None
     specification = None
 
-def parseDefines( mxInput ):
-    defines = {}
-    matchIter = re.finditer("#define\s+(\S+)\s+(\S+)", mxInput)
-    for match in matchIter:
-        defines[match.group(1)] = match.group(2)
-    return defines
+def parseDefine( mxInput ):
+    global defines
+    match = re.match("\s*#define\s+(\S+)\s+(\S+)\s*", mxInput)
+    defines[match.group(1)] = match.group(2)
+    return len(match.group(0))
 
 def parseTitle( mxInput ):
-    match = re.search("^Title(.*)$", mxInput, re.MULTILINE | re.IGNORECASE)
-    if match == None:
-        return None
-    else:
-        title = match.group(1).strip()
-        title = title.replace('.','_')
-        return title
+    global title
+    match = re.match("\s*Title(.*)$", mxInput, re.MULTILINE | re.IGNORECASE)
+    title = match.group(1).strip()
+    title = title.replace('.','_')
+    return len(match.group(0)) + 1
 
-def parseMatrices( mxInput, defines ):
-    retval = {}
-    block = re.search("Begin Matrices;(.*?)End Matrices;", 
+def parseMatrices( mxInput ):
+    global defines, matrices
+    block = re.match("\s*Begin Matrices;(.*?)End Matrices;\s*", 
     	mxInput, re.MULTILINE | re.IGNORECASE | re.DOTALL)
-    if block == None:
-        return retval
     declareLines = block.group(1).strip().split('\n')
     for declare in declareLines:
         declare = declare.strip()
@@ -56,29 +56,57 @@ def parseMatrices( mxInput, defines ):
             elif re.search("Unique", declare, re.IGNORECASE) != None:
                 matrix.free = True
                 matrix.unique = True
-            retval[matrix.name] = matrix
-    return retval
+            matrices[matrix.name] = matrix
+    return len(block.group(0))
 
-def parseSpecifications( mxInput, defines, matrices ):
-    matchIter = re.finditer("^spec\S*\s+(\S+)", mxInput, re.IGNORECASE | re.MULTILINE)
-    for match in matchIter:
-        specification = list()
-        matrixName = match.group(1)
-        matrix = matrices[matrixName]
-        specCount = specLength[matrix.type](matrix.nrow, matrix.ncol)
-        specs = re.search("^spec\S*\s+" + matrixName + "((\s*\S+\s*)" + "{" + str(specCount) + "})",
-                          mxInput, re.IGNORECASE | re.MULTILINE)
-        specIter = re.finditer("\S+", specs.group(1))
-        for spec in specIter:
-            try:
-                specification.append(int(spec.group(0)))
-            except:
-                specification.append(int(defines[spec.group(0)]))
-        matrices[matrixName].specification = specification
+def parseSpecification( mxInput ):
+    global defines, matrices
+    match = re.match("\s*spec\S*\s+(\S+)", mxInput, re.IGNORECASE | re.MULTILINE)
+    specification = list()
+    matrixName = match.group(1)
+    matrix = matrices[matrixName]
+    specCount = specLength[matrix.type](matrix.nrow, matrix.ncol)
+    specs = re.search("\s*spec\S*\s+" + matrixName + "((\s*\S+\s*)" + "{" + str(specCount) + "})\s*",
+                       mxInput, re.IGNORECASE | re.MULTILINE)
+    specIter = re.finditer("\S+", specs.group(1))
+    for spec in specIter:
+         try:
+             specification.append(int(spec.group(0)))
+         except:
+             specification.append(int(defines[spec.group(0)]))
+    matrices[matrixName].specification = specification
+    return len(specs.group(0))
 
+def parseStartOrValue( mxInput ):
+    global valBuffer
+    match = re.match("\s*(Start|Value)\s+(\S+)\s+(All|(\S+[ ]+)+)\s*", mxInput, re.IGNORECASE)
+    if (match.group(3) == "All"):
+        for matrix in matrices.values():
+            mname = "matrix" + matrix.name
+            if match.group(2) == "Value":
+                direction = "!"
+            else:
+                direction = ""
+            valBuffer += mname + "@values[" + direction + mname + "@free] <- " + match.group(2) + '\n'
+    else:
+       tripleIter = re.finditer("(\S+)\s*(\S+)\s*(\S+)\s*", match.group(3))
+       for triple in tripleIter:
+           mname = "matrix" + triple.group(1)
+           if triple.group(2) in defines:
+               row = defines[triple.group(2)]
+           else:
+               row = triple.group(2)
+           if triple.group(3) in defines:
+               col = defines[triple.group(3)]
+           else:
+               col = triple.group(3)
+           valBuffer += mname + "@values[" + row + "," + col + "] <- " + match.group(2) + '\n'
+    valBuffer += '\n'
+    return len(match.group(0))
         
 
-def printMatrices( matrices ):
+def printMatrices():
+    global matrices
     startCounter = 1
     for matrix in matrices.values():
         outstring = "matrix" + matrix.name + " <- "
@@ -109,7 +137,12 @@ def printMatrices( matrices ):
         print outstring
         print
 
-def printModel( title, matrices ):
+def printValueBuffer():
+    global valBuffer
+    print valBuffer
+
+def printModel():
+    global matrices
     print "model <- mxModel(name = \"" + title + "\")"
     matrixNames = str(map(lambda x: "matrix" + x, matrices.keys()))
     matrixNames = string.replace(matrixNames, "[", "")
@@ -125,57 +158,40 @@ specLength = {   "Diag"  : lambda row, col: row,
                  "Lower" : lambda row, col: row * (row + 1) / 2,
                  "Full"  : lambda row, col: row * col }
 
-def parseStartOrValue( mxInput, defines, matrices ):
-    matchIter = re.finditer("(Start|Value)\s+(\S+)\s+(All|(\S+[ ]+)+)", mxInput, re.IGNORECASE)
-    for match in matchIter:
-        if (match.group(3) == "All"):
-            for matrix in matrices.values():
-                mname = "matrix" + matrix.name
-                if match.group(2) == "Value":
-                    direction = "!"
-                else:
-                    direction = ""
-                print mname + "@values[" + direction + mname + "@free] <- " + match.group(2)
-        else:
-           tripleIter = re.finditer("(\S+)\s*(\S+)\s*(\S+)\s*", match.group(3))
-           for triple in tripleIter:
-               mname = "matrix" + triple.group(1)
-               if triple.group(2) in defines:
-                   row = defines[triple.group(2)]
-               else:
-                   row = triple.group(2)
-               if triple.group(3) in defines:
-                   col = defines[triple.group(3)]
-               else:
-                   col = triple.group(3)
-               print mname + "@values[" + row + "," + col + "] <- " + match.group(2)
-        print    
+mxDirectives = {    "\s*title" : parseTitle,
+                    "\s*#define" : parseDefine,
+                    "\s*Begin Matrices" : parseMatrices,
+                    "\s*spec\S*" : parseSpecification,
+                    "\s*(start|value)" : parseStartOrValue }
 
+def tryDirectives ( mxInput ):
+    for directive in mxDirectives.keys():
+        match = re.match(directive, mxInput, re.IGNORECASE)
+        if (match != None):
+            subtract = mxDirectives[directive](mxInput)
+            mxInput = mxInput[subtract : ]
+            return mxInput
+    match = re.match(".+", mxInput)
+    if (match != None):
+        subtract = len(match.group(0)) + 1
+        mxInput = mxInput[subtract : ]
+    return mxInput
+                
 def parseModel( mxInput ):
 
     # Remove all comments from the file
-    mxInput = re.sub('!.*\n', '\n', mxInput)
-
-    # Find the title of the super-model
-    title = parseTitle(mxInput)
-
-    # Find all the #define statements
-    defines = parseDefines(mxInput)
-
-    # Find all the matrix definitions
-    matrices = parseMatrices(mxInput, defines)
-
-    # Find all the matrix specifications
-    parseSpecifications(mxInput, defines, matrices)
+    mxInput = re.sub(re.compile('!.*$', re.MULTILINE), '', mxInput)
+    
+    while len(mxInput) > 0:
+        mxInput = tryDirectives(mxInput)
 
     # Print matrix declarations
-    printMatrices(matrices)
+    printMatrices()
 
-    #Find all Start or Value declarations and print them
-    parseStartOrValue(mxInput, defines, matrices)
-
+    # Print any value assignments
+    printValueBuffer()
 
     # Print model declaration
-    printModel(title, matrices)
+    printModel()
 
 parseModel(sys.stdin.read())
