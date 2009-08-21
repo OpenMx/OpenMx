56d39f24a281b025c40f98aa749289e41e627293
tbates <tbates@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-08-21 16:33:56 +0000

    Added comments
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@735 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- demo/UnivariateTwinAnalysis_MatrixRaw.R
+++ demo/UnivariateTwinAnalysis_MatrixRaw.R
@@ -23,14 +23,22 @@ twinACEModel <- mxModel("twinACE",
     mxMatrix("Full", nrow=1, ncol=1, free=FALSE, values=.5,  name="h"),
     mxAlgebra(X %*% t(X), name="A"),
     mxAlgebra(Y %*% t(Y), name="C"),
-    mxAlgebra(Z %*% t(Z), name="E"), 
+    mxAlgebra(Z %*% t(Z), name="E"),
+    # Algebra for expected variance/covariance matrix in MZs
     mxAlgebra(rbind (cbind(A+C+E  , A+C),
                      cbind(A+C    , A+C+E)), dimnames = list(selVars, selVars), name="expCovMZ"),
+    # Algebra for expected variance/covariance matrix in DZs
     mxAlgebra(rbind (cbind(A+C+E  , h%x%A+C),
                      cbind(h%x%A+C, A+C+E)), dimnames = list(selVars, selVars), name="expCovDZ"),
+		# Build model for the MZ data.
     mxModel("MZ",
+			  # read in the manifest variables (selVars), to provide the observed covariance matrix and means for this group. 
         mxData(mzfData, type="raw"), 
-        mxFIMLObjective("twinACE.expCovMZ", "twinACE.expMeanMZ")),
+				# link these observations to an objective - our expected MZ covariance and means, 
+				# so that the likelihood of the observed data can be calculated from their departure from our expectations.
+        mxFIMLObjective("twinACE.expCovMZ", "twinACE.expMeanMZ")
+    ),
+		# Build model for the DZ data.
     mxModel("DZ", 
         mxData(dzfData, type="raw"), 
         mxFIMLObjective("twinACE.expCovDZ", "twinACE.expMeanDZ")),
@@ -42,14 +50,17 @@ twinACEFit <- mxRun(twinACEModel)
 
 MZc <- mxEval(expCovMZ, twinACEFit)
 DZc <- mxEval(expCovDZ, twinACEFit)
-M <- mxEval(expMeanMZ, twinACEFit)
-A <- mxEval(A, twinACEFit)
-C <- mxEval(C, twinACEFit)
-E <- mxEval(E, twinACEFit)
-V <- (A+C+E)
-a2 <- A/V
-c2 <- C/V
-e2 <- E/V
+M   <- mxEval(expMeanMZ, twinACEFit)
+
+# Retrieve the A, C, and E variance components
+A   <- mxEval(A, twinACEFit)
+C   <- mxEval(C, twinACEFit)
+E   <- mxEval(E, twinACEFit)
+
+totalVariance <- (A+C+E)
+a2  <- A/totalVariance  # Standardize the variance components
+c2  <- C/totalVariance
+e2  <- E/totalVariance
 ACEest <- rbind(cbind(A,C,E),cbind(a2,c2,e2))
 LL_ACE <- mxEval(objective, twinACEFit)
 
@@ -85,15 +96,16 @@ twinAEModel <- mxModel(twinACEModel,
     )
 twinAEFit <- mxRun(twinAEModel)
 
+# As above, retrieve covariance, means, and A, C, and E variance components
 MZc <- mxEval(expCovMZ, twinAEFit)
 DZc <- mxEval(expCovDZ, twinAEFit)
-A <- mxEval(A, twinAEFit)
-C <- mxEval(C, twinAEFit)
-E <- mxEval(E, twinAEFit)
+A   <- mxEval(A, twinAEFit)
+C   <- mxEval(C, twinAEFit)
+E   <- mxEval(E, twinAEFit)
 V <- (A + C + E)
-a2 <- A / V
-c2 <- C / V
-e2 <- E / V
+a2  <- A / V
+c2  <- C / V
+e2  <- E / V
 AEest <- rbind(cbind(A, C, E),cbind(a2, c2, e2))
 LL_AE <- mxEval(objective, twinAEFit)
 
