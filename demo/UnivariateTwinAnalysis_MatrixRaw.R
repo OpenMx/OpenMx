703acbae1bfc8b830af965d8e3a83364196e70b2
tbates <tbates@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-08-21 16:03:08 +0000

    UnivariateTwinAnalysis_MatrixRaw.R
    labeled all parameter calls and changed "T" to "TRUE"
    
    
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@734 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- demo/UnivariateTwinAnalysis_MatrixRaw.R
+++ demo/UnivariateTwinAnalysis_MatrixRaw.R
@@ -15,12 +15,12 @@ cov(dzfData,use="complete")
 
 #Fit ACE Model with RawData and Matrices Input
 twinACEModel <- mxModel("twinACE", 
-    mxMatrix("Full", 1, 2, T, 20, "mean", dimnames=list(NULL, selVars), name="expMeanMZ"), 
-    mxMatrix("Full", 1, 2, T, 20, "mean", dimnames=list(NULL, selVars), name="expMeanDZ"), 
-    mxMatrix("Full", nrow=1, ncol=1, free=TRUE, values=.6, label="a", name="X"),
-    mxMatrix("Full", nrow=1, ncol=1, free=TRUE, values=.6, label="c", name="Y"),
-    mxMatrix("Full", nrow=1, ncol=1, free=TRUE, values=.6, label="e", name="Z"),
-    mxMatrix("Full", nrow=1, ncol=1, free=FALSE, values=.5, name="h"),
+    mxMatrix("Full", nrow=1, ncol=2, free=TRUE,  values= 20, label="mean", dimnames=list(NULL, selVars), name="expMeanMZ"), 
+    mxMatrix("Full", nrow=1, ncol=2, free=TRUE,  values= 20, label="mean", dimnames=list(NULL, selVars), name="expMeanDZ"), 
+    mxMatrix("Full", nrow=1, ncol=1, free=TRUE,  values=.6,  label="a", name="X"),
+    mxMatrix("Full", nrow=1, ncol=1, free=TRUE,  values=.6,  label="c", name="Y"),
+    mxMatrix("Full", nrow=1, ncol=1, free=TRUE,  values=.6,  label="e", name="Z"),
+    mxMatrix("Full", nrow=1, ncol=1, free=FALSE, values=.5,  name="h"),
     mxAlgebra(X %*% t(X), name="A"),
     mxAlgebra(Y %*% t(Y), name="C"),
     mxAlgebra(Z %*% t(Z), name="E"), 
