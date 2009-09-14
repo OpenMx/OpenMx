26526a73c9bd721c8b2e7bf2e100ac9d624c887d
tbates <tbates@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-09-14 18:50:57 +0000

    Convert to use three models: model, mz, and dz Reference MZ-group A,C,E matrices in DZ algebra Compute standardized path coefficients:    var = matrix(0,nVar,nVar);  diag(var)=diag(Vtot);  # variances on the diagonal, 0's elsewhere.    SD =  solve(sqrt(var))   # Inverse (solve) of diagonal matrix of standard deviations: \sqrt(var)~ in oldMx-speak Improved comments
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@813 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- demo/TwinAnalysis_Multivariate_Matrix_Raw.R
+++ demo/TwinAnalysis_Multivariate_Matrix_Raw.R
@@ -1,12 +1,20 @@
 # Multivariate matrix-style twin script...
-# History: tbates 24.09.2009 first draft
+# History: tbates 24.08.2009 first draft
+# History: tbates 12.09.2009 
+# 	Convert to use three models: model, mz, and dz
+# 	Reference MZ-group A,C,E matrices in DZ algebra
+# 	Compute standardized path coefficients:
+# 		var = matrix(0,nVar,nVar);  diag(var)=diag(Vtot);  # variances on the diagonal, 0's elsewhere.
+# 		SD =  solve(sqrt(var))   # Inverse (solve) of diagonal matrix of standard deviations: \sqrt(var)~ in oldMx-speak
+# 	Improved comments
 
 require(OpenMx)
 #Import Data
+
 data(twinData)
 twinVar = names(twinData); twinVar
 # [1] "fam"   "age"   "zyg"   "part"  "wt1"   "wt2"   "ht1"   "ht2"   "htwt1" "htwt2" "bmi1"  "bmi2" 
-selVars <- c('bmi1','ht1', 'bmi2', 'ht2');  # pick out variables to be modeled (in this case two), for twin 1 and then twin 2
+selVars <- c('ht1', 'bmi1','ht2','bmi2');  # pick out variables to be modeled (in this case two), for twin 1 and then twin 2
 mzfData <- as.matrix(subset(twinData, zyg==1, selVars)) # assumes MZ F pairs are coded 1
 dzfData <- as.matrix(subset(twinData, zyg==3, selVars)) # assumes MZ M pairs are coded 3
 head(mzfData);
@@ -21,13 +29,11 @@ cov2cor(cov(dzfData,use="complete"));
 ##### Fit ACE Model  ##### 
 # Define 1*1 constant of 0.5 for DZ cov A, or .25 for D
 hMatrix = mxMatrix("Full", nrow=1, ncol=1, free=FALSE, values=.5, name="h")
-qMatrix = mxMatrix("Full", nrow=1, ncol=1, free=FALSE, values=.25, name="q")
 
 # use MZT1's means for the two traits as start values for the means
 meanStarts = rep(obsMZmeans[1:2],2);
 	#      bmi1       ht1      bmi1       ht1 
 	# 21.353328  1.629724 21.353328  1.629724 
-
 meanLabels = paste("Trait", rep(1:nVar,2),  "mean", sep=""); # make labels for the means matrix
 # [1] "Trait1mean" "Trait2mean" "Trait1mean" "Trait2mean"
 
@@ -37,71 +43,80 @@ expMZMeans = mxMatrix("Full", nrow=1, ncol=(nVar*2), free=TRUE, values=meanStart
 expDZMeans = expMZMeans; 
 expDZMeans@name="expMeanDZ"; 
 
-# Matrices for path coefficients, Labels for cells, names for the matrices
-aLabels <- c(
-	"a1v1",
-	"a1v2", "a2v2");
-cLabels <- c(
-	"c1v1",
-	"c1v2", "c2v2");
-eLabels <- c(
-	"e1v1",
-	"e1v2", "e2v2");
+# Matrices for path coefficients
+aMatrix = mxMatrix("Lower", nrow=nVar, ncol=nVar, free=TRUE, values=.5, name="a") # Additive genetic path coefficient
+cMatrix = mxMatrix("Lower", nrow=nVar, ncol=nVar, free=TRUE, values=.5, name="c") # Common environmental path coefficient
+eMatrix = mxMatrix("Lower", nrow=nVar, ncol=nVar, free=TRUE, values=.5, name="e") # Unique environmental path coefficient
 
-aMatrix = mxMatrix("Lower", nrow=nVar, ncol=nVar, free=TRUE, values=.5, labels=aLabels, name="a") # Additive genetic path coefficient
-cMatrix = mxMatrix("Lower", nrow=nVar, ncol=nVar, free=TRUE, values=.5, labels=cLabels, name="c") # Common environmental path coefficient
-eMatrix = mxMatrix("Lower", nrow=nVar, ncol=nVar, free=TRUE, values=.5, labels=eLabels, name="e") # Unique environmental path coefficient
+# Make the mz group: define variance as square of path coefficients, define algebra of twin covariance, 
+# import mz data, and set objective to model the covariance and means observed in these data
 
-twinACEModel <- mxModel("twinACE",
-	hMatrix, expMZMeans, expDZMeans,
-	aMatrix, cMatrix, eMatrix,
+mzGroup <- mxModel("mz",
+	hMatrix, aMatrix, cMatrix, eMatrix,
+	expMZMeans,
 	# Multiply by each path coefficient by its inverse to get variance component
-    mxAlgebra(a %*% t(a), name="A"), # additive genetic variance
-    mxAlgebra(c %*% t(c), name="C"), # common environmental variance
-    mxAlgebra(e %*% t(e), name="E"), # unique environmental variance
-
+  mxAlgebra(a %*% t(a), name="A"), # additive genetic variance
+  mxAlgebra(c %*% t(c), name="C"), # common environmental variance
+  mxAlgebra(e %*% t(e), name="E"), # unique environmental variance
 	# MZ ACE algebra
 	mxAlgebra(rbind (cbind(A+C+E  , A+C),
-                     cbind(A+C    , A+C+E)), dimnames = list(selVars, selVars), name="expCovMZ"),
-	# DZ ACE algebra
-    mxAlgebra(rbind (cbind(A+C+E  , h%x%A+C),
-                     cbind(h%x%A+C, A+C+E)), dimnames = list(selVars, selVars), name="expCovDZ"),
-	# Import raw data for each group, setting its objective to the respective group algebras
-    mxModel("MZ", mxData(mzfData, type="raw"), mxFIMLObjective("twinACE.expCovMZ", "twinACE.expMeanMZ")),
-    mxModel("DZ", mxData(dzfData, type="raw"), mxFIMLObjective("twinACE.expCovDZ", "twinACE.expMeanDZ")),
-	# Make algebra for sum of the group objectives,    
-	mxAlgebra(MZ.objective + DZ.objective, name="twin"), 
-	# Set this to be the objective for the supermodel
+                   cbind(A+C    , A+C+E)), dimnames = list(selVars, selVars), name="expCov"),
+	# Import raw data setting and set objective
+	mxData(mzfData, type="raw"), 
+	mxFIMLObjective("expCov", "expMeanMZ")
+)
+
+# make the dz group: much simpler - just has its data and means, and refers to the ACE matrices 
+# in mz group so that both groups are fitted of the same parameters
+dzGroup <- mxModel("dz",
+	hMatrix, 
+	expDZMeans,
+	 mxAlgebra(rbind (cbind(mz.A+ mz.C+ mz.E  , h %x% mz.A + mz.C),
+	                  cbind(h %x% mz.A + mz.C, mz.A + mz.C + mz.E)), dimnames = list(selVars, selVars), name="expCov"),
+	mxData(dzfData, type="raw"), 
+	mxFIMLObjective("expCov", "expMeanDZ")
+)
+
+# Combine the mz and dz groups in a supermodel which can have as its objective maximising the likelihood ofboth groups simultaneously.
+model = mxModel("ACE", mzGroup, dzGroup,
+	mxAlgebra(mz.objective + dz.objective, name="twin"), 
 	mxAlgebraObjective("twin")
 )
 
 #Run ACE model
-twinACEFit = mxRun(twinACEModel)
+fit = mxRun(model)
 
 #Extract various fitted results
-MZc = mxEval(expCovMZ,  twinACEFit) # Same effect as expCovMZ@matrices$twinACEFit
-DZc = mxEval(expCovDZ,  twinACEFit)
-M   = mxEval(expMeanMZ, twinACEFit)
-A   = mxEval(A, twinACEFit)
-C   = mxEval(C, twinACEFit)
-E   = mxEval(E, twinACEFit)
+MZc = mxEval(mz.expCov,  fit); # Same effect as expCovMZ@matrices$fit
+DZc = mxEval(dz.expCov,  fit);
+M   = mxEval(mz.expMeanMZ, fit);
+A   = mxEval(mz.A, fit);
+C   = mxEval(mz.C, fit);
+E   = mxEval(mz.E, fit);
+Vtot= A+C+E;
 
-# Calculate the genetic (and C & E) covariances
-rA = cov2cor(A) # calculates rg (off diagonal)
-rC = cov2cor(C) # calculates rc (off diagonal)
-rE = cov2cor(E) # calculates re (off diagonal)
-ACErs = data.frame(round(cbind(rA,rC,rE),2), row.names=selVars[1:nVar]); 
-names(ACErs)<- paste(rep(c("A", "C", "E"),each=2), rep(1:2), sep=""); ACErs
+a   = mxEval(mz.a, fit);
+c   = mxEval(mz.c, fit);
+e   = mxEval(mz.e, fit);
 
 # Calc standardised variance components
-Vtot= (A+C+E)
-a2  = A/Vtot
-c2  = C/Vtot
-e2  = E/Vtot
-ACEest = data.frame(round(cbind(a2,c2,e2),2), row.names=selVars[1:nVar])
-# make a nice-ish table
-names(ACEest)<- paste(rep(c("A", "C", "E"),each=2), rep(1:2), sep="")
+var = matrix(0,nVar,nVar);  diag(var)=diag(Vtot);  # variances on the diagonal, 0's elsewhere.
+SD    <- solve(sqrt(var))   # Inverse (solve) of diagonal matrix of standard deviations: \sqrt(var)~ in oldMx-speak
+
+# standardized _path_ coefficients ready to be stacked together
+a2 <- SD %*% a ; # Standardized path coefficients
+c2 <- SD %*% c ;
+e2 <- SD %*% e ;
+ACEest = data.frame(round(cbind(a2,c2,e2),3), row.names=selVars[1:nVar])
+# make a table
+names(ACEest)<- paste(rep(c("A", "C", "E"),each=2), rep(1:2), sep="");
+print(ACEest);
+#          A1    A2 C1 C2     E1    E2
+# ht1   0.938 0.000  0  0  0.345 0.000
+# bmi1 -0.030 0.883  0  0 -0.145 0.445
 
 # Get the fit, and print along with standardised ACE
-LL_ACE = mxEval(objective, twinACEFit)
-ACEest; LL_ACE
+LL_ACE = mxEval(objective, fit); print(LL_ACE)
+
+#           [,1]
+# [1,] -1500.460
