b01189d6701a36f7b051282a0cc82bc7b3ed6241
tbates <tbates@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2009-10-16 16:04:56 +0000

    removed comment about now bug which is fixed, added comment about dot output completeness
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@937 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/failing/omx_graphvis_example.R
+++ models/failing/omx_graphvis_example.R
@@ -1,3 +1,9 @@
+# SCRIPT: omx_graphvis_example.R
+# Author: Timothy  Bates tim.bates@ed.ac.uk
+# History:  Fri Oct 16 17:02:32 BST 2009
+# OpenMx: http://www.openmx.virginia.com
+#  .dot output not modelling means
+##########################################
 require(OpenMx)
 testData = as.matrix(rnorm(n=1000, mean=0, sd=1)) # make a thousand numbers, with mean 0 and sd 1
 selVars = c("X")
@@ -9,4 +15,18 @@ model <- mxModel("univSat1", manifestVars= selVars,
 )
 fit = mxRun(model)
 omxGraphviz(fit)
-# Error: mxPath() call will generate 0 paths but you have specified 1 arrows with 'from' argument assigned to  and 'to' argument assigned to 
+
+# dot has a manifest with cov arrows, but no means triangle
+# digraph univSat1 { 
+#    node [style=filled, fontname="Arial", fontsize=16]
+#    X [shape=box, fillcolor="#a9fab1", height=0.5, width=0.5];
+#    X -> X[dir=both, headport=s, tailport=s];
+# }
+
+# summary(fit)
+#    name matrix row col   Estimate  Std.Error
+# 1 var X      S   1   1 0.99325690 0.03114318
+# 2  <NA>      M   1   1 0.01635556 0.02262332
+# ...
+# Observed statistics:  1000 
+# Estimated parameters:  2 
