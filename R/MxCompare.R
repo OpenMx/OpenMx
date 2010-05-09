88d883a13aace2ba07bc4ff9c6878a9cd79eb7e8
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2010-05-09 16:26:18 +0000

    Improvements to the mxCompare() function
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1242 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- R/MxCompare.R
+++ R/MxCompare.R
@@ -13,7 +13,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-mxCompare <- function(base, comparison, digits = 3) {
+mxCompare <- function(base, comparison, digits = 3, all = FALSE) {
 	if (missing(base)) {
 		stop("'base' argument be a MxModel object or list of MxModel objects")	
 	}
@@ -41,14 +41,55 @@ mxCompare <- function(base, comparison, digits = 3) {
 	}
 	baseSummaries <- omxLapply(base, summary)	
 	compareSummaries <- omxLapply(comparison, summary)
-	resultsTable <- showFitStatistics(baseSummaries, compareSummaries, digits)
+	resultsTable <- showFitStatistics(baseSummaries, compareSummaries, digits, all)
 	return(resultsTable)
 }
 
-showFitStatistics <- function(baseSummaries, compareSummaries, digits) {
-	statistics <- mapply(collectStatistics, baseSummaries, compareSummaries, digits)
-	statistics <- data.frame(t(statistics))
+showFitStatistics <- function(baseSummaries, compareSummaries, digits, all) {
+	statistics <- list()
+	if(all) {
+		for(i in 1:length(baseSummaries)) {
+			nextBaseSummary <- baseSummaries[[i]]
+			statistics[[length(statistics) + 1]] <- collectBaseStatistics(nextBaseSummary, digits)
+			for(j in 1:length(compareSummaries)) {
+				nextCompareSummary <- compareSummaries[[j]]
+				statistics[[length(statistics) + 1]] <- collectStatistics(nextBaseSummary,
+					nextCompareSummary, digits)
+			}
+		}
+	} else {
+		maxLength <- max(length(baseSummaries), length(compareSummaries))
+		previousBaseSummaryIndex <- -1
+		for(i in 1:maxLength) {
+			nextBaseSummaryIndex <- (i - 1) %% length(baseSummaries) + 1
+			nextCompareSummaryIndex <- (i - 1) %% length(compareSummaries) + 1
+			nextBaseSummary <- baseSummaries[[nextBaseSummaryIndex]]
+			nextCompareSummary <- compareSummaries[[nextCompareSummaryIndex]]
+			if (previousBaseSummaryIndex != nextBaseSummaryIndex) {
+				statistics[[length(statistics) + 1]] <- collectBaseStatistics(nextBaseSummary, digits)
+			}
+			statistics[[length(statistics) + 1]] <- collectStatistics(nextBaseSummary,
+				nextCompareSummary, digits)
+			previousBaseSummaryIndex <- nextBaseSummaryIndex
+		}
+	}
+	statistics <- do.call(rbind, statistics)
 	return(statistics)
+}
+
+collectBaseStatistics <- function(refSummary, digits) {
+	baseStats <- data.frame(stringsAsFactors = FALSE,
+		refSummary$modelName,
+		as.character(NA),
+		refSummary$estimatedParameters,
+		signif(refSummary$Minus2LogLikelihood, digits),
+		refSummary$degreesOfFreedom,
+		signif(refSummary$AIC.Mx, digits),
+		as.numeric(NA),
+		as.numeric(NA), 
+		as.numeric(NA))
+	names(baseStats) <- c("base", "comparison", "ep", "minus2LL", "df", "AIC", "diffLL", "diffdf", "p")
+	return(baseStats)
 }
 
 collectStatistics <- function(refSummary, otherSummary, digits) {
