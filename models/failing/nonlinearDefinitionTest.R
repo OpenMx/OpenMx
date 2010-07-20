af59e99381c1157c4f99c02ed5540644393116f4
restabrook <restabrook@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2010-07-20 17:25:31 +0000

    Added nonlinearDefinitionTest.R
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1324 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ models/failing/nonlinearDefinitionTest.R
@@ -0,0 +1,44 @@
+#preamble of some kind
+
+library(OpenMx)
+
+#sample size
+n <- 500
+
+# generate data
+x <- rnorm(n, 3, 2)
+z <- rep(1:4, each=n/4)
+y <- (.8^z) * x + rnorm(n)
+
+data <- data.frame(x,y,z)
+
+model <- mxModel("Non Linear Definition",
+  mxData(data, "raw"),
+  mxMatrix("Diag", 2, 2, T, 1, name="S"),
+  mxMatrix("Full", 2, 2, 
+    free=c(F, F, F, F), 
+    values=c(1, 0, 0, 1),  
+    labels=c(NA, NA, "bexp[1,1]", NA),
+    name="IA"),
+  mxMatrix("Full", 1, 1, T, 0.7, "beta1", name="B"),
+  mxMatrix("Full", 1, 1, F, 0, "data.z", name="D"),
+  mxMatrix("Full", 1, 2, T, 0, c("mu_x", "beta0"), name="M"),
+  mxAlgebra(B ^ D, name="bexp"),
+  mxAlgebra(M %*%IA, name="mu"),
+  mxAlgebra(IA %*% S %*% t(IA), name="sigma"),
+  mxFIMLObjective("sigma", "mu", dimnames=c("x", "y"))
+)
+
+results <- mxRun(model)
+
+summary(results)
+
+check <- nls(y ~ b0 + (b1 ^ z) * x, start=list(b0=0, b1=0.7))
+
+#beta0
+omxCheckCloseEnough(results@output$estimate[5], 
+  summary(check)$parameters[1], 0.01)
+
+#beta1
+omxCheckCloseEnough(results@output$estimate[3], 
+  summary(check)$parameters[2], 0.01)
