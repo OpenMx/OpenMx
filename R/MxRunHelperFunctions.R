6e1ea8bf9eb175eb5616c5b43222a714b58b7e51
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2010-10-08 18:22:10 +0000

    Eliminated warnings for confidence interval optimization codes.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1463 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- R/MxRunHelperFunctions.R
+++ R/MxRunHelperFunctions.R
@@ -60,18 +60,10 @@ processOptimizerOutput <- function(suppressWarnings, flatModel, matrixNames,
 		names(output$algebras) <- algebraNames
 	}
 	if (length(output$confidenceIntervals) > 0) {
-		dimnames(output$confidenceIntervals) <- list(intervalNames, c('lbound', 'ubound'))
-		dimnames(output$confidenceIntervalCodes) <- list(intervalNames, c('lbound', 'ubound'))
-		lowerWarnings <- which(output$confidenceIntervalCodes[,1] > 0)
-		upperWarnings <- which(output$confidenceIntervalCodes[,2] > 0)
-		if (length(lowerWarnings) > 0 && !suppressWarnings) {
-			intervalWarnings("lower", flatModel@name, lowerWarnings, 
-				output$confidenceIntervalCodes[,1], intervalNames)
-		}
-		if (length(upperWarnings) > 0 && !suppressWarnings) {
-			intervalWarnings("upper", flatModel@name, upperWarnings, 
-				output$confidenceIntervalCodes[,2], intervalNames)
-		}
+		dimnames(output$confidenceIntervals) <- list(
+			intervalNames, c('lbound', 'ubound'))
+		dimnames(output$confidenceIntervalCodes) <- list(
+			intervalNames, c('lbound', 'ubound'))
 	}
 	return(output)
 }
@@ -94,17 +86,6 @@ processErrorConditions <- function(model, unsafe, suppressWarnings) {
 	            output$status[[3]]), call. = FALSE)
 	    }
     }
-}
-
-intervalWarnings <- function(type, modelname, indices, codes, intervalNames) {
-	for(i in 1:length(indices)) {
-		index <- indices[[i]]
-		npsolWarnings(
-			paste("In model", omxQuotes(modelname), 
-			"while computing the", type, "bound for", 
-			omxQuotes(intervalNames[[index]])),
-			codes[[index]])
-	}
 }
 
 populateRunStateInformation <- function(model, parameters, matrices, 
