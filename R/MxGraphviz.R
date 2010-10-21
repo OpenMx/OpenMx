3004d1c277adb0ca028e7b9a2aea6e9290bfc0df
tbates <tbates@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2010-10-21 21:49:14 +0000

    edited writeDotFile private function 1. added code for a rank statement arranging manifests at bottom (won't suit all models but many will be neater) 2. added comment lines to distinguish the sections of the .dot file
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1476 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- R/MxGraphviz.R
+++ R/MxGraphviz.R
@@ -14,62 +14,72 @@
 #   limitations under the License.
 
 
-writeDotFile <- function(model, graph, dotFilename) {
-	outputLines <- list()
-	graphName <- paste('"', model@name, '"', sep='')
-	outputLines <- c(outputLines, paste("digraph", graphName, "{"))
-	outputLines <- c(outputLines, paste('\t', 'node [style=filled, fontname="Arial", fontsize=16];'))
+writeDotFile <-function (model, graph, dotFilename) 
+{
+    outputLines <- list()
+    graphName   <- paste("\"", model@name, "\"", sep = "")
+    outputLines <- c(outputLines, paste("digraph", graphName, "{"))
+    outputLines <- c(outputLines, paste("\t", "node [style=filled, fontname=\"Ariel\", fontsize=16];"))
 	if (length(graph@manifestVars) > 0) {
-		for(i in 1:length(graph@manifestVars)) {
-			outputLines <- c(outputLines, paste('\t', graph@manifestVars[[i]], 
-				'[shape=box, fillcolor="#a9fab1", height=0.5, width=0.5];'))
-		}
-	}
-	if (length(graph@latentVars) > 0) {
-		for(i in 1:length(graph@latentVars)) {
-			outputLines <- c(outputLines, paste('\t', graph@latentVars[[i]], 
-				'[shape=circle, fillcolor="#f4fd78"];'))		
-		}
-	}
-	if (!is.null(model[['M']])) {
-		outputLines <- c(outputLines, paste('\t', 'one', '[shape=triangle];'))
-	}
-	if (length(graph@paths) > 0) {
-		for(i in 1:length(graph@paths)) {
-			path <- graph@paths[[i]]
-			allfrom <- path@from
-			allto <- path@to
-			allarrows <- path@arrows
-			maxlength <- max(length(allfrom), length(allto))
-			for(i in 0:(maxlength - 1)) {
-				outputArrow <- list()
-				from <- allfrom[i %% length(allfrom) + 1]
-				to <- allto[i %% length(allto) + 1]
-				arrows <- allarrows[i %% length(allarrows) + 1]
-				outputArrow <- c(outputArrow, paste('\t', from, "->", to))
-				if (arrows == 1) {
-					outputArrow <- c(outputArrow, "[dir=forward]")
-				} else if (arrows == 2) {
-				    if (from == to && from %in% graph@latentVars) {
-						outputArrow <- c(outputArrow, "[dir=both, headport=n, tailport=n]")			
-					} else if (from == to) {
-						outputArrow <- c(outputArrow, "[dir=both, headport=s, tailport=s]")						
-					} else {
-						outputArrow <- c(outputArrow, "[dir=both]")					
-					}
-				}
-				outputArrow <- c(outputArrow, ';')
-				outputArrow <- paste(outputArrow, collapse='')
-				outputLines <- c(outputLines, outputArrow)
-			}
-		}
-	}
-	outputLines <- c(outputLines, '}\n')
-	outputLines <- paste(outputLines, collapse='\n')
-	cat(outputLines, file = dotFilename)
-	return(invisible(outputLines))
+        outputLines <-append(outputLines, "/* Manifest Variables*/")
+	    rankString  <-paste(graph@manifestVars, collapse="; ")
+		rankString  <-paste("{ rank = max;", rankString, "}", collapse="") # force manifests to bottom of graph
+		outputLines <-append(outputLines, rankString)
+        for (i in 1:length(graph@manifestVars)) {
+            outputLines <- c(outputLines, paste("\t", graph@manifestVars[[i]], 
+                "[shape=box, fillcolor=\"#a9fab1\", height=0.5, width=0.5];"))
+        }
+    }
+    if (length(graph@latentVars) > 0) {
+        outputLines <-append(outputLines, "/* Latent Variables*/")
+		for (i in 1:length(graph@latentVars)) {
+            outputLines <- c(outputLines, paste("\t", graph@latentVars[[i]], 
+                "[shape=circle, fillcolor=\"#f4fd78\"];"))
+        }
+    }
+    if (!is.null(model[["M"]])) {
+        outputLines <-append(outputLines, "/* Means */")
+        outputLines <- c(outputLines, paste("\t", "one", "[shape=triangle];"))
+    }
+    if (length(graph@paths) > 0) {
+        outputLines <-append(outputLines, "/* Paths */")
+		for (i in 1:length(graph@paths)) {
+            path    <- graph@paths[[i]]
+            allfrom <- path@from
+            allto   <- path@to
+            allarrows <- path@arrows
+            maxlength <- max(length(allfrom), length(allto))
+            for (i in 0:(maxlength - 1)) {
+                outputArrow <- list()
+                from   <- allfrom[i%%length(allfrom) + 1]
+                to     <- allto[i%%length(allto) + 1]
+                arrows <- allarrows[i%%length(allarrows) + 1]
+                outputArrow <- c(outputArrow, paste("\t", from, "->", to))
+                if (arrows == 1) {
+                  outputArrow <- c(outputArrow, "[dir=forward]")
+                }
+                else if (arrows == 2) {
+                  if (from == to && from %in% graph@latentVars) {
+                    outputArrow <- c(outputArrow, "[dir=both, headport=n, tailport=n]")
+                  }
+                  else if (from == to) {
+                    outputArrow <- c(outputArrow, "[dir=both, headport=s, tailport=s]")
+                  }
+                  else {
+                    outputArrow <- c(outputArrow, "[dir=both]")
+                  }
+                }
+                outputArrow <- c(outputArrow, ";")
+                outputArrow <- paste(outputArrow, collapse = "")
+                outputLines <- c(outputLines, outputArrow)
+            }
+        }
+    }
+    outputLines <- c(outputLines, "}\n")
+    outputLines <- paste(outputLines, collapse = "\n")
+    cat(outputLines, file = dotFilename)
+    return(invisible(outputLines))
 }
-
 omxGraphviz <- function(model, dotFilename = "") {
 	if (missing(model) || !is(model, "MxModel")) {
 		stop("The first argument is not an MxModel object")
