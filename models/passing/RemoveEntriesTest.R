cf789e456b64c3d6bd8c4212239faf57206b91d9
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2011-01-06 04:34:36 +0000

    Bug fix in mxModel() when using remove = TRUE
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1524 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ models/passing/RemoveEntriesTest.R
@@ -0,0 +1,30 @@
+#
+#   Copyright 2007-2010 The OpenMx Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+require(OpenMx)
+A <- mxMatrix('Full', 1, 1, name = 'A')
+B <- mxAlgebra(A + A, name = 'B')
+omxCheckError(mxModel('model', 'A', 'B'),
+	paste("Cannot use character vectors",
+	"when remove = FALSE. Instead supply",
+	"the named entity to the mxModel() function: 'A' and 'B'"))
+model <- mxModel('model', A, B)
+omxCheckError(mxModel(model, A, B, remove=TRUE),
+	paste("Cannot use named entities",
+	"when remove = TRUE. Instead give",
+	"the name of the entity when removing it.",
+	"See http://openmx.psyc.virginia.edu/wiki/mxmodel-help#Remove_an_object_from_a_model"))
+model <- mxModel(model, 'A', 'B', remove=TRUE)
+omxCheckEquals(length(names(model)), 0)
