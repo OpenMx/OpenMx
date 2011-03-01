fe5223276a9a5af6510a4821946291b9080f550c
tbates <tbates@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2011-03-01 14:47:53 +0000

    Added "As of openMx 1.0, valid types include "RAM" and "raw" " to the type description in mxModel.Rd Added explicit model type of "raw" to RAM-FIML-defvars.R and homogenised use of T and F for TRUE and FALSE
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1565 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/passing/RAM-FIML-defvars.R
+++ models/passing/RAM-FIML-defvars.R
@@ -20,13 +20,15 @@ require(OpenMx)
 yi <- c(-0.264,-0.230,0.166,0.173,0.225,0.291,0.309,0.435,0.476,0.617,0.651,0.718,0.740,0.745,0.758,0.922,0.938,0.962,1.522,1.844)
 vi <- c(0.086,0.106,0.055,0.084,0.071,0.078,0.051,0.093,0.149,0.095,0.110,0.054,0.081,0.084,0.087,0.103,0.113,0.083,0.100,0.141)
 my.df <- cbind(yi,vi)
-test <- mxModel("test", mxData(observed=my.df, type="raw"),
-mxMatrix("Zero", ncol=1, nrow=1, free=FALSE, name="A"),
-mxMatrix("Full", ncol=1, nrow=1, free=FALSE, values=0, labels="data.vi", name="V"),
-mxMatrix("Full", ncol=1, nrow=1, free=TRUE, values=0.1, lbound=0.0000001, name="Tau"),
-mxMatrix("Iden", ncol=1, nrow=1, name="F", dimnames=list(c("yi"), c("yi"))),
-mxMatrix("Full", ncol=1, nrow=1, free=T, values=0, name="M"),
-mxAlgebra(V+Tau, name="S"),
-mxRAMObjective("A", "S", "F", "M"))
+test <- mxModel("test", type="raw",
+	mxMatrix("Zero", ncol=1, nrow=1, free=F, name="A"),
+	mxMatrix("Full", ncol=1, nrow=1, free=F, values=0,   labels="data.vi", name="V"),
+	mxMatrix("Full", ncol=1, nrow=1, free=T, values=0.1, lbound=0.0000001, name="Tau"),
+	mxMatrix("Full", ncol=1, nrow=1, free=T, values=0,   name="M"),
+	mxMatrix("Iden", ncol=1, nrow=1, dimnames=list(c("yi"), c("yi")), name="F"),
+	mxAlgebra(V+Tau, name="S"),
+	mxRAMObjective("A", "S", "F", "M"),
+	mxData(observed=my.df, type="raw")
+)
 out <- mxRun(test, suppressWarnings=TRUE)
 omxCheckCloseEnough(mxEval(objective, out), 27.8, 0.01)
