4ef1ce1dec39607c14e0ce2466d58420e7d9ca94
restabrook <restabrook@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2011-05-16 20:48:42 +0000

    Updated JointFIMLRegresionTest.R
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1644 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/failing/JointFIMLRegressionTest.R
+++ models/failing/JointFIMLRegressionTest.R
@@ -1,4 +1,9 @@
+# test of the joint ordinal/continuous fiml optimizer
+# create a single continuous and single ordinal variable
+# test both logistic regression and simple regression
+
 library(OpenMx)
+set.seed(1110221056)
 
 # sample size and threshold
 n <- 500
@@ -9,8 +14,9 @@ x <- rnorm(n)
 z <- .8 * x + rnorm(n, 0, .6)
 y <- rep(0, n)
 y[z>thresh] <- 1
-y <- as.ordered(y)
-testData <- data.frame(x, y)
+#y <- as.ordered(y)
+testData <- data.frame(x, as.ordered(y))
+names(testData) <- c("x", "y")
 
 # make solve(I-A) = I + A matrices
 iaMat1 <- mxMatrix("Full", 2, 2,
@@ -25,7 +31,7 @@ iaMat2 <- mxMatrix("Full", 2, 2,
 		name="IA")
 
 # make a model with just a regression of continuous on binary
-testModel <- mxModel("JointRegressionTest",
+regModel1 <- mxModel("JointSimpleRegressionTest",
 	mxData(testData, "raw"),
 	iaMat1,
 	mxMatrix("Diag", 2, 2,
@@ -41,15 +47,21 @@ testModel <- mxModel("JointRegressionTest",
 		name="thresh"),
 	mxFIMLObjective("cov", "mean", names(testData), thresholds="thresh", threshnames="y")
 	)
+# now a logistic regression
+regModel2 <- mxModel(regModel, iaMat2, name="JointLogisticRegressionTest")
 	
-# run it
-testResults <- mxRun(testModel)
+# run them
+regResults1 <- mxRun(regModel1)
+regResults2 <- mxRun(regModel2)
 
-# uh-oh
-summary(testResults)
+# the summaries
+summary(regResults1)
+summary(regResults2)
 
-# check out the Hessian
-testResults@output
+# check the likelihoods
+omxCheckCloseEnough(regResults1@output$Minus2LogLikelihood, 1789.092, 0.001)
+omxCheckCloseEnough(regResults2@output$Minus2LogLikelihood, 1789.092, 0.001)
 
-# here's the simple regression, which may have a scaling difference
-summary(lm(x~y))
+# check the parameters
+omxCheckCloseEnough(regResults1@output$estimate, c(0.847, 0.282, -0.036, 0.062), 0.001)
+omxCheckCloseEnough(regResults2@output$estimate, c(1.592, 1.001, -0.036, 0.117), 0.001)
