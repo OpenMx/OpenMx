d819a4e40428b4395c9416751c8a9a0b67c482b5
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2011-08-06 18:44:11 +0000

    Backporting bug fix to 1.1 stable branch
    git-svn-id: http://openmx.psyc.virginia.edu/svn/tags/stable-1.1@1745 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ R/MxErrorHandling.R
@@ -0,0 +1,40 @@
+#
+#   Copyright 2007-2011 The OpenMx Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+matchStack <- function(syscall, function_name) {
+    return(identical(syscall[[1]], function_name))
+}
+
+imxLocateFunction <- function(function_name) {
+    callstack <- sys.calls()
+    if (is.null(callstack)) {
+        msg <- paste("(oops) could not find function",
+            function_name)
+        return(msg)
+    }
+    query <- sapply(callstack, matchStack, as.symbol(function_name))
+    matches <- which(query)
+    if (length(matches) == 0) {
+        msg <- paste("(oops) could not find function",
+            function_name)
+        return(msg)
+    } else {
+        firstmatch <- matches[[1]]
+        return(callstack[[firstmatch]])
+    }
+}
+
+
+
