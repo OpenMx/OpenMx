b76c9d8ca29763e53fa234f5e1deeeb327d46160
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2012-02-18 03:10:56 +0000

    Initial commit of regression testing script.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1936 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ inst/tools/regressionTesting.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ "$#" -ne "4" ];
+then
+    echo ""
+    echo "expected arguments: [SVNPATH] [OUTDIR] [START-REV] [END-REV]"
+    echo ""
+    exit
+fi
+
+SVN_ROOT=$1
+OUTPUT_DIR=$2
+START_REVISION=$3
+END_REVISION=$4
+
+cd $SVN_ROOT
+for ((rev=$START_REVISION; rev <= $END_REVISION; rev++))
+do
+#   svn checkout -r $rev
+#   make nightly > performance.results
+   awk '/Results:/ {flag=1}flag' performance.results | tail -n +2 > performance.data
+   cat performance.data | while read line;
+   do
+      set $line
+      echo -e "$rev\t$2" >> $OUTPUT_DIR/$1.results
+   done
+done
+
