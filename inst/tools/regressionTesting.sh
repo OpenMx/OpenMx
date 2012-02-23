01b0d0e4635d70f3aed8b341f7543c6cc5807289
mspiegel <mspiegel@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2012-02-23 01:42:47 +0000

    Fixes to regression testing script.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@1941 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- inst/tools/regressionTesting.sh
+++ inst/tools/regressionTesting.sh
@@ -2,7 +2,7 @@
 if [ "$#" -ne "4" ];
 then
     echo ""
-    echo "expected arguments: [SVNPATH] [OUTDIR] [START-REV] [END-REV]"
+    echo "expected arguments: [NEWDIR] [OUTDIR] [START-REV] [END-REV]"
     echo ""
     exit
 fi
@@ -12,18 +12,20 @@ OUTPUT_DIR=$2
 START_REVISION=$3
 END_REVISION=$4
 
+mkdir -p $SVN_ROOT
+svn checkout -r $START_REVISION http://openmx.psyc.virginia.edu/svn/trunk $SVN_ROOT
 cd $SVN_ROOT
 for ((rev=$START_REVISION; rev <= $END_REVISION; rev++))
 do
-   svn update -r $rev --non-interactive --force
-   svn revert --recursive .
+   svn update -r $rev --non-interactive
    make install
    make nightly > performance.results
-   awk '/Results:/ {flag=1}flag' performance.results | tail -n +2 > performance.data
+   awk '/Runtimes:/ {flag=1}flag' performance.results | tail -n +2 | sed '$d' > performance.data
    cat performance.data | while read line;
    do
       set $line
-      echo -e "$rev\t$2" >> $OUTPUT_DIR/$1.results
+      location=`basename $1`
+      echo -e "$rev\t$2" >> $OUTPUT_DIR/$location.results
    done
 done
-
+rm -rf $SVN_ROOT
