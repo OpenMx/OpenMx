6434477f1ffd0199bfa951d147d9861e2ff8982b
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-03-22 09:55:17 +0000

    Remove a bit more automatic version stamp stuff
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3191 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- inst/tools/findBuildNum.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-v=$(svnversion -c)
-if [ "$v" = 'Unversioned directory' -o "$v" = exported ]; then
-  echo 0  # date -u +'%s' changes too often
-else
-  echo $v | sed -e 's/[MS]//g' -e 's/^[[:digit:]]*://'
-fi
