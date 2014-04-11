4040a5bbf4b6f98cde0e90e1ac86c91edb94d711
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-04-11 18:15:49 +0000

    Update failing test to check for correct answers
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3322 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/failing/SimpleConstraint.R
+++ models/failing/SimpleConstraint.R
@@ -1,21 +1,20 @@
-# valgrind detects problems when run with CSOLNP
-
 library(OpenMx)
+
+#mxOption(NULL, "Default optimizer", "NPSOL")
 
 #Create a constraint between MxMatrices 'A' and 'B'
 constraint <- mxConstraint(A > B, name = 'AdominatesB')
 
 # Constrain matrix 'K' to be equal to matrix 'limit'
-
 model <- mxModel(model="con_test",
                  mxMatrix(type="Full", nrow=2, ncol=2, free=TRUE, name="K"),
-                 mxMatrix(type="Full", nrow=2, ncol=2, free=FALSE, name="limit", values=
-                            1:4),
+                 mxMatrix(type="Full", nrow=2, ncol=2, free=FALSE, name="limit", values=1:4),
                  mxConstraint(K == limit, name = "Klimit_equality"),
                  mxAlgebra(min(K), name="minK"),
                  mxAlgebraObjective("minK")
 )
-## Not run:
 
 fit <- mxRun(model)
-fit$matrices$K$values
+
+omxCheckCloseEnough(fit$output$fit, 1, 1e-4)
+omxCheckCloseEnough(c(fit$matrices$K$values), 1:4, 1e-4)
