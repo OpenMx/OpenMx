9e1b088413545240521a4050c8f8e6884b11680d
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-06-06 13:40:32 +0000

    Warn if not at a subversion known commit
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3534 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- inst/tools/findBuildNum.sh
+++ inst/tools/findBuildNum.sh
@@ -6,6 +6,9 @@ set -o nounset
 v=$(svnversion -c)
 if [ "$v" = 'Unversioned directory' -o "$v" = exported ]; then
   if ! which git >/dev/null; then echo 0; exit; fi
+  if ! git log -1 | grep -q git-svn-id; then
+      echo "WARNING: Subversion does not have all GIT commits in your current branch" >/dev/fd/2
+  fi
   git log | grep git-svn-id | head -n 1 | cut -d '@' -f 2 | cut -d ' ' -f 1
 else
   echo $v | sed -e 's/[MS]//g' -e 's/^[[:digit:]]*://'
