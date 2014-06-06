ef6de1e7019522c35afe139a4df73180d202bb2d
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-06-06 03:39:33 +0000

    Cope when neither SVN nor GIT is installed
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3533 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- inst/tools/findBuildNum.sh
+++ inst/tools/findBuildNum.sh
@@ -5,6 +5,7 @@ set -o nounset
 
 v=$(svnversion -c)
 if [ "$v" = 'Unversioned directory' -o "$v" = exported ]; then
+  if ! which git >/dev/null; then echo 0; exit; fi
   git log | grep git-svn-id | head -n 1 | cut -d '@' -f 2 | cut -d ' ' -f 1
 else
   echo $v | sed -e 's/[MS]//g' -e 's/^[[:digit:]]*://'
