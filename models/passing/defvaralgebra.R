b82dd034721e41f5d2b51c571e71f99304823291
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-07-08 03:59:28 +0000

    Updated expected error for "better error when definition variable is missing"
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3623 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/passing/defvaralgebra.R
+++ models/passing/defvaralgebra.R
@@ -9,7 +9,7 @@ omxCheckEquals(mxEval(A[1,1], model, compute=FALSE), 0)
 omxCheckError(mxEval(A[1,1], model, compute=TRUE, defvar.row = 0),
 	paste("Row number '0' is out of",
 	"bounds for definition variable",
-	"'data.foo' used in the context of 'A'"))
+	"'data.foo' used in the context of 'A'\nOne possibility is you didn't add this variable to the data for this group?"))
 omxCheckEquals(mxEval(A[1,1], model, compute=TRUE), 1)
 omxCheckEquals(mxEval(A[1,1], model, compute=TRUE, defvar.row=2), 2)
 omxCheckEquals(mxEval(A[1,1], model, compute=TRUE, defvar.row=3), 3)
@@ -18,7 +18,7 @@ omxCheckEquals(mxEval(A[1,1], model, compute=TRUE, defvar.row=5), 5)
 omxCheckError(mxEval(A[1,1], model, compute=TRUE, defvar.row = 6),
 	paste("Row number '6' is out of",
 	"bounds for definition variable",
-	"'data.foo' used in the context of 'A'"))
+	"'data.foo' used in the context of 'A'\nOne possibility is you didn't add this variable to the data for this group?"))
 
 omxCheckEquals(mxEval(B[1,1], model, compute=TRUE), 2)
 omxCheckEquals(mxEval(B[1,1], model, compute=TRUE, defvar.row=2), 4)
@@ -28,8 +28,8 @@ omxCheckEquals(mxEval(B[1,1], model, compute=TRUE, defvar.row=5), 10)
 omxCheckError(mxEval(B[1,1], model, compute=TRUE, defvar.row = 0),
 	paste("Row number '0' is out of",
 	"bounds for definition variable",
-	"'data.foo' used in the context of 'B'"))
+	"'data.foo' used in the context of 'B'\nOne possibility is you didn't add this variable to the data for this group?"))
 omxCheckError(mxEval(B[1,1], model, compute=TRUE, defvar.row = 6),
 	paste("Row number '6' is out of",
 	"bounds for definition variable",
-	"'data.foo' used in the context of 'B'"))
+	"'data.foo' used in the context of 'B'\nOne possibility is you didn't add this variable to the data for this group?"))
