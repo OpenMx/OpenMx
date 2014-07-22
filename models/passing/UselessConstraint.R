b0189f8d3370d034ba486d5fd46f3697c295bda0
mzahery <mzahery@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-07-22 20:56:57 +0000

    Previously failing tests are moved into passing
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3665 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ models/passing/UselessConstraint.R
@@ -0,0 +1,26 @@
+# http://openmx.psyc.virginia.edu/issue/2014/05/memory-leak-when-running-ram-model-constraint
+
+library(OpenMx)
+
+mxOption(NULL, "Default optimizer", "CSOLNP") #works fine on NPSOL
+
+data(demoOneFactor)
+manifests <- names(demoOneFactor)
+latents <- c("G")
+factorModelPath <- mxModel("OneFactorPath",
+                           type="RAM",
+                           manifestVars = manifests,
+                           latentVars = latents,
+                           mxPath(from=latents, to=manifests,
+                                  labels=paste("l",1:5,sep="")),
+                           mxPath(from=manifests, arrows=2),
+                           mxPath(from=latents, arrows=2,
+                                  free=FALSE, values=1.0),
+                           mxData(cov(demoOneFactor), type="cov",
+                                  numObs=500),
+                           mxAlgebra(S[6,6],name="GV"),
+                           mxConstraint(GV-1==0,name="pointless"))
+#factorModelPath <- mxOption(factorModelPath,"Checkpoint Directory","C:/Work/OpenMx_dev/")
+#factorModelPath <- mxOption(factorModelPath,"Checkpoint Units","evaluations")
+#factorModelPath <- mxOption(factorModelPath,"Checkpoint Count",1)
+factorFit <- mxRun(factorModelPath)
