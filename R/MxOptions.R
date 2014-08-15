044a48d9109a8aa8fd8c409c0168ca8c2d589523
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-08-15 13:40:17 +0000

    Switch default optimizer back to CSOLNP
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3751 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- R/MxOptions.R
+++ R/MxOptions.R
@@ -106,7 +106,7 @@ determineDefaultOptimizer <- function() {
 	engine <- Sys.getenv("IMX_OPT_ENGINE")
 	if (!nchar(engine)) {
 		if (imxHasNPSOL()) {
-			engine <- "NPSOL"
+			engine <- "CSOLNP"
 		} else {
 			engine <- "CSOLNP"
             #engine <- "NLOPT"
