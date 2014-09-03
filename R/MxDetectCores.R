e9ed5c1ed979ac16711c896689b51f0878a93c48
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-09-03 13:43:58 +0000

    Depend on package parallel (in R core since 2.14)
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3788 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- R/MxDetectCores.R
+++ R/MxDetectCores.R
@@ -13,30 +13,4 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# Taken from the package "parallel" available in R >= 2.14.0
-omxDetectCores <- memoize(function(all.tests = FALSE, logical = FALSE) {
-	if("package:parallel" %in% search()) {
-		return(detectCores(all.tests, logical))
-	} else if(.Platform$OS.type == "windows") {        
-            return(1L)
-    } else {
-		systems <-
-			list(darwin = "/usr/sbin/sysctl -n hw.ncpu 2>/dev/null",
-			freebsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
-			linux = "grep processor /proc/cpuinfo 2>/dev/null | wc -l",
-			irix  = c("hinv | grep Processors | sed 's: .*::'",
-						"hinv | grep '^Processor '| wc -l"),
-			solaris = if(logical) "/usr/sbin/psrinfo -v | grep 'Status of.*processor' | wc -l" else "/usr/sbin/psrinfo -p")
-		for (i in seq(systems)) {
-        	if(all.tests ||
-            	length(grep(paste("^", names(systems)[i], sep=''),
-                               R.version$os))) {
-                for (cmd in systems[i]) {
-                	a <- gsub("^ +","", system(cmd, TRUE)[1])
-                    if (length(grep("^[1-9]", a))) return(as.integer(a))
-                }
-			}
-		}
-        return(NA_integer_)
-    }
-})
+omxDetectCores <- memoize(detectCores)
