fe25f2ab315ccf282cc26aedfabc79616b883441
jpritikin <jpritikin@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2014-11-04 17:27:02 +0000

    Document that mxLog will fail in Rgui on Windows
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@3970 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/passing/mxLog.R
+++ models/passing/mxLog.R
@@ -1,4 +1,9 @@
 library(OpenMx)
 
+# Rgui on Windows does not support stdout/stderr
+#
+# http://cran.r-project.org/bin/windows/base/rw-FAQ.html
+# See "8.6 The output from my C code disappears. Why?"
+
 # This should not result in an error
 omxCheckTrue(imxLog("test"))
