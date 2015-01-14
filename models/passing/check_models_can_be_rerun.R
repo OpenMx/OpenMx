3ae569d3eff22d018eada234edd6a35483057d7d
tbates <tbates@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2015-01-14 17:35:20 +0000

    check regression for bug where models cant be re-run
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@4149 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

---
+++ models/passing/check_models_can_be_rerun.R
@@ -0,0 +1,16 @@
+# At one point we had a bug such that models could not be re-run without generating an error
+# This tests that we don't get a regression on that bug
+
+data(myFADataRaw, package="OpenMx")
+manifests = paste("x", 1:3, sep="")
+latents   = c("G")
+m1 <- mxModel("m1", type="RAM",
+	manifestVars = manifests, latentVars   = latents,
+	mxPath(from = latents, to = manifests),
+	mxPath(from = manifests, arrows = 2), # manifest residuals 
+	mxPath(from = latents, arrows = 2, free = FALSE, values = 1), # latent fixed@1
+	mxPath(from = "one", to = manifests, arrows = 1), # manifest means
+	mxData(myFADataRaw[1:100, manifests], type = "raw")
+)
+m1 = mxRun(m1)
+m1 = mxRun(m1)
