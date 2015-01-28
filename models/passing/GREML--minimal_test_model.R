adc7d127d391db936ca568a1b59059e624269e7f
rkirkpatrick <rkirkpatrick@df83fd30-4cca-4c36-ab9d-e8b5583ccbd1>
2015-01-28 18:10:07 +0000

    GREML test script should make sure the model runs both with and without analytic derivatives.
    git-svn-id: http://openmx.psyc.virginia.edu/svn/trunk@4198 df83fd30-4cca-4c36-ab9d-e8b5583ccbd1

--- models/passing/GREML--minimal_test_model.R
+++ models/passing/GREML--minimal_test_model.R
@@ -4,7 +4,7 @@ set.seed(1234)
 dat <- cbind(rnorm(100),rep(1,100))
 colnames(dat) <- c("y","x")
 
-ge <- mxExpectationGREML(V="V",X="X",y="y",dV=c(ve="I"))
+ge <- mxExpectationGREML(V="V",X="X",y="y",dV=c(ve="I"),fixedEffects=T)
 gff <- mxFitFunctionGREML()
 plan <- mxComputeSequence(steps=list(
   mxComputeNewtonRaphson(freeSet=c("Ve"),fitfunction="fitfunction"),
@@ -17,9 +17,9 @@ testmod <- mxModel(
   "GREMLtest",
   mxData(observed = matrix(as.double(NA),1,1,dimnames = list("asdf","asdf")), type="raw"),
   mxMatrix(type = "Full", nrow = 1, ncol=1, free=T, values = 2, labels = "ve", lbound = 0.0001, name = "Ve"),
-  mxMatrix("Iden",nrow=100,name="I"),
-  mxMatrix(type = "Full", nrow=100, ncol=1, free=F, values=dat[,2], name = "X"),
-  mxMatrix(type = "Full", nrow=100, ncol=1, free=F, values=dat[,1], name="y"),
+  mxMatrix("Iden",nrow=100,name="I",condenseSlots=T),
+  mxMatrix(type = "Full", nrow=100, ncol=1, free=F, values=dat[,2], name = "X",condenseSlots=T),
+  mxMatrix(type = "Full", nrow=100, ncol=1, free=F, values=dat[,1], name="y",condenseSlots=T),
   mxAlgebra(I %x% Ve,name="V"),
   ge,
   gff,
@@ -32,3 +32,25 @@ omxCheckCloseEnough(testrun$output$estimate[1],var(dat[,1]),epsilon=10^-5)
 omxCheckCloseEnough(testrun$fitfunction$info$b,mean(dat[,1]),epsilon=10^-5)
 omxCheckCloseEnough(testrun$fitfunction$info$bcov,var(dat[,1])/100,epsilon=10^-5)
 omxCheckCloseEnough(testrun$output$standardErrors[1],sqrt((2*testrun$output$estimate^2)/100),epsilon=10^-3)
+
+
+
+ge2 <- mxExpectationGREML(V="V",X="X",y="y",fixedEffects=T)
+
+testmod2 <- mxModel(
+  "GREMLtest",
+  mxData(observed = matrix(as.double(NA),1,1,dimnames = list("asdf","asdf")), type="raw"),
+  mxMatrix(type = "Full", nrow = 1, ncol=1, free=T, values = 2, labels = "ve", lbound = 0.0001, name = "Ve"),
+  mxMatrix("Iden",nrow=100,name="I",condenseSlots=T),
+  mxMatrix(type = "Full", nrow=100, ncol=1, free=F, values=dat[,2], name = "X",condenseSlots=T),
+  mxMatrix(type = "Full", nrow=100, ncol=1, free=F, values=dat[,1], name="y",condenseSlots=T),
+  mxAlgebra(I %x% Ve,name="V"),
+  ge,
+  gff
+)
+
+testrun2 <- mxRun(testmod2)
+
+omxCheckCloseEnough(testrun2$output$estimate[1],var(dat[,1]),epsilon=10^-5)
+omxCheckCloseEnough(testrun2$fitfunction$info$b,mean(dat[,1]),epsilon=10^-5)
+omxCheckCloseEnough(testrun2$fitfunction$info$bcov,var(dat[,1])/100,epsilon=10^-5)
