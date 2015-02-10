b61616628d42cffd71897b8b9f16c5c537b08fba
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2015-02-10 16:01:04 -0500

    Delete unused test from 2010

--- tests/testMxMatrix.R
+++
@@ -1,6 +0,0 @@
-library(OpenMx)
-
-symmMatrix <- mxMatrix("Symm", nrow = 3, ncol = 3)
-symmMatrix@values[1,2] <- 2
-symmMatrix@values[2,1] <- 2 # now required explicitly
-omxCheckIdentical(symmMatrix[2,1], symmMatrix[1,2])
