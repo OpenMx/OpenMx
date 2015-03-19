a52f84da0ee47f3e872bc7ef2cf75d0aad29cfa6
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2015-03-19 11:42:47 -0400

    Simplify version number determination

--- inst/tools/findBuildNum.sh
+++ inst/tools/findBuildNum.sh
@@ -3,11 +3,8 @@
 set -o errexit
 set -o nounset
 
-v=$(svnversion -c)
-if [ "$v" = 'Unversioned directory' -o "$v" = exported ]; then
-  if ! which git >/dev/null; then echo 0; exit; fi
-  git describe | sed -e 's/^v//' -e 's/-[^-]*$//'
+if ! which git >/dev/null; then
+  echo 2.0
 else
-  # The 1.0 is just a fake placeholder
-  echo 1.0-$v | sed -e 's/[MS]//g' -e 's/[[:digit:]]*://'
+  git describe | sed -e 's/^v//' -e 's/-[^-]*$//'
 fi
