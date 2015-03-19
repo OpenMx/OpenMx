8def55d60b813e2dec4388d3a8066187484cfaf0
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2015-03-19 11:53:53 -0400

    Add Richardson extrapolation variant of finite differences

--- src/finiteDifferences.h
+++ src/finiteDifferences.h
@@ -35,4 +35,34 @@ void fd_jacobian(T1 ff, Eigen::MatrixBase<T2> &point, Eigen::MatrixBase<T3> &ref
 	}
 }
 
+template <typename T1, typename T2, typename T3>
+void re_gradient(T1 ff, Eigen::MatrixBase<T2> &point, Eigen::MatrixBase<T3> &gradOut)
+{
+        const double eps = 1e-4;
+	const int numIter = 3;
+
+	Eigen::VectorXd p2;
+	for (int px=0; px < int(point.size()); ++px) {
+		double offset = std::max(fabs(point[px] * eps), eps);
+		double Gaprox[numIter];
+		for(int k = 0; k < numIter; k++) {
+			p2 = point;
+			p2[px] += offset;
+			double f1 = ff(p2);
+			p2 = point;
+			p2[px] -= offset;
+			double f2 = ff(p2);
+			Gaprox[k] = (f1 - f2) / (2.0 * offset);
+			offset *= .5;
+		}
+		for(int m = 1; m < numIter; m++) {						// Richardson Step
+			for(int k = 0; k < (numIter - m); k++) {
+				// NumDeriv Hard-wires 4s for r here. Why?
+				Gaprox[k] = (Gaprox[k+1] * pow(4.0, m) - Gaprox[k])/(pow(4.0, m)-1);
+			}
+		}
+		gradOut[px] = Gaprox[0];
+	}
+}
+
 #endif
