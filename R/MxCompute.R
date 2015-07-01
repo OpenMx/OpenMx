167506245d8bda3381ff8238c1ec2d20cff5ae5d
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2015-07-01 12:03:52 +0530

    Pretty print MxComputeSequence steps

--- R/MxCompute.R
+++ R/MxCompute.R
@@ -1283,8 +1283,10 @@ setMethod("displayCompute", signature(Ob="MxComputeSequence", indent="integer"),
 		  callNextMethod();
 		  sp <- paste(rep('  ', indent), collapse="")
 		  cat(sp, "independent :", Ob@independent, '\n')
+		  stepName <- paste("'", names(Ob@steps), "'",sep='')
+		  if (length(stepName) != length(Ob@steps)) stepName <- 1:length(Ob@steps)
 		  if (length(Ob@steps)) for (step in 1:length(Ob@steps)) {
-			  cat(sp, "steps[[", step, "]] :", '\n')
+			  cat(sp, "steps[[", stepName[step], "]] :", '\n')
 			  displayCompute(Ob@steps[[step]], indent+1L)
 		  }
 		  invisible(Ob)
