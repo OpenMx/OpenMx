13dcafd4c785848550a4a40c4c6973c19444df13
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2016-01-24 14:55:27 -0500

    Puzzle over test

--- inst/models/passing/multilevelLatentRegression2.R
+++ inst/models/passing/multilevelLatentRegression2.R
@@ -2,7 +2,7 @@ library(OpenMx)
 
 set.seed(1)
 
-numIndicators <- 3
+numIndicators <- 4
 
 numDistricts <- 2
 numSchools <- 2
@@ -28,7 +28,7 @@ studentData <- genData(teacherData, numStudents, 'studentID')
 
 createIndicators <- function(latentSkill, indicatorVariance) {
 	if (missing(indicatorVariance)) {
-		indicatorVariance <- rlnorm(numIndicators) / 8
+		indicatorVariance <- rep(1, numIndicators) #rlnorm(numIndicators) / 8
 	}
 	ind <- matrix(NA, length(latentSkill), length(indicatorVariance))
 	for (ix in 1:length(latentSkill)) {
@@ -36,7 +36,7 @@ createIndicators <- function(latentSkill, indicatorVariance) {
 				   function(sd) rnorm(1, mean=latentSkill[ix], sd=sd))
 	}
 	# per indicator mean
-#	ind <- t(t(ind) + runif(length(indicatorVariance),min=-1,max=1))
+#	ind <- t(t(ind) + runif(numIndicators,min=-1,max=1))
 	colnames(ind) <- paste0('i', 1:length(indicatorVariance))
 	as.data.frame(ind)
 }
@@ -46,21 +46,25 @@ schoolData <- cbind(schoolData, createIndicators(schoolData$skill))
 teacherData <- cbind(teacherData, createIndicators(teacherData$skill))
 studentData <- cbind(studentData, createIndicators(studentData$skill))
 
-#studentData$i4[runif(nrow(studentData)) > .8] <- NA
-#teacherData$i4[runif(nrow(teacherData)) > .8] <- NA
+studentData$i4[runif(nrow(studentData)) > .8] <- NA
+teacherData$i4[runif(nrow(teacherData)) > .8] <- NA
 
 mkSingleFactor <- function(latent=c()) {
 	mxModel('template', type='RAM',
 		manifestVars = paste0('i', 1:numIndicators),
 		latentVars = c("skill",latent),
 		mxPath('skill', arrows=2, labels="Var", values=rlnorm(1), lbound=.01),
-		mxPath(paste0('i',1:numIndicators), arrows=2,
-		       values=rlnorm(1), labels="Err", lbound=.01),
+#		mxPath(paste0('i',1:numIndicators), arrows=2,
+#		       values=rlnorm(1), labels="Err", lbound=.01),
+		mxPath(paste0('i',1:numIndicators), arrows=2, values=1, free=FALSE),
 		mxPath("one", paste0('i',1:numIndicators), free=FALSE, values=0),
 		mxPath('skill', paste0('i',1:numIndicators),
-		       labels=paste0('L',1:numIndicators), lbound=0,
-		       values=runif(numIndicators-1, .5,1.5),
-		       free=c(FALSE, rep(TRUE,numIndicators-1))))
+		       values=1, free=FALSE)
+#		mxPath('skill', paste0('i',1:numIndicators),
+#		       labels=paste0('L',1:numIndicators), lbound=0,
+#		       values=runif(numIndicators-1, .5,1.5),
+#		       free=c(FALSE, rep(TRUE,numIndicators-1)))
+		)
 }
 
 singleFactor <- mkSingleFactor(NULL)
@@ -81,9 +85,75 @@ schMod <- mxModel(relabel(mkSingleFactor(), "school"), dMod,
 		  mxData(type="raw", observed=schoolData, primaryKey="schoolID", sort=FALSE),
 		  mxPath('district.skill', 'skill', joinKey="districtID", values=runif(1)))
 
+if (1) {
+schMod$expectation$rampart <- 0L
+square <- mxRun(mxModel(schMod,
+		      mxComputeSequence(list(
+			  mxComputeOnce('fitfunction', 'fit'),
+			  mxComputeNumericDeriv(checkGradient=FALSE, hessian=FALSE, iterations=2),
+			  mxComputeReportDeriv()
+		      ))))
+
+schMod$expectation$rampart <- 1L
+rotated <- mxRun(mxModel(schMod,
+		      mxComputeSequence(list(
+			  mxComputeOnce('fitfunction', 'fit'),
+			  mxComputeNumericDeriv(checkGradient=FALSE, hessian=FALSE, iterations=2),
+			  mxComputeReportDeriv(),
+			  mxComputeReportExpectation()
+		      ))))
+
+ex <- rotated$expectation
+eo <- ex$output
+ed <- ex$debug
+print(ed$rampartUsage)
+#print(round(ed$A[1:20,1:20],2))
+#print(round(ed$rA[1:20,1:20],2))
+#print(ed$mean)
+
+#omxCheckCloseEnough(ed$rampartUsage, c(11064L, 317L, 198L, 2L), 1L)
+omxCheckCloseEnough(rotated$output$fit, square$output$fit, 1e-8)
+round(rotated$output$gradient - square$output$gradient, 2)
+omxCheckCloseEnough(rotated$output$gradient, square$output$gradient, 1e-5)
+}
+
+stop("here")
+
 tMod <- mxModel(relabel(singleFactor, "teacher"), schMod,
 		  mxData(type="raw", observed=teacherData, primaryKey="teacherID", sort=FALSE),
 		  mxPath('school.skill', 'skill', joinKey="schoolID", values=runif(1)))
+
+if (1) {
+tMod$expectation$rampart <- 0L
+square <- mxRun(mxModel(tMod,
+		      mxComputeSequence(list(
+			  mxComputeOnce('fitfunction', 'fit'),
+			  mxComputeNumericDeriv(checkGradient=FALSE, hessian=FALSE, iterations=2),
+			  mxComputeReportDeriv()
+		      ))))
+
+tMod$expectation$rampart <- 1L
+rotated <- mxRun(mxModel(tMod,
+		      mxComputeSequence(list(
+			  mxComputeOnce('fitfunction', 'fit'),
+			  mxComputeNumericDeriv(checkGradient=FALSE, hessian=FALSE, iterations=2),
+			  mxComputeReportDeriv(),
+			  mxComputeReportExpectation()
+		      ))))
+
+ex <- rotated$expectation
+eo <- ex$output
+ed <- ex$debug
+print(ed$rampartUsage)
+print(round(ed$A[1:20,1:20],2))
+print(round(ed$rA[1:20,1:20],2))
+#print(ed$mean)
+
+#omxCheckCloseEnough(ed$rampartUsage, c(11064L, 317L, 198L, 2L), 1L)
+omxCheckCloseEnough(rotated$output$fit, square$output$fit, 1e-8)
+round(rotated$output$gradient - square$output$gradient, 2)
+omxCheckCloseEnough(rotated$output$gradient, square$output$gradient, 1e-5)
+}
 
 sMod <- mxModel(relabel(singleFactor, "student"), tMod,
 		  mxData(type="raw", observed=studentData, primaryKey="studentID", sort=FALSE),
@@ -119,7 +189,6 @@ print(round(ed$rA[1:20,1:20],2))
 omxCheckCloseEnough(rotated$output$fit, square$output$fit, 1e-8)
 round(rotated$output$gradient - square$output$gradient, 2)
 omxCheckCloseEnough(rotated$output$gradient, square$output$gradient, 1e-5)
-omxCheckCloseEnough(rotated$output$hessian, square$output$hessian, 1e-1)
 }
 
 
