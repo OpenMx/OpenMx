50a597b19cd09ba4eef699e467afb382b2e9d922
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2016-01-24 16:25:22 -0500

    Add another lmer example

---
+++ inst/models/passing/lmer-2.R
@@ -0,0 +1,61 @@
+libraries <- rownames(installed.packages())
+if (!all(c("lme4","nlme") %in% libraries)) stop("SKIP")
+
+library(lme4)
+data(Orthodont, package="nlme")
+Orthodont$nsex <- as.numeric(Orthodont$Sex=="Male")
+Orthodont$nsexage <- with(Orthodont, nsex*age)
+fm1 <- lmer(distance ~ age + (age|Subject) + (0+nsex|Subject) +
+                (0 + nsexage|Subject), data=Orthodont, REML=FALSE)
+
+library(OpenMx)
+
+if (is.factor(Orthodont$Subject)) {
+    Orthodont$Subject <- as.integer(unclass(Orthodont$Subject))
+}
+
+bySubj <- mxModel(model="subj", type="RAM",
+                  latentVars = c('intercept', paste0(c("age", 'nsex', "nsexage"), "L")),
+                  mxData(data.frame(Subject=unique(Orthodont$Subject)),
+                                    type="raw", primaryKey="Subject"),
+                  mxPath(c('intercept', 'ageL'), c('intercept', 'ageL'),
+                         arrows=2, "unique.pairs", values=c(1,.1,1), labels=c('subjInt', 'subjIntAge', 'subjAge')),
+                  mxPath(c('nsexL', 'nsexageL'), arrows=2, values=1))
+
+ortho <- mxModel(model="ortho", bySubj, type="RAM", manifestVars=c("distance"),
+              latentVars = c("ageL"),
+              mxData(type="raw", observed=Orthodont[,c('distance', 'age', 'Subject', 'nsex', "nsexage")], sort = FALSE),
+              mxPath(c("one"), "distance"),
+              mxPath(c("one"), "ageL", free=FALSE, labels="data.age"),
+              mxPath("ageL", "distance"),
+              mxPath("distance", arrows=2, values=1),
+              mxPath("subj.intercept", "distance", values=1, free=FALSE, joinKey="Subject"),
+              mxPath("subj.ageL", "distance", labels="data.age", free=FALSE, joinKey="Subject"),
+              mxPath("subj.nsexL", "distance", labels="data.nsex", free=FALSE, joinKey="Subject"),
+              mxPath("subj.nsexageL", "distance", labels="data.nsexage", free=FALSE, joinKey="Subject"))
+
+if (1) {
+  # load lme4's parameters
+    p1 <- ortho
+    p1$subj$S$values[c('intercept', 'ageL'),c('intercept', 'ageL')] <- 
+        VarCorr(fm1)$Subject
+    p1$subj$S$values[c('nsexL'),c('nsexL')] <- 
+        VarCorr(fm1)$Subject.1
+    p1$subj$S$values[c('nsexageL'),c('nsexageL')] <- 
+        VarCorr(fm1)$Subject.2
+
+    p1$A$values['distance','ageL'] <- fixef(fm1)['age']
+    p1$M$values[,'distance'] <- fixef(fm1)['(Intercept)']
+    p1$S$values['distance','distance'] <- getME(fm1, "sigma")^2
+
+    pt1 <- mxRun(mxModel(p1, mxComputeSequence(list(
+        mxComputeOnce('fitfunction', 'fit'),
+        mxComputeReportExpectation()))))
+
+    omxCheckCloseEnough(logLik(pt1), logLik(fm1), 1e-6)
+}
+
+ortho <- mxRun(ortho)
+
+# OpenMx finds a better solution
+omxCheckCloseEnough(ortho$output$fit, 436.73, 1e-2)
