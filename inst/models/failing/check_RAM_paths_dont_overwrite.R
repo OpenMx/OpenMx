067bdb5dc87c66ee36766e30eb3305dddc398525
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2016-03-15 09:43:28 -0400

    Record Steve's comment from the mailing list

--- inst/models/failing/check_RAM_paths_dont_overwrite.R
+++ inst/models/failing/check_RAM_paths_dont_overwrite.R
@@ -1,3 +1,15 @@
+## This behavior is purposeful.  We made a design decision to not allow mxPath 
+## to create simultaneous single headed and double headed arrows in the same 
+## position in the A and S matrices.  The rare (really, are there any?) cases 
+## when this makes sense are far outweighed by the times when people are either 
+## (a) making a mistake or (b) trying to convert a single headed into a double 
+## headed arrow or vice versa.  Of course, this behavior can be overridden by 
+## directly manipulating an mxMatrix.  We did (and I do still) consider mxPath() 
+## to be the training wheels for OpenMx.  The idea being we would like to make 
+## it difficult to make mistakes.  I believe this philosophy is still correct.
+##
+## https://lists.virginia.edu/sympa/arc/openmx-developers/2016-03/msg00085.html
+
 # "~/bin/OpenMx/inst/models/failing/check_RAM_paths_dont_overwrite.R"
 
 # When adding paths in mxModel, I think everything the user requests should be added
