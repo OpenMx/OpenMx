3233337caafd9e9bddbbd06903ccebdc0ac391ac
RMKirkpatrick <rkirkpatrick2@vcu.edu>
2016-10-28 11:56:55 -0400

    Add two more MxModels to analytic-Jacobians test script.

--- inst/models/passing/AnalyticConstraintJacobians--PowellBenchmark.R
+++ inst/models/passing/AnalyticConstraintJacobians--PowellBenchmark.R
@@ -196,3 +196,63 @@ omxCheckEquals(powellrun4$output$iterations,powellrun5$output$iterations)
 omxCheckEquals(powellrun4$output$evaluations,powellrun5$output$evaluations)
 omxCheckCloseEnough(coef(powellrun4),coef(powellrun5))
 summary(powellrun5)
+
+
+#For the sake of comparison, this model uses Jacobians but no gradient:
+powellmod6 <- mxModel(
+	"PowellBenchmarkWithJacobiansButNoGradient",
+	mxMatrix(type="Full",nrow=1,ncol=5,free=T,values=c(-2,2,2,-1,-1),labels=paste("x",1:5,sep=""),name="X"),
+	mxAlgebra( exp(prod(X)), name="powellfunc"),
+	# mxAlgebra( cbind(powellfunc*X[1,2]*X[1,3]*X[1,4]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,3]*X[1,4]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,2]*X[1,4]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,2]*X[1,3]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,2]*X[1,3]*X[1,4]), 
+	# 					 name="objgrad", 
+	# 					 dimnames=list(NULL,paste("x",1:5,sep="")) ),
+	mxConstraint(sum(X%^%2) - 10 == 0, name="c1",jac="jac1"),
+	mxConstraint(X[1,2]*X[1,3]-5*X[1,4]*X[1,5] == 0, name="c2",jac="jac2" ),
+	mxConstraint(X[1,1]^3 + X[1,2]^3 + 1 == 0, name="c3",jac="jac3" ),
+	mxAlgebra(cbind(2*X[1,1],2*X[1,2],2*X[1,3],2*X[1,4],2*X[1,5]),name="jac1",
+						dimnames=list(NULL,paste("x",1:5,sep=""))
+	),
+	mxAlgebra(cbind(0,X[1,3],X[1,2],-5*X[1,5],-5*X[1,4]),name="jac2",
+						dimnames=list(NULL,paste("x",1:5,sep=""))),
+	mxAlgebra(cbind(3*X[1,1]^2, 3*X[1,2]^2, 0, 0, 0),name="jac3",dimnames=list(NULL,paste("x",1:5,sep=""))),
+	mxFitFunctionAlgebra(algebra="powellfunc")
+)
+# powellmod6 <- mxRun(powellmod6,onlyFrontend=T)
+# powellmod6$compute$steps$GD$verbose <- 3L
+# powellmod6$compute$.persist <- TRUE
+powellrun6 <- mxRun(powellmod6)
+powellrun6$fitfunction$result
+powellrun6$output$iterations
+powellrun6$output$evaluations
+summary(powellrun6)
+
+
+#Finally, this model uses no analytic derivatives at all:
+powellmod7 <- mxModel(
+	"PowellBenchmarkNoJacobians",
+	mxMatrix(type="Full",nrow=1,ncol=5,free=T,values=c(-2,2,2,-1,-1),labels=paste("x",1:5,sep=""),name="X"),
+	mxAlgebra( exp(prod(X)), name="powellfunc"),
+	# mxAlgebra( cbind(powellfunc*X[1,2]*X[1,3]*X[1,4]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,3]*X[1,4]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,2]*X[1,4]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,2]*X[1,3]*X[1,5],
+	# 								 powellfunc*X[1,1]*X[1,2]*X[1,3]*X[1,4]), 
+	# 					 name="objgrad", 
+	# 					 dimnames=list(NULL,paste("x",1:5,sep="")) ),
+	mxConstraint(sum(X%^%2) - 10 == 0, name="c1"),
+	mxConstraint(X[1,2]*X[1,3]-5*X[1,4]*X[1,5] == 0, name="c2"),
+	mxConstraint(X[1,1]^3 + X[1,2]^3 + 1 == 0, name="c3"),
+	mxFitFunctionAlgebra(algebra="powellfunc")
+)
+# powellmod7 <- mxRun(powellmod7,onlyFrontend=T)
+# powellmod7$compute$steps$GD$verbose <- 3L
+# powellmod7$compute$.persist <- TRUE
+powellrun7 <- mxRun(powellmod7)
+powellrun7$fitfunction$result
+powellrun7$output$iterations
+powellrun7$output$evaluations
+summary(powellrun7)
