f5cfbdeed23d867cbfd4f7a96e93692c75a4d4ee
Rob <rkirkpatrick2@vcu.edu>
2016-11-15 14:35:26 -0500

    Add a test for NPSOL's "warm start" feature.

---
+++ inst/models/passing/WarmStartTest.R
@@ -0,0 +1,43 @@
+require(OpenMx)
+
+if (mxOption(NULL,"Default optimizer")!='NPSOL') stop("SKIP")
+
+set.seed(1611150)
+x <- matrix(rnorm(1000))
+colnames(x) <- "x"
+
+m1 <- mxModel(
+	"m1",
+	mxData(observed=x,type="raw"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=0,labels="mu",name="Mu"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=1,labels="sigma2",name="Sigma",lbound=0),
+	mxExpectationNormal(covariance="Sigma",means="Mu",dimnames=c("x")),
+	mxFitFunctionML()
+)
+run1 <- mxRun(m1)
+
+hess.ini <- matrix(-2*c(-1000/1,0,0,-1000/2),nrow=2,ncol=2)
+ws <- chol(hess.ini)
+
+plan <- mxComputeSequence(steps=list(
+	mxComputeGradientDescent(engine="NPSOL",warmStart=ws),
+	mxComputeNumericDeriv(),
+	mxComputeStandardError(),
+	mxComputeHessianQuality(),
+	mxComputeReportDeriv(),
+	mxComputeReportExpectation()
+))
+
+m2 <- mxModel(
+	"m2",
+	plan,
+	mxData(observed=x,type="raw"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=0,labels="mu",name="Mu"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=1,labels="sigma2",name="Sigma",lbound=0),
+	mxExpectationNormal(covariance="Sigma",means="Mu",dimnames=c("x")),
+	mxFitFunctionML()
+)
+run2 <- mxRun(m2)
+
+if(run2$output$evaluations > run1$output$evaluations){stop("WarmStartTest.R failed")}
+
