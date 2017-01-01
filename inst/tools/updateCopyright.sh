d32d3258e089b98d0b95c852a46be2e5198ad6c9
mhunter1 <mhunter.ou@gmail.com>
2017-01-01 10:00:08 -0600

    Update copyright script to 2017

--- inst/tools/updateCopyright.sh
+++ inst/tools/updateCopyright.sh
@@ -2,28 +2,28 @@
 
 cd ./src;
 echo "Update src header files";
-find . -name "*.h" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.h" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update src cpp files";
-find . -name "*.cpp" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.cpp" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update R files";
 cd ../R;
-find . -name "*.R" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.R" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update man pages";
 cd ../man
-find . -name "*.Rd" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.Rd" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update demo R files";
 cd ../demo
-find . -name "*.R" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.R" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update models/enormous files";
 cd ../inst/models/enormous;
-find . -name "*.R" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.R" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update models/failing files";
 cd ../failing;
-find . -name "*.R" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.R" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update models/nightly files";
 cd ../nightly;
-find . -name "*.R" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.R" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 echo "Update models/passing files";
 cd ../passing;
-find . -name "*.R" -exec sed -i "s/Copyright 2007-2015 The OpenMx/Copyright 2007-2016 The OpenMx/g" '{}' \;
+find . -name "*.R" -exec sed -i "s/Copyright 2007-2016 The OpenMx/Copyright 2007-2017 The OpenMx/g" '{}' \;
 cd ../../..;
