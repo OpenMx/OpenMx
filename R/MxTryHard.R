6c5b8389a56ee68a65cd5eb9daeca96bc5a91121
RMKirkpatrick <rkirkpatrick2@vcu.edu>
2017-01-17 16:55:07 -0500

    Re-indent mxTryHard() signature (whitespace changes only).

--- R/MxTryHard.R
+++ R/MxTryHard.R
@@ -18,7 +18,7 @@
 #How can mxTryHard() be improved for ordinal-threshold analyses?
 
 mxTryHard <- function(model, extraTries = 10, greenOK = FALSE, loc = 1, 
-		      scale = 0.25,  initialGradientStepSize = imxAutoOptionValue("Gradient step size"), 
+											scale = 0.25,  initialGradientStepSize = imxAutoOptionValue("Gradient step size"), 
 											initialGradientIterations = imxAutoOptionValue('Gradient iterations'),
 											initialTolerance=as.numeric(mxOption(NULL,'Optimality tolerance')), 
 											checkHess = TRUE, fit2beat = Inf, paste = TRUE,
