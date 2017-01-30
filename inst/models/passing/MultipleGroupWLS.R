5fb2f686fc0ef9ebe1304e47fa85f5f90e54492b
mhunter1 <mhunter.ou@gmail.com>
2017-01-29 22:56:48 -0600

    Test multigroup autostarts

--- inst/models/passing/MultipleGroupWLS.R
+++ inst/models/passing/MultipleGroupWLS.R
@@ -26,9 +26,9 @@ wdata <- mxDataWLS(tmpFrame)
 
 # Define the matrices
 
-S <- mxMatrix(type = "Full", nrow = 2, ncol = 2, values=c(1,0,0,1), 
+S <- mxMatrix(type = "Full", nrow = 2, ncol = 2, values=c(1,0,0,1),
               free=c(TRUE,FALSE,FALSE,TRUE), labels=c("Vx", NA, NA, "Vy"), name = "S")
-A <- mxMatrix(type = "Full", nrow = 2, ncol = 2, values=c(0,1,0,0), 
+A <- mxMatrix(type = "Full", nrow = 2, ncol = 2, values=c(0,1,0,0),
               free=c(FALSE,TRUE,FALSE,FALSE), labels=c(NA, "b", NA, NA), name = "A")
 I <- mxMatrix(type="Iden", nrow=2, ncol=2, name="I")
 
@@ -43,7 +43,7 @@ fitFunction <- mxFitFunctionWLS()
 
 # Define the model
 
-tmpModel <- mxModel(model="exampleModel", S, A, I, expCov, expFunction, fitFunction, 
+tmpModel <- mxModel(model="exampleModel", S, A, I, expCov, expFunction, fitFunction,
                     wdata)
 
 # Fit the model and print a summary
@@ -54,3 +54,17 @@ summary(tmpModelOut)
 tmpModel2 <- mxModel(tmpModel,name="tmp2")
 twoGroup <- mxModel("two", tmpModel, tmpModel2, mxFitFunctionMultigroup(c("exampleModel","tmp2")))
 twoGroup <- mxRun(twoGroup)
+
+# Experiment with multigroup automatic start values
+autModel1 <- mxModel(tmpModel, name="auto1", mxData(tmpFrame, 'raw'),
+	mxMatrix(name='expMean', nrow=1, ncol=2, free=TRUE),
+	mxExpectationNormal(covariance="expCov", means='expMean', dimnames=tmpNames),
+	mxFitFunctionML())
+autModel2 <- mxModel(autModel1, name="auto2")
+autGroup <- mxModel("group", autModel1, autModel2, mxFitFunctionMultigroup(c("auto1","auto2")))
+autStart <- mxAutoStart(autGroup)
+
+# Starting values from mxAutoStart are close to the final estimates from multigroup WLS
+omxCheckClosEnough(coef(autStart)[names(coef(twoGroup))], coef(twoGroup), 1e-3)
+
+
