bfbd873012ea541e54da3a8c517874d5c57039fc
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2017-02-21 15:58:19 -0500

    Exclude more builtins from dependency checking

--- util/update-dependencies.R
+++ util/update-dependencies.R
@@ -8,35 +8,40 @@ options(download.file.method = 'wget')  # easier to route through SOCKS
 ip <- installed.packages()
 ap <- available.packages()
 
-updateDependencies <- function(desc) {
+extractDependencies <- function(desc) {
 	rawPkg <- read.dcf(desc,
 			   fields=c('LinkingTo', 'Depends', 'Suggests', 'Imports'))
 	pkg <- unlist(strsplit(rawPkg, split=",", fixed=TRUE))
 	pkg <- sub("\\(.*\\)", "", pkg)
 	pkg <- gsub("\\s", "", pkg)
 	pkg <- pkg[!is.na(pkg)]
+}
 
+updateDependencies <- function(pkg) {
 	rv <- R.Version()
 	fullversion <- paste(rv$major, rv$minor, sep=".")
 
 	have <- ip[ip[,'Package'] %in% pkg & ip[,'Built'] == fullversion, 'Version']
 
-	bestVersion <- ap[ap[,'Package'] %in% pkg, 'Version']
-
 	for (p1 in pkg) {
-		if (p1 == 'R' || p1 == 'methods' || p1 == 'parallel' || p1 == 'Rmpi' || p1 == 'OpenMx') next
 		bestVersion <- ap[ap[,'Package'] == p1, 'Version']
 		haveVersion <- have[p1]
-		if (!length(bestVersion)) stop(p1)
+		if (!length(bestVersion)) cat(paste("Is", p1, "a built-in package?"),fill=T)
 		if (!is.na(haveVersion) && haveVersion == bestVersion) {
 			cat(paste("package", p1, "version", bestVersion, "is already installed"), fill=T)
 			next
 		}
+#		cat(paste("install", p1),fill=T)
 		install.packages(p1)
 	}
 }
 
 args <- commandArgs(trailingOnly = TRUE)
+pkg <- c()
 for (arg in args) {
-	updateDependencies(arg)
+	pkg <- union(pkg, extractDependencies(arg))
 }
+pkg <- setdiff(pkg, c('R', 'methods', 'parallel', 'Rmpi',
+                      'OpenMx', 'stats', 'utils', 'graphics', 'grDevices'))
+cat(deparse(pkg), fill=TRUE)
+updateDependencies(pkg)
