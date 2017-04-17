OpenMx/OpenMx

commit 84e2cdc242d949c2c653c2b24c71b3f302f54135

Implement analytic equality-constraint Jacobians for SLSQP.

src/ComputeGD.cpp
@@ -260,20 +260,43 @@ double GradientOptimizerContext::solFun(double *myPars, int* mode)
 
 // NOTE: All non-linear constraints are applied regardless of free
 // variable group.
-void GradientOptimizerContext::solEqBFun()
+void GradientOptimizerContext::solEqBFun(bool wantAJ) //<--"want analytic Jacobian"
 {
 	const int eq_n = (int) equality.size();
 	omxState *st = fc->state;
 
 	if (!eq_n) return;
-
-	int cur = 0;
-	for(int j = 0; j < int(st->conListX.size()); j++) {
-		omxConstraint &con = *st->conListX[j];
-		if (con.opCode != omxConstraint::EQUALITY) continue;
-
-		con.refreshAndGrab(fc, &equality(cur));
-		cur += con.size;
+	
+	analyticEqJacTmp.setConstant(analyticEqJacTmp.rows(),analyticEqJacTmp.cols(),NA_REAL);
+	
+	if(wantAJ && usingAnalyticJacobian){
+		int cur = 0, c=0, roffset=0;
+		for(int j = 0; j < int(st->conListX.size()); j++) {
+			omxConstraint &con = *st->conListX[j];
+			if (con.opCode != omxConstraint::EQUALITY) continue;
+			
+			con.refreshAndGrab(fc, &equality(cur));
+			if(con.jacobian != NULL){
+				omxRecompute(con.jacobian, fc);
+				for(c=0; c<con.jacobian->cols; c++){
+					if(con.jacMap[c]<0){continue;}
+					for(roffset=0; roffset<con.size; roffset++){
+						analyticEqJacTmp(cur+roffset,con.jacMap[c]) = con.jacobian->data[c * con.size + roffset];
+					}
+				}
+			}
+			cur += con.size;
+		}
+	}
+	else{
+		int cur = 0;
+		for(int j = 0; j < int(st->conListX.size()); j++) {
+			omxConstraint &con = *st->conListX[j];
+			if (con.opCode != omxConstraint::EQUALITY) continue;
+			
+			con.refreshAndGrab(fc, &equality(cur));
+			cur += con.size;
+		}
 	}
 
 	if (verbose >= 3) {
@@ -309,6 +332,19 @@ void GradientOptimizerContext::myineqFun()
 		mxPrintMat("inequality", inequality);
 	}
 };
+
+void GradientOptimizerContext::checkForAnalyticJacobians()
+{
+	usingAnalyticJacobian = false;
+	omxState *st = fc->state;
+	for(int i=0; i < (int) st->conListX.size(); i++){
+		omxConstraint &cs = *st->conListX[i];
+		if(cs.jacobian){
+			usingAnalyticJacobian = true;
+			return;
+		}
+	}
+}
 
 // ------------------------------------------------------------
 
@@ -543,6 +579,7 @@ void omxComputeGD::computeImpl(FitContext *fc)
 		break;
         case OptEngine_NLOPT:
 		if (rf.maxMajorIterations == -1) rf.maxMajorIterations = Global->majorIterations;
+		rf.checkForAnalyticJacobians();
 		omxInvokeNLOPT(rf);
 		rf.finish();
 		fc->wanted |= FF_COMPUTE_GRADIENT;
@@ -555,7 +592,7 @@ void omxComputeGD::computeImpl(FitContext *fc)
 		fc->copyParamToModel();
 		rf.setupSimpleBounds();
 		rf.setupIneqConstraintBounds();
-		rf.solEqBFun();
+		rf.solEqBFun(false);
 		rf.myineqFun();
 		if(rf.inequality.size() == 0 && rf.equality.size() == 0) {
 			omxSD(rf);   // unconstrained problems

src/finiteDifferences.h
@@ -223,20 +223,25 @@ struct central_difference_jacobi {
 
 template <typename T1, typename T2, typename T3, typename T4, typename T5>
 void jacobianImpl(T1 ff,  Eigen::MatrixBase<T2> &ref, Eigen::MatrixBase<T3> &point,
-		  int numIter, const double eps, T4 dfn, Eigen::MatrixBase<T5> &jacobiOut)
+                  int numIter, const double eps, T4 dfn, Eigen::MatrixBase<T5> &jacobiOut)
 {
 	// TODO evaluate jacobian in parallel
 	for (int px=0; px < int(point.size()); ++px) {
-		double offset = std::max(fabs(point[px] * eps), eps);
-		Eigen::MatrixXd Gaprox(ref.size(), numIter);
-		dfn(ff, ref, point, offset, px, numIter, Gaprox);
-		for(int m = 1; m < numIter; m++) {						// Richardson Step
-			for(int k = 0; k < (numIter - m); k++) {
-				// NumDeriv Hard-wires 4s for r here. Why?
-				Gaprox.col(k) = (Gaprox.col(k+1) * pow(4.0, m) - Gaprox.col(k))/(pow(4.0, m)-1);
+		if(isnan(jacobiOut.row(px).sum())){
+			double offset = std::max(fabs(point[px] * eps), eps);
+			Eigen::MatrixXd Gaprox(ref.size(), numIter);
+			dfn(ff, ref, point, offset, px, numIter, Gaprox);
+			for(int m = 1; m < numIter; m++) {						// Richardson Step
+				for(int k = 0; k < (numIter - m); k++) {
+					// NumDeriv Hard-wires 4s for r here. Why?
+					Gaprox.col(k) = (Gaprox.col(k+1) * pow(4.0, m) - Gaprox.col(k))/(pow(4.0, m)-1);
+				}
+			}
+			for(int i=0; i<jacobiOut.cols(); i++){
+				//jacobiOut.row(px) = Gaprox.col(0).transpose();
+				if(isnan(jacobiOut(px,i))){jacobiOut(px,i) = Gaprox(i,0);}
 			}
 		}
-		jacobiOut.row(px) = Gaprox.col(0).transpose();
 	}
 }
 

src/nloptcpp.cpp
@@ -8,6 +8,7 @@
 #include "nloptcpp.h"
 #include "Compute.h"
 #include "ComputeGD.h"
+//#include <Rmath.h>
 
 #include "nlopt.h"
 #include "slsqp.h"
@@ -69,8 +70,18 @@ struct equality_functional {
 	template <typename T1, typename T2>
 	void operator()(Eigen::MatrixBase<T1> &x, Eigen::MatrixBase<T2> &result) const {
 		goc.copyFromOptimizer(x.derived().data());
-		goc.solEqBFun();
+		goc.solEqBFun(false);
 		result = goc.equality;
+	}
+	
+	template <typename T1, typename T2, typename T3>
+	void operator()(Eigen::MatrixBase<T1> &x, Eigen::MatrixBase<T2> &result, Eigen::MatrixBase<T3> &jacobian) const {
+		goc.copyFromOptimizer(x.derived().data());
+		goc.analyticEqJacTmp.resize(jacobian.cols(), jacobian.rows());
+		goc.solEqBFun(true);
+		result = goc.equality;
+		jacobian = goc.analyticEqJacTmp.transpose();
+		
 	}
 };
 
@@ -82,7 +93,7 @@ static void nloptEqualityFunction(unsigned m, double* result, unsigned n, const 
 	Eigen::VectorXd Eresult(ctx.origeq);
 	Eigen::MatrixXd jacobian(n, ctx.origeq);
 	equality_functional ff(goc);
-	ff(Epoint, Eresult);
+	ff(Epoint, Eresult, jacobian);
 	if (grad) {
 		goc.eqNorm = Eresult.array().abs().sum();
 		fd_jacobian(goc.gradientAlgo, goc.gradientIterations, goc.gradientStepSize,

inst/models/passing/AnalyticConstraintJacobians--PowellBenchmark.R
@@ -15,7 +15,7 @@
 
 
 library(OpenMx)
-#mxOption(NULL,"Default optimizer","NPSOL")
+#mxOption(NULL,"Default optimizer","SLSQP")
 
 powellmod1 <- mxModel(
 	"PowellBenchmarkNoJacobians",
@@ -75,7 +75,6 @@ powellrun2$output$evaluations
 summary(powellrun2)
 
 
-#Right now, only NPSOL knows how to use analytic Jacobians:
 if(mxOption(NULL,"Default optimizer")=="NPSOL"){
   #Analytic Jacobians should, if nothing else, cut down on the number of fitfunction evaluations:
 	omxCheckEquals(omxGreaterThan(powellrun1$output$evaluations,powellrun2$output$evaluations),1)
@@ -128,6 +127,19 @@ if(mxOption(NULL,"Default optimizer")=="NPSOL"){
 			"PowellBenchmarkWithJacobians.c2[1,1]","PowellBenchmarkWithJacobians.c3[1,1]")
 	)
 } else if(mxOption(NULL,"Default optimizer")=="SLSQP"){
+	#Analytic Jacobians should, if nothing else, cut down on the number of fitfunction evaluations:
+	omxCheckEquals(omxGreaterThan(powellrun1$output$evaluations,powellrun2$output$evaluations),1)
+	
+	#At the solution, equality constraints should be satisfied within feasibility tolerance:
+	omxCheckCloseEnough(powellrun1$compute$steps$GD$output$constraintFunctionValues,c(0,0,0),
+											as.numeric(mxOption(NULL,"Feasibility tolerance")))
+	omxCheckCloseEnough(powellrun2$compute$steps$GD$output$constraintFunctionValues,c(0,0,0),
+											as.numeric(mxOption(NULL,"Feasibility tolerance")))
+	
+	#The numerical and analytic Jacobians should agree closely:
+	omxCheckCloseEnough(a=powellrun1$compute$steps$GD$output$constraintJacobian,b=powellrun2$compute$steps$GD$output$constraintJacobian,
+											epsilon=1e-5)
+	
 	#Check naming of constraint-related information:
 	omxCheckEquals(
 		names(powellrun1$output$constraintFunctionValues),
@@ -356,7 +368,7 @@ powellrun7$output$iterations
 powellrun7$output$evaluations
 summary(powellrun7)
 
-if(mxOption(NULL,"Default optimizer")=="NPSOL"){
+if(mxOption(NULL,"Default optimizer") %in% c("NPSOL","SLSQP")){
 	tbl <- data.frame(
 		c("Yes","Yes","No","No"),c("No","Yes","Yes","No"),
 		c(powellrun1$output$evaluations,powellrun2$output$evaluations,powellrun6$output$evaluations,

src/ComputeGD.h
@@ -96,7 +96,7 @@ class GradientOptimizerContext {
 
 	double solFun(double *myPars, int* mode);
 	double recordFit(double *myPars, int* mode);
-	void solEqBFun();
+	void solEqBFun(bool wantAJ);
 	void myineqFun();
 	template <typename T1, typename T2, typename T3> void allConstraintsFun(
 			Eigen::MatrixBase<T1> &constraintOut, Eigen::MatrixBase<T2> &jacobianOut, Eigen::MatrixBase<T3> &needcIn, int mode);
@@ -104,6 +104,8 @@ class GradientOptimizerContext {
 	template <typename T1> void linearConstraintCoefficients(Eigen::MatrixBase<T1> &lcc);
 	bool usingAnalyticJacobian;
 	void checkForAnalyticJacobians();
+	Eigen::MatrixXd analyticEqJacTmp; //<--temporarily holds analytic Jacobian (if present) for an equality constraint
+	Eigen::MatrixXd analyticIneqJacTmp; //<--temporarily holds analytic Jacobian (if present) for an inequality constraint
 	void useBestFit();
 	void copyToOptimizer(double *myPars);
 	void copyFromOptimizer(double *myPars, FitContext *fc2);

src/omxNPSOLSpecific.cpp
@@ -180,19 +180,6 @@ void GradientOptimizerContext::linearConstraintCoefficients(Eigen::MatrixBase<T1
 	}
 }
 
-void GradientOptimizerContext::checkForAnalyticJacobians()
-{
-	usingAnalyticJacobian = false;
-	omxState *st = fc->state;
-	for(int i=0; i < (int) st->conListX.size(); i++){
-		omxConstraint &cs = *st->conListX[i];
-		if(cs.jacobian){
-			usingAnalyticJacobian = true;
-			return;
-		}
-	}
-}
-
 static double getNPSOLFeasibilityTolerance()
 {
 	// convert units (who knows what the units are)

src/subnp.cpp
@@ -181,7 +181,7 @@ void CSOLNP::solnp(double *solPars, int verbose)
     
     Eigen::MatrixXd constraint_e;
     
-    fit.solEqBFun();
+    fit.solEqBFun(false);
     Eigen::RowVectorXd eqv_e(neq);
     eqv_e = fit.equality;
     
@@ -256,7 +256,7 @@ void CSOLNP::solnp(double *solPars, int verbose)
     Eigen::MatrixXd ob_e(1, 1 + neq + nineq);
     Eigen::RowVectorXd funvMatrix_e(1);
     funvMatrix_e[0] = funv;
-    fit.solEqBFun();
+    fit.solEqBFun(false);
     eqv_e = fit.equality;
         
     fit.myineqFun();
@@ -313,7 +313,7 @@ void CSOLNP::solnp(double *solPars, int verbose)
             temp = p_e.block(0, nineq, 1, np);
             funv = fit.solFun(temp.data(), &mode);
             funvMatrix_e[0] = funv;
-            fit.solEqBFun();
+            fit.solEqBFun(false);
             eqv_e = fit.equality;
             fit.myineqFun();
             ineqv_e = -fit.inequality;
@@ -360,7 +360,7 @@ void CSOLNP::solnp(double *solPars, int verbose)
         
         solnp_nfn = solnp_nfn + 1;
 
-        fit.solEqBFun();
+        fit.solEqBFun(false);
         eqv_e = fit.equality;
         funvMatrix_e[0] = funv;
         fit.myineqFun();
@@ -687,7 +687,7 @@ void CSOLNP::subnp(Eigen::MatrixBase<T2>& pars, Eigen::MatrixBase<T1>& yy_e, Eig
             }
             funv = fit.solFun(tmpv_e.data(), &mode);
             
-            fit.solEqBFun();
+            fit.solEqBFun(false);
             fit.myineqFun();
             
             solnp_nfn = solnp_nfn + 1;
@@ -859,7 +859,7 @@ void CSOLNP::subnp(Eigen::MatrixBase<T2>& pars, Eigen::MatrixBase<T1>& yy_e, Eig
             mode = 0;
         }
         
-        fit.solEqBFun();
+        fit.solEqBFun(false);
         
         fit.myineqFun();
         
@@ -924,7 +924,7 @@ void CSOLNP::subnp(Eigen::MatrixBase<T2>& pars, Eigen::MatrixBase<T1>& yy_e, Eig
                     funv = 1e24;
                     mode = 0;
                 }
-                fit.solEqBFun();
+                fit.solEqBFun(false);
                 fit.myineqFun();
                 
                 solnp_nfn = solnp_nfn + 1;
@@ -1143,7 +1143,7 @@ void CSOLNP::subnp(Eigen::MatrixBase<T2>& pars, Eigen::MatrixBase<T1>& yy_e, Eig
             mode = 0;
         }
         
-        fit.solEqBFun();
+        fit.solEqBFun(false);
         fit.myineqFun();
         
         solnp_nfn = solnp_nfn + 1;
@@ -1204,7 +1204,7 @@ void CSOLNP::subnp(Eigen::MatrixBase<T2>& pars, Eigen::MatrixBase<T1>& yy_e, Eig
                 mode = 0;
             }
             
-            fit.solEqBFun();
+            fit.solEqBFun(false);
             fit.myineqFun();
             
             solnp_nfn = solnp_nfn + 1;

commit 818f7d4bd12feb9996c2f00cc09fb14f417a5e15

Cure regression to Oakes SEs (ComputeEM) introduced in 84e2cdc242

src/Compute.cpp
@@ -2301,7 +2301,7 @@ void ComputeEM::Oakes(FitContext *fc)
 
 	Eigen::MatrixXd jacobian(freeVars, freeVars);
 	estep_jacobian_functional ejf(this, fc);
-	fd_jacobian(GradientAlgorithm_Forward, 1, 1e-5, ejf, refGrad, optimumCopy, jacobian);
+	fd_jacobian<false>(GradientAlgorithm_Forward, 1, 1e-5, ejf, refGrad, optimumCopy, jacobian);
 
 	fc->infoMethod = infoMethod;
 	fc->preInfo();

src/ComputeGD.cpp
@@ -260,7 +260,7 @@ void GradientOptimizerContext::solEqBFun(bool wantAJ) //<--"want analytic Jacobi
 	/*Note that this needs to happen even if no equality constraints have analytic Jacobians, because
 	analyticEqJacTmp is copied to the Jacobian matrix the elements of which are populated by code in
 	finiteDifferences.h, which knows to numerically populate an element if it's NA:*/
-	analyticEqJacTmp.setConstant(analyticEqJacTmp.rows(),analyticEqJacTmp.cols(),NA_REAL);
+	analyticEqJacTmp.setConstant(NA_REAL);
 	
 	int cur=0, j=0, c=0, roffset=0;
 	for(j = 0; j < int(st->conListX.size()); j++) {
@@ -294,7 +294,7 @@ void GradientOptimizerContext::myineqFun(bool wantAJ)
 
 	if (!ineq_n) return;
 	
-	analyticIneqJacTmp.setConstant(analyticIneqJacTmp.rows(),analyticIneqJacTmp.cols(),NA_REAL);
+	analyticIneqJacTmp.setConstant(NA_REAL);
 	
 	int cur=0, j=0, c=0, roffset=0;
 	for (j=0; j < int(st->conListX.size()); j++) {

src/finiteDifferences.h
@@ -221,31 +221,29 @@ struct central_difference_jacobi {
 	};
 };
 
-template <typename T1, typename T2, typename T3, typename T4, typename T5>
+template <bool initialized, typename T1, typename T2, typename T3, typename T4, typename T5>
 void jacobianImpl(T1 ff,  Eigen::MatrixBase<T2> &ref, Eigen::MatrixBase<T3> &point,
                   int numIter, const double eps, T4 dfn, Eigen::MatrixBase<T5> &jacobiOut)
 {
-	// TODO evaluate jacobian in parallel
-	for (int px=0; px < int(point.size()); ++px) {
-		if(std::isnan(jacobiOut.row(px).sum())){
-			double offset = std::max(fabs(point[px] * eps), eps);
-			Eigen::MatrixXd Gaprox(ref.size(), numIter);
-			dfn(ff, ref, point, offset, px, numIter, Gaprox);
-			for(int m = 1; m < numIter; m++) {						// Richardson Step
-				for(int k = 0; k < (numIter - m); k++) {
-					// NumDeriv Hard-wires 4s for r here. Why?
-					Gaprox.col(k) = (Gaprox.col(k+1) * pow(4.0, m) - Gaprox.col(k))/(pow(4.0, m)-1);
+	// TODO refactor, evaluate jacobian in parallel
+	if (initialized) {
+		for (int px=0; px < int(point.size()); ++px) {
+			if(std::isnan(jacobiOut.row(px).sum())){
+				double offset = std::max(fabs(point[px] * eps), eps);
+				Eigen::MatrixXd Gaprox(ref.size(), numIter);
+				dfn(ff, ref, point, offset, px, numIter, Gaprox);
+				for(int m = 1; m < numIter; m++) {						// Richardson Step
+					for(int k = 0; k < (numIter - m); k++) {
+						// NumDeriv Hard-wires 4s for r here. Why?
+						Gaprox.col(k) = (Gaprox.col(k+1) * pow(4.0, m) - Gaprox.col(k))/(pow(4.0, m)-1);
+					}
+				}
+				for(int i=0; i<jacobiOut.cols(); i++){
+					if(std::isnan(jacobiOut(px,i))){jacobiOut(px,i) = Gaprox(i,0);}
 				}
 			}
-			for(int i=0; i<jacobiOut.cols(); i++){
-				//jacobiOut.row(px) = Gaprox.col(0).transpose();
-				if(std::isnan(jacobiOut(px,i))){jacobiOut(px,i) = Gaprox(i,0);}
-			}
 		}
-	}/*
-	//If there are no constraint functions with analytic Jacobians, we can avoid unnecessary row sums, 
-	//unnecessary elementwise checking for NaNs, and use a simpler and better-tested block of code:
-	else{
+	} else {
 		for (int px=0; px < int(point.size()); ++px) {
 			double offset = std::max(fabs(point[px] * eps), eps);
 			Eigen::MatrixXd Gaprox(ref.size(), numIter);
@@ -258,21 +256,21 @@ void jacobianImpl(T1 ff,  Eigen::MatrixBase<T2> &ref, Eigen::MatrixBase<T3> &poi
 			}
 			jacobiOut.row(px) = Gaprox.col(0).transpose();
 		}
-	}*/
+	}
 }
 
-template <typename T1, typename T2, typename T3, typename T4>
+template <bool initialized, typename T1, typename T2, typename T3, typename T4>
 void fd_jacobian(GradientAlgorithm algo, int numIter, double eps, T1 ff, Eigen::MatrixBase<T2> &ref,
 	      Eigen::MatrixBase<T3> &point, Eigen::MatrixBase<T4> &jacobiOut)
 {
 	switch (algo) {
 	case GradientAlgorithm_Forward:{
 		forward_difference_jacobi dfn;
-		jacobianImpl(ff, ref, point, numIter, eps, dfn, jacobiOut);
+		jacobianImpl<initialized>(ff, ref, point, numIter, eps, dfn, jacobiOut);
 		break;}
 	case GradientAlgorithm_Central:{
 		central_difference_jacobi dfn;
-		jacobianImpl(ff, ref, point, numIter, eps, dfn, jacobiOut);
+		jacobianImpl<initialized>(ff, ref, point, numIter, eps, dfn, jacobiOut);
 		break;}
 	default: Rf_error("Unknown gradient algorithm %d", algo);
 	}

src/nloptcpp.cpp
@@ -99,7 +99,7 @@ static void nloptEqualityFunction(unsigned m, double* result, unsigned n, const 
 	else{
 		ff(Epoint, Eresult, jacobian);
 		goc.eqNorm = Eresult.array().abs().sum();
-		fd_jacobian(goc.gradientAlgo, goc.gradientIterations, goc.gradientStepSize,
+		fd_jacobian<true>(goc.gradientAlgo, goc.gradientIterations, goc.gradientStepSize,
               ff, Eresult, Epoint, jacobian);
 		if (ctx.eqmask.size() == 0) {
 			ctx.eqmask.assign(m, false);
@@ -191,7 +191,7 @@ static void nloptInequalityFunction(unsigned m, double *result, unsigned n, cons
 			mxPrintMat("major iteration ineq", Eresult);
 		}
 		goc->ineqNorm = Eresult.array().abs().sum();
-		fd_jacobian(goc->gradientAlgo, goc->gradientIterations, goc->gradientStepSize,
+		fd_jacobian<true>(goc->gradientAlgo, goc->gradientIterations, goc->gradientStepSize,
               ff, Eresult, Epoint, jacobian);
 		if (!std::isfinite(Eresult.sum())) {
 			// infeasible at start of major iteration