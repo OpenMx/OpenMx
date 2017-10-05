d00a8acd800a8d6f9b71a7aa3159da1e0ebdcccd
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2017-10-05 12:55:39 -0400

    WLS: Skip thresholds check for all-continuous case

--- src/omxWLSFitFunction.cpp
+++ src/omxWLSFitFunction.cpp
@@ -445,9 +445,11 @@ void omxWLSFitFunction::init()
 		newObj->standardMeans = omxInitMatrix(1, ncol, TRUE, currentState);
 	}
 	omxMatrix *obsThresholdsMat = oo->expectation->data->obsThresholdsMat;
-	if (obsThresholdsMat->rows != oo->expectation->thresholdsMat->rows ||
-	    obsThresholdsMat->cols != oo->expectation->thresholdsMat->cols) {
-		omxRaiseError("Observed and expected threshold matrices must have the same number of rows and columns");
+	if (obsThresholdsMat && oo->expectation->thresholdsMat) {
+		if (obsThresholdsMat->rows != oo->expectation->thresholdsMat->rows ||
+		    obsThresholdsMat->cols != oo->expectation->thresholdsMat->cols) {
+			omxRaiseError("Observed and expected threshold matrices must have the same number of rows and columns");
+		}
 	}
 	
 	flattenDataToVector(newObj->observedCov, newObj->observedMeans, obsThresholdsMat, oThresh, newObj->observedFlattened);
