75cd08f291d38666c6eba009e2dbb1f9c82320f3
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-05-04 16:17:41 -0400

    mxComputeGenSA: Add support for non-linear constraints

--- inst/models/passing/GenSA.R
+++ inst/models/passing/GenSA.R
@@ -46,3 +46,34 @@ m2 <- mxRun(m2)
 
 log2 <- read.table(path, header=TRUE, sep='\t', check.names=FALSE)
 omxCheckEquals(nrow(log2), 2)
+
+# ----
+
+dimension <- 2
+
+m1 <- mxModel(
+	"rastrigin",
+	mxMatrix(type='Full', values=runif(dimension, min=-1e6, max=1e6),
+		ncol=1, nrow=dimension, free=TRUE, name='x'),
+	mxAlgebra(sum(x*x - 10 * cos(2 * pi * x)) + 10 * dimension, name="fit"),
+	mxConstraint(x[1,1] > .5, "con1"),
+	mxFitFunctionAlgebra('fit'),
+	mxComputeGenSA()
+)
+
+m1 <- mxRun(m1)
+omxCheckCloseEnough(coef(m1), c(1,0), 1e-2)
+
+m1 <- mxModel(
+	"rastrigin",
+	mxMatrix(type='Full', values=runif(dimension, min=-1e6, max=1e6),
+		ncol=1, nrow=dimension, free=TRUE, name='x'),
+	mxAlgebra(sum(x*x - 10 * cos(2 * pi * x)) + 10 * dimension, name="fit"),
+	mxConstraint(x[1,1] == -1, "con1"),
+	mxFitFunctionAlgebra('fit'),
+	mxComputeGenSA()
+)
+
+m1 <- mxRun(m1)
+omxCheckCloseEnough(coef(m1), c(-1,0), 1e-2)
+
