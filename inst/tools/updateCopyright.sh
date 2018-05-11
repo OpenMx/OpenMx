bcdf9f416d2d3ca68bb12053615a8308b6d26eef
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-05-11 13:45:13 -0400

    Be smarter about updating copyright dates

--- inst/tools/updateCopyright.sh
+++ inst/tools/updateCopyright.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $(git grep -l 'Copyright')
+fileList=$(comm -12 <(git grep -l 'Copyright' | sort) <(git diff --name-only @{1.year.ago} | sort))
 
-perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $(git grep -l 'Copyright')
+perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $fileList
+
+perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $fileList
