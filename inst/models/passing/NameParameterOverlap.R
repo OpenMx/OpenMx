0f169da2819f3c30df4c35e9f2d9728a2d11955e
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-05-11 13:37:24 -0400

    Update laggard copyright dates to 2018

--- inst/models/passing/NameParameterOverlap.R
+++ inst/models/passing/NameParameterOverlap.R
@@ -4,7 +4,7 @@
 # 2017-04-14 04:50PM TBATES Correct bug in error check - this is now passing.
 
 #
-#   Copyright 2007-2012 by the individuals mentioned in the source code history
+#   Copyright 2007-2018 by the individuals mentioned in the source code history
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
--- inst/tools/updateCopyright.sh
+++ inst/tools/updateCopyright.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history' $(git grep -l 'Copyright')
+perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $(git grep -l 'Copyright')
 
-perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history' $(git grep -l 'Copyright')
+perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $(git grep -l 'Copyright')
