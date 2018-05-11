5cd50b0139128151fc03dd20d96670c7b0aff8ac
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-05-11 09:12:38 -0400

    GenSA: Remove equality constraint test

--- inst/models/passing/GenSA.R
+++ inst/models/passing/GenSA.R
@@ -63,17 +63,3 @@ m1 <- mxModel(
 
 m1 <- mxRun(m1)
 omxCheckCloseEnough(coef(m1), c(1,0), 1e-2)
-
-m1 <- mxModel(
-	"rastrigin",
-	mxMatrix(type='Full', values=runif(dimension, min=-1e6, max=1e6),
-		ncol=1, nrow=dimension, free=TRUE, name='x'),
-	mxAlgebra(sum(x*x - 10 * cos(2 * pi * x)) + 10 * dimension, name="fit"),
-	mxConstraint(x[1,1] == -1, "con1"),
-	mxFitFunctionAlgebra('fit'),
-	mxComputeSimAnnealing()
-)
-
-m1 <- mxRun(m1)
-omxCheckCloseEnough(coef(m1), c(-1,0), 1e-2)
-
