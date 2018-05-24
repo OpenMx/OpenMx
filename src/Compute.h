278618e6290e845473be99d843e5dd61beffbdcd
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-05-24 13:51:05 -0400

    Repair copyright year ranges

--- src/Compute.h
+++ src/Compute.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2007-2018 by the individuals mentioned in the source code history
+ *  Copyright 2013-2018 by the individuals mentioned in the source code history
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
-/*File created 2013*/
 
 #ifndef _OMX_COMPUTE_H_
 #define _OMX_COMPUTE_H_
