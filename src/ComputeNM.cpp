df4e2b71a8580d0ce6747c91ba507c9fcab1cf2f
RMKirkpatrick <rkirkpatrick2@vcu.edu>
2018-05-24 14:45:03 -0400

    Nelder-Mead: correct capitalization from previous commit.

--- src/ComputeNM.cpp
+++ src/ComputeNM.cpp
@@ -295,7 +295,7 @@ void omxComputeNM::computeImpl(FitContext *fc){
 			if(verbose){mxLog("l1p iteration %d",k);}
 			if(k>0){
 				if(nmoc.iniSimplexMat.rows() || nmoc.iniSimplexMat.cols()){nmoc.iniSimplexMat.resize(0,0);}
-				if(nmoc.statuscode==10 || Global->timedout){break;}
+				if(nmoc.statuscode==10 || Global->timedOut){break;}
 				if( !nmoc.estInfeas && nmoc.statuscode==0 ){
 					if(verbose){mxLog("l1p solution found");}
 					break;
