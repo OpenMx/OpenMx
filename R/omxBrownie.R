30fe6e8cfde7bb2f08042490e7a72e2c51a8e659
mhunter1 <mhunter.ou@gmail.com>
2018-11-15 20:40:27 -0500

    Restore Ryne's sacred brownie recipe
    You can still do vegan if you must.

--- R/omxBrownie.R
+++ R/omxBrownie.R
@@ -14,32 +14,43 @@
 #   limitations under the License.
 
 
-omxBrownie <- function(quantity=1, walnuts=TRUE){
+omxBrownie <- function(quantity=1, walnuts=TRUE, vegan=FALSE){
 	if ((is.character(walnuts) && walnuts == 'allergic') ||
 			(is.logical(walnuts) && walnuts == TRUE)) {
 		if (is.character(walnuts)) walnuts <- FALSE
 	} else {
 		stop("Walnuts are required for brownies. Please correct the 'walnuts' argument to either TRUE (default) or 'allergic'.")
 	}
-	if (walnuts) {
-		amt <- c(.5, .5, 4/3,   1, 1/4, 2/3, 3, 16, 180, 1, 1)
+	if(vegan){
+		if (walnuts) {
+			amt <- c(.5, .5, 4/3,   1, 1/4, 2/3, 3, 16, 180, 1, 1)
+		} else {
+			amt <- c(.5, .5, 2/3, 2/3, 1/4,   0, 3, 16, 180, 1, 1)
+		}
+		unit <- c("cup", "cup", "cup", "cup", "tsp",
+			'cup', "cup", "dates", "grams", 'Tbsp', "tsp")
+		ing <- c("whole wheat flour",
+			"black bean flour",
+			"sucanat",
+			"Dutched cocoa powder",
+			"salt",
+			"chopped walnuts",
+			"water",
+			"deglet nour dates",
+			"coconut butter (gently warmed to 90F)",
+			"blackstrap molasses",
+			"vanilla extract")
 	} else {
-		amt <- c(.5, .5, 2/3, 2/3, 1/4,   0, 3, 16, 180, 1, 1)
+		amt <- c(4, 2, 8, 1.25, 1, 0.75, 0.5, 1.5)
+		unit <- c("whole", "cup", "oz", "cup",
+			"tsp", "cup", "tsp", "cup")
+		ing <- c("eggs", "granulated sugar",
+			"butter (room temperature)",
+			"cocoa powder", "vanilla extract",
+			"all-purpose flour", "salt",
+			"chopped walnuts")
 	}
 	amt <- round(amt*quantity, 3)
-	unit <- c("cup", "cup", "cup", "cup", "tsp",
-		'cup', "cup", "dates", "grams", 'Tbsp', "tsp")
-	ing <- c("whole wheat flour",
-		"black bean flour",
-		"sucanat",
-		"Dutched cocoa powder",
-		"salt",
-		"chopped walnuts",
-		"water",
-		"deglet nour dates",
-		"coconut butter (gently warmed to 90F)",
-		"blackstrap molasses",
-		"vanilla extract")
 	brown <- list(
 		data.frame("Qty."=amt, "Unit"=unit, "Ingredient"=ing),
 		matrix(c(quantity, quantity, 1, 1, 1, 1, quantity, 1,
@@ -56,9 +67,9 @@ omxBrownie <- function(quantity=1, walnuts=TRUE){
 			),
 		matrix(c(
 		"Heat oven to 350 degrees Fahrenheit.", 
-		"Mix dry ingredients with a fork.",
-		"Puree water and dates in a high speed blender.",
-		"Combine all ingredients and mix thoroughly.",
+		ifelse(vegan, "Mix dry ingredients with a fork.", "Lightly butter baking pan(s), then insert parchment sling."),
+		ifelse(vegan, "Puree water and dates in a high speed blender.", "Beat eggs using mixer until homogonous. Beat in sugar until 'ribbon stage' is reached."),
+		ifelse(vegan, "Combine all ingredients and mix thoroughly.", "Mix in other ingredients, stirring until just combined."),
 		"Bake at 350 for 25 to 30 minutes. Brownies are done when toothpick inserted in brownies comes out clean.",
 		"Place on rack to cool. When nearly at room temperature, brownies may be removed by picking up parchment sling for easy cutting."), ncol=1)
 		)
@@ -68,6 +79,7 @@ omxBrownie <- function(quantity=1, walnuts=TRUE){
 	}
 	return(brown)
 }
+
 
 ### future work for OmxBrownie objects
 ### this doesn't work yet
