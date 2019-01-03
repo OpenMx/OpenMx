d63594b52b0bd6ab4875eebfc5797e56a5927422
mhunter1 <mhunter.ou@gmail.com>
2019-01-03 12:33:33 -0500

    Update copyright script with new year

--- inst/tools/updateCopyright.sh
+++ inst/tools/updateCopyright.sh
@@ -2,6 +2,6 @@
 
 fileList=$(comm -12 <(git grep -l 'Copyright' | sort) <(git diff --name-only @{1.year.ago} | sort))
 
-perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $fileList
+perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-2019 by the individuals mentioned in the source code history/' $fileList
 
-perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-2018 by the individuals mentioned in the source code history/' $fileList
+perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-2019 by the individuals mentioned in the source code history/' $fileList
