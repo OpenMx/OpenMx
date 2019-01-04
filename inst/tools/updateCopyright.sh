29b579b91d9e169373ea3a39c2bef28cc4cb92b0
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2019-01-04 10:03:06 -0500

    Again try to update the copyright for only files modified in the past year

--- inst/tools/updateCopyright.sh
+++ inst/tools/updateCopyright.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-fileList=$(comm -12 <(git grep -l 'Copyright' | sort) <(git diff --name-only @{1.year.ago} | sort))
+anchor=v2.9.8-5-gbcdf9f416  # Immediately after the previous year's copyright update
+year=2019
 
-perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-2019 by the individuals mentioned in the source code history/' $fileList
+fileList=$(comm -12 <(git grep -l 'Copyright' | sort) <(git diff --name-only $anchor | sort))
 
-perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-2019 by the individuals mentioned in the source code history/' $fileList
+perl -pi -e 's/Copyright (\d+) by the individuals mentioned in the source code history/Copyright $1-'$year' by the individuals mentioned in the source code history/' $fileList
+
+perl -pi -e 's/Copyright (\d+)-(\d+) by the individuals mentioned in the source code history/Copyright $1-'$year' by the individuals mentioned in the source code history/' $fileList
