d2f53daa44f0b57d7dbb23de1a68b05baeafedf2
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2019-01-14 14:32:37 -0500

    Run integration tests against the latest versions of dep packages

---
+++ revdep/getPackages.R
@@ -0,0 +1,6 @@
+ap = available.packages()
+pk = c('ctsem', 'EasyMx', 'ifaTools', 'metaSEM', 'semtree', 'umx')
+v = sapply(pk, function(x) ap[which(ap[,'Package'] == x), 'Version'],
+            USE.NAMES=FALSE)
+cat(mapply(function(p1, v1) { paste0(p1,'_',v1,'.tar.gz') },
+           pk, v, USE.NAMES = FALSE))
