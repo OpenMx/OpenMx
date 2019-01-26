f8368c91faec69e4eb31039a47a0c3c8593ff8df
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2019-01-25 19:55:48 -0500

    Rethink parallel controller

--- src/CovEntrywisePar.h
+++ src/CovEntrywisePar.h
@@ -1,26 +1,35 @@
 #ifndef COVENTRYWISEPARALLEL_H_
 #define COVENTRYWISEPARALLEL_H_
 
+#include <queue>
 #include "omxState.h"
 #include "ConcurrentQueue.h"
+
+template <class T> struct pairMaxCompare {
+	bool operator()(const T &x, const T &y){
+		return (x.first + x.second) > (y.first + y.second);
+	}
+};
 
 template <typename CalcEntry>
 void CovEntrywiseParallel(int numThreads, CalcEntry &ce)
 {
 	const bool debug = false;
+	std::mutex delayedMutex;
+	std::priority_queue< std::pair<int,int>,
+		std::vector< std::pair<int,int> >,
+		pairMaxCompare< std::pair<int,int> > > delayed;
 	ConcurrentDeque< std::pair<int,int> > todo;
-	// Use type 'long' to ensure that writes don't interfere with each other
-	Eigen::Array<long, Eigen::Dynamic, 1> thrDone(numThreads);
-	thrDone.setZero();
 	int numCols = ce.getNumCols();
 	int numColsStar = triangleLoc1(numCols);
+	// Use type 'long' to ensure that writes don't interfere with each other
 	// isDone might behave badly so we copy it and track status ourselves
 	Eigen::Array<long, Eigen::Dynamic, 1> diagDone(numCols);
 	for (int cx=0; cx < numCols; ++cx) {
 		diagDone[cx] = ce.isDone(cx,cx);
 		todo.push_nolock(std::make_pair(cx,cx));
 	}
-	int maxIter = numColsStar + numThreads;
+	int remain = numColsStar;
 #pragma omp parallel num_threads(numThreads)
 	while (1) {
 		int tid = omp_get_thread_num();
@@ -37,54 +46,61 @@ void CovEntrywiseParallel(int numThreads, CalcEntry &ce)
 					omxRaiseErrorf("CovEntrywiseParallel: unknown exception");
 				}
 				diagDone[t1.first] = 1; // regardless of ce.isDone
+				if (debug) mxLog("diag %d done", t1.first);
 			}
 			for (int rx=0; rx < t1.first; ++rx) {
-				if (ce.isDone(rx, t1.first)) { thrDone[tid] += 1; continue; }
-				if (debug) mxLog("todo.push_front(%d,%d)", rx, t1.first);
-				if (diagDone(t1.first)) {
-					todo.push_front(std::make_pair(rx, t1.first));
-				} else {
+				if (ce.isDone(rx, t1.first)) {
+#pragma omp atomic
+					remain -= 1; // switch back to thrDone TODO
+					continue;
+				}
+				if (diagDone(rx)) {
 					todo.push_back(std::make_pair(rx, t1.first));
+				} else {
+					std::unique_lock<std::mutex> mlock(delayedMutex);
+					delayed.push(std::make_pair(rx, t1.first));
+					if (debug) mxLog("delay %d %d", rx, t1.second);
 				}
 			}
 		} else {
-			if (!diagDone[t1.first]) {
-				if (debug) mxLog("requeue %d %d", t1.first, t1.second);
-				todo.push_back(t1);
-				continue;
-			} else {
-				try {
-					ce.offDiag(t1.first, t1.second);
-				} catch (const std::exception& e) {
-					omxRaiseErrorf("%s", e.what());
-				} catch (...) {
-					omxRaiseErrorf("CovEntrywiseParallel: unknown exception");
-				}
+			try {
+				if (!isErrorRaised()) ce.offDiag(t1.first, t1.second);
+			} catch (const std::exception& e) {
+				omxRaiseErrorf("%s", e.what());
+			} catch (...) {
+				omxRaiseErrorf("CovEntrywiseParallel: unknown exception");
 			}
 		}
-		thrDone[tid] += 1;
-		int thrDoneSum = thrDone.sum();
+		while (delayed.size()) {
+			std::pair<int,int> top;
+			{
+				std::unique_lock<std::mutex> mlock(delayedMutex);
+				if (delayed.size() == 0) break;
+				top = delayed.top();
+				if (debug) mxLog("delayed.top is (%d,%d) pending %d",
+						 top.first, top.second, int(delayed.size()));
+				if (diagDone[top.first] && diagDone[top.second]) {
+					delayed.pop();
+					if (debug) mxLog("todo.push(%d,%d) %d left",
+							 top.first, top.second, int(delayed.size()));
+				} else break;
+			}
+			todo.push_back(top);
+		}
+#pragma omp atomic
+		remain -= 1;
 		if (tid == 0) {
-			ce.reportProgress(thrDoneSum);
+			ce.reportProgress(numColsStar - remain);
 			bool gotInt = omxGlobal::interrupted();
 			if (gotInt && debug) mxLog("interrupt");
 		}
-		if (thrDoneSum == numColsStar || isErrorRaised()) {
+		if (debug) mxLog(". %d/%d %d/%d", remain, numColsStar, int(diagDone.sum()), numCols);
+		if (remain==0 || isErrorRaised()) {
 			for (int tx=0; tx < numThreads; ++tx) {
 				todo.push_front(std::make_pair(-1, tx));
 			}
 		}
-#pragma omp atomic
-		maxIter -= 1;
-		if (maxIter < 0) {
-			mxLog("CovEntrywiseParallel loop %d failed to terminate, deque=%d",
-			      numCols, int(todo.size()));
-			mxPrintMat("thrDone", thrDone);
-			mxPrintMat("diagDone", diagDone);
-			break;
-		}
 	}
-	if (maxIter < 0) mxThrow("Contact OpenMx developers");
 }
 
 #endif
