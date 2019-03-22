ff2a4862a2ecf74970468c17022b71a28ac13155
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2019-03-22 10:09:15 -0400

    WLS: Variance must be larger than epsilon, not zero

--- src/omxData.cpp
+++ src/omxData.cpp
@@ -1199,7 +1199,7 @@ void omxData::wlsAllContinuousCumulants(omxState *state)
 	Ecov -= totalWeight * Emean * Emean.transpose();
 	Ecov /= totalWeight - 1;
 	for (int cx=0; cx < int(dc.size()); ++cx) {
-		if (Ecov(cx,cx) > 0.) continue;
+		if (Ecov(cx,cx) > std::numeric_limits<double>::epsilon()) continue;
 		mxThrow("%s: '%s' has no observed variance", name, dc[cx]);
 	}
 
@@ -2341,7 +2341,7 @@ struct sampleStats {
 			pv.theta.segment(0, olsr.beta.size()) = olsr.beta;
 			pv.theta[olsr.beta.size()] = olsr.var;
 			Ecov(yy,yy) = olsr.var;
-			if (olsr.var == 0.0) {
+			if (olsr.var < std::numeric_limits<double>::epsilon()) {
 				omxRaiseErrorf("%s: '%s' has no observed variance", data.name, dc[yy]);
 			}
 			Emean[yy] = pv.theta[0];
