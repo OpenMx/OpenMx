9d72b2d005df3ec9c593d9495e759c1592855887
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2019-05-24 10:02:39 -0400

    Refrain from using *_unlocked system APIs (plink)

--- src/plink2_base.h
+++ src/plink2_base.h
@@ -481,13 +481,9 @@ typedef uint32_t BoolErr;
 #  define FOPEN_RB "r"
 #  define FOPEN_WB "w"
 #  define FOPEN_AB "a"
-#  if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
-#    define fread_unlocked fread
-#    define fwrite_unlocked fwrite
-#  endif
-#  if defined(__NetBSD__)
-#    define ferror_unlocked ferror
-#  endif
+#  define fread_unlocked fread
+#  define fwrite_unlocked fwrite
+#  define ferror_unlocked ferror
 #endif
 
 #ifdef _WIN32
