697abbbaf9fc6a1ffa1c25c5b56c906af1f446c3
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2019-09-09 10:36:29 -0400

    ComputeLoop: Learn the last index if available

--- src/omxState.cpp
+++ src/omxState.cpp
@@ -704,7 +704,7 @@ void omxGlobal::reportProgress1(const char *context, std::string detail)
 	auto &cli = Global->computeLoopIndex;
 	auto &clm = Global->computeLoopMax;
 	std::string str;
-	if (cli.size() == 1 && cli[0] > 0 && clm[0] != 0 && cli[0] <= clm[0]) {
+	if (cli.size() == 1 && cli[0] > 1 && clm[0] != 0 && cli[0] <= clm[0]) {
 		str += "[";
 		double pctDone = cli[0] / double(clm[0]);
 		auto elapsed = now - Global->startTime;
