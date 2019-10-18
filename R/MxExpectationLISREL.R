OpenMx/OpenMx

commit 34868e55073aa38d1d5c1af14877d965b3206395

Optimize processing of RAM's F matrix

R/MxExpectationRAM.R
@@ -212,10 +212,10 @@ setMethod("genericExpFunConvert", signature("MxExpectationRAM"),
 		translatedNames <- fMatrixTranslateNames(fMatrix, modelname)
 		.Object@depth <- generateRAMDepth(flatModel, aMatrix, model@options)
 		if (length(translatedNames)) {
+			.Object@dataColumns <- generateDataColumns(flatModel, translatedNames, data)
 			if (mxDataObject@type == 'raw' || mxDataObject@type == 'acov') {
 				threshName <- .Object@thresholds
 				checkNumberOrdinalColumns(mxDataObject)
-				.Object@dataColumns <- generateDataColumns(flatModel, translatedNames, data)
 				verifyThresholds(flatModel, model, labelsData, data, translatedNames, threshName)
 				.Object@thresholds <- imxLocateIndex(flatModel, threshName, name)
 				retval <- generateThresholdColumns(flatModel, model, labelsData, translatedNames, data, threshName)

inst/models/passing/tangleF.R
@@ -94,12 +94,62 @@ jointData <- read.table("data/jointdata.txt", header=TRUE)
 jointData[,c(2,4,5)] <- mxFactor(jointData[,c(2,4,5)], 
 				 levels=list(c(0,1), c(0, 1, 2, 3), c(0, 1, 2)))
 
-wlsData <- mxDataWLS(jointData, type='DLS')
+acovPerm <- function(wd, perm) {
+  sz <- length(perm)
+  tcount <- colSums(!is.na(wd$thresholds))
+  names(tcount) <- NULL
+  tstart <- cumsum(c(0,tcount))
+  
+  tperm <- rep(NA, sum(tcount))
+  to <- 1
+  thresholdColumns <- match(colnames(wd$thresholds), colnames(wd$means)) #oldCol
+  newOrder <- order(perm[thresholdColumns]) # newOrder
+  
+  for (t1 in 1:length(newOrder)) {
+    oldIndex <- newOrder[t1]
+    for (cx in 1:tcount[oldIndex]) {
+      tperm[to] <- tstart[oldIndex] + cx
+      to <- to + 1
+    }
+  }
+#  print(tperm)
+
+  iperm <- rep(NA,length(perm))
+  for (xx in 1:length(iperm)) iperm[perm[xx]] = xx
+  
+  part1 <- (sz * (sz+1))/2
+  
+  mm <- matrix(NA,5,5)
+  mm[lower.tri(mm, diag = TRUE)] <- 1:part1
+  mm[upper.tri(mm)] <- t(mm)[upper.tri(mm)]
+  #  print(mm[perm,perm]-1)
+  
+  aperm <- 1:nrow(wd$acov)
+  aperm[1:part1] <- vech(mm[iperm,iperm])
+  aperm[(part1 + 1):(part1 + sz)] <-
+    aperm[(part1 + 1):(part1 + sz)][iperm]
+  aperm[(part1 + sz + 1):(part1 + sz + sum(tcount))] <-
+    aperm[(part1 + sz + 1):(part1 + sz + sum(tcount))][tperm]
+  aperm
+}
+
+if (1) {  # demonstrate the effect of data column permutation
+  wlsData <- mxDataWLS(jointData, type='WLS')
+  perm <- sample.int(5,5)
+  wlsData2 <- mxDataWLS(jointData[,perm], type='WLS')
+
+  aperm <- acovPerm(wlsData2, perm)
+
+  omxCheckCloseEnough(max(abs(diag(wlsData2$acov)[aperm] - diag(wlsData$acov))), 0, 1e-6)
+  omxCheckCloseEnough(max(abs(wlsData2$acov[aperm,aperm] - wlsData$acov)), 0, 1e-6)
+}
 
 mkModel <- function(shuffle, wls) {
 	myData <- jointData
 	if (shuffle) {
-		myData <- myData[, sample.int(ncol(myData), ncol(myData))]
+#	  dperm <- c( 4,   0,   2,   3,   1) + 1
+	  dperm <- sample.int(ncol(myData), ncol(myData))
+		myData <- myData[, dperm]
 	}
 
 	thresh <- mxMatrix("Full", 3, 3, FALSE, 0, name="Th")
@@ -118,7 +168,7 @@ mkModel <- function(shuffle, wls) {
 
 	colnames(thresh) <- paste0('z',c(2,4,5))
 
-	manifestVars <- colnames(myData)
+	manifestVars <- colnames(jointData)
 	if (shuffle) manifestVars <- sample(manifestVars, length(manifestVars))
 
 	latentVars <- 'l1'
@@ -144,7 +194,7 @@ mkModel <- function(shuffle, wls) {
 			 values=0,
 			 dimnames=list(allVars, allVars), name="A"),
 		mxMatrix("Full", 1, length(allVars),
-			 free=freeMean, #values=runif(length(allVars), -.1,.1),#remove TODO
+			 free=freeMean,
 			 dimnames=list(NULL, allVars), name="M"),
 		mxExpectationRAM(M="M", thresholds="Th"),
 		mxComputeGradientDescent())
@@ -159,7 +209,8 @@ mkModel <- function(shuffle, wls) {
 	ta1$M$values[1,'z1'] <- c(.1)
 
 	if (wls) {
-		ta1 <- mxModel(ta1, wlsData, mxFitFunctionWLS())
+	  md <- suppressWarnings(mxDataWLS(myData, type='WLS'))
+		ta1 <- mxModel(ta1, md, mxFitFunctionWLS())
 	} else {
 		ta1 <- mxModel(ta1, mxData(myData, type="raw"), mxFitFunctionML(jointConditionOn = "continuous"))
 	}
@@ -172,6 +223,6 @@ for (wls in c(FALSE,TRUE)) {
 	fit2 <- mxRun(mkModel(TRUE, wls))
 	fit3 <- mxRun(mkModel(TRUE, wls))
 
-	omxCheckCloseEnough(fit1$output$fit - fit2$output$fit, 0, 1e-8)
-	omxCheckCloseEnough(fit1$output$fit - fit3$output$fit, 0, 1e-8)
+	omxCheckCloseEnough(fit1$output$fit - fit2$output$fit, 0, 1e-6)
+	omxCheckCloseEnough(fit1$output$fit - fit3$output$fit, 0, 1e-6)
 }

src/omxMLFitFunction.cpp
@@ -31,6 +31,7 @@ static const double MIN_VARIANCE = 1e-6;
 
 struct MLFitState {
 
+	bool copiedData;
 	omxMatrix* observedCov;
 	omxMatrix* observedMeans;
 	omxMatrix* expectedCov;
@@ -44,6 +45,10 @@ static void omxDestroyMLFitFunction(omxFitFunction *oo) {
 
 	if(OMX_DEBUG) {mxLog("Freeing ML Fit Function.");}
 	MLFitState* omlo = ((MLFitState*)oo->argStruct);
+	if (omlo->copiedData) {
+		omxFreeMatrix(omlo->observedCov);
+		omxFreeMatrix(omlo->observedMeans);
+	}
 	delete omlo;
 }
 
@@ -366,12 +371,26 @@ void omxInitMLFitFunction(omxFitFunction* oo)
 	oo->populateAttrFun = omxPopulateMLAttributes;
 	oo->canDuplicate = true;
 
-	if(OMX_DEBUG) { mxLog("Processing Observed Covariance."); }
 	newObj->observedCov = omxDataCovariance(dataMat);
-	if(OMX_DEBUG) { mxLog("Processing Observed Means."); }
 	newObj->observedMeans = omxDataMeans(dataMat);
+	newObj->copiedData = false;
+
+	auto dc = oo->expectation->getDataColumns();
+	if (dc.size()) {
+		newObj->copiedData = true;
+		newObj->observedCov = omxCreateCopyOfMatrix(newObj->observedCov, oo->matrix->currentState);
+		newObj->observedMeans = omxCreateCopyOfMatrix(newObj->observedMeans, oo->matrix->currentState);
+		Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic, int> pm(dc);
+		EigenMatrixAdaptor Ecov(newObj->observedCov);
+		Ecov.derived() = (pm * Ecov * pm.transpose()).eval();
+		if (newObj->observedMeans) {
+			EigenVectorAdaptor Emean(newObj->observedMeans);
+			Emean.derived() = (pm * Emean).eval();
+		}
+	}
+
 	if(OMX_DEBUG && newObj->observedMeans == NULL) { mxLog("ML: No Observed Means."); }
-	if(OMX_DEBUG) { mxLog("Processing n."); }
+
 	newObj->n = omxDataNumObs(dataMat);
 
 	newObj->expectedCov = omxGetExpectationComponent(oo->expectation, "cov");

src/RAMInternal.h
@@ -393,8 +393,9 @@ namespace RelationalRAMExpectation {
 			return (omxRAMExpectation*) model->argStruct;
 		};
 		std::vector< omxMatrix* > &getBetween() const;
-		const Eigen::Map<Eigen::VectorXi> getDataColumns() const {
-			const Eigen::Map<Eigen::VectorXi> vec(model->dataColumnFun(model), model->numDataColumns);
+		typedef Eigen::Matrix<int, Eigen::Dynamic, 1> DataColumnType;
+		const Eigen::Map<DataColumnType> getDataColumns() const {
+			const Eigen::Map<DataColumnType> vec(model->dataColumnFun(model), model->numDataColumns);
 			return vec;
 		};
 		void dataRow(omxMatrix *out) const;
@@ -544,15 +545,13 @@ class omxRAMExpectation {
 	bool trivialF;
 	unsigned Zversion;
 	omxMatrix *_Z;
+	Eigen::VectorXi dataCols;  // composition of F permutation and expectation->dataColumns
 	std::vector< omxThresholdColumn > thresholds;
  public:
 	std::vector< dvScoreboardSetType > dvScoreboard;
 	Eigen::VectorXd hasVariance;
 	std::vector<bool> ignoreDefVar;
 	std::vector<bool> latentFilter; // false when latent
-
-	// composition of F permutation and expectation->dataColumns
-	Eigen::VectorXi dataCols;
 
  	omxRAMExpectation(omxMatrix *Z) : trivialF(false), Zversion(0), _Z(Z) {};
 	~omxRAMExpectation() {
@@ -581,22 +580,32 @@ class omxRAMExpectation {
 	bool forceSingleGroup;
 
 	template <typename T>
-	void studyF(const Eigen::MatrixBase<T> &dataColumns)
+	void studyF(const Eigen::MatrixBase<T> &dataColumns,
+		    std::vector< omxThresholdColumn > &origThresholdInfo)
 	{
 		EigenMatrixAdaptor eF(F);
 		latentFilter.assign(eF.cols(), false);
-		bool isRaw = dataColumns.size();
-		if (isRaw) dataCols.resize(eF.rows());
-		if (!eF.rows()) return;
+		dataCols.resize(eF.rows());
+		if (!eF.rows()) return;  // no manifests
 		for (int cx =0, dx=0; cx < eF.cols(); ++cx) {
 			int dest;
 			double isManifest = eF.col(cx).maxCoeff(&dest);
 			latentFilter[cx] = isManifest;
-			if (isManifest && isRaw) {
-				dataCols[dx++] = dataColumns[dest];
+			if (isManifest) {
+				int newDest = dataColumns.size()? dataColumns[dest] : dest;
+				dataCols[dx] = newDest;
+				if (origThresholdInfo.size()) {
+					omxThresholdColumn adj = origThresholdInfo[dest];
+					adj.dColumn = dx;
+					thresholds.push_back(adj);
+				}
+				dx += 1;
 			}
 		}
 	};
+	int *getDataColumnsPtr() const {
+		return (int*) dataCols.data();
+	}
 
 	std::vector< omxThresholdColumn > &getThresholdInfo() { return thresholds; }
 };

src/omxData.cpp
@@ -287,17 +287,16 @@ void omxData::newDataStatic(omxState *state, SEXP dataObj)
 
 		//for(int i = 0; i < od->obsThresholdsMat->cols; i++) {
 		for(int i = 0; i < od->cols; i++) {
-			if(levels[i] > 0){
-				omxThresholdColumn tc;
-				tc.dColumn = i;
-				tc.column = columns[i];
-				tc.numThresholds = levels[i];
-				od->thresholdCols.push_back(tc);
-				od->numFactor++; //N.B. must increment numFactor when data@type=='raw' (above) AND when data@type=='acov' (here)
-				if(OMX_DEBUG) {
-					mxLog("Column %d is ordinal with %d thresholds in threshold column %d.", 
-					      i, levels[i], columns[i]);
-				}
+			if (levels[i] == NA_INTEGER) continue;
+			omxThresholdColumn tc;
+			tc.dColumn = i;
+			tc.column = columns[i];
+			tc.numThresholds = levels[i];
+			od->thresholdCols.push_back(tc);
+			od->numFactor++; //N.B. must increment numFactor when data@type=='raw' (above) AND when data@type=='acov' (here)
+			if(OMX_DEBUG) {
+				mxLog("%s: column %d is ordinal with %d thresholds in threshold column %d.", 
+				      name, i, levels[i], columns[i]);
 			}
 		}
 	}
@@ -403,7 +402,7 @@ omxMatrix* omxDataCovariance(omxData *od)
 		return omxGetExpectationComponent(ex, "covariance");
 	}
 
-	Rf_error("%s: type='cov' data must be in matrix storage", od->name);
+	Rf_error("%s: type='%s' data must be in matrix storage", od->name, od->_type);
 }
 
 omxMatrix* omxDataAcov(omxData *od)

src/omxData.h
@@ -185,6 +185,7 @@ inline int omxKeyDataElement(omxData *od, int row, int col)
 	return cd.intData[row];
 }
 
+inline bool omxDataHasMatrix(omxData *od) { return od->dataMat != 0; }
 omxMatrix* omxDataCovariance(omxData *od);
 omxMatrix* omxDataMeans(omxData *od);
 omxMatrix* omxDataAcov(omxData *od);

src/omxExpectation.cpp
@@ -102,21 +102,20 @@ void omxExpectation::loadThresholds(int numCols, int *thresholdColumn, int *thre
 	numOrdinal = 0;
 	thresholds.reserve(numCols);
 	for(int index = 0; index < numCols; index++) {
+		omxThresholdColumn col;
+		col.dColumn = index;
 		if(thresholdColumn[index] == NA_INTEGER) {	// Continuous variable
 			if(OMX_DEBUG) {
 				mxLog("Column %d is continuous.", index);
 			}
-			omxThresholdColumn col;
 			thresholds.push_back(col);
 		} else {
-			omxThresholdColumn col;
-			col.dColumn = index;
 			col.column = thresholdColumn[index];
 			col.numThresholds = thresholdNumber[index];
 			thresholds.push_back(col);
 			if(OMX_DEBUG) {
-				mxLog("Column %d is ordinal with %d thresholds in threshold column %d.", 
-				      index, thresholdNumber[index], thresholdColumn[index]);
+				mxLog("%s: column %d is ordinal with %d thresholds in threshold column %d.", 
+				      name, index, thresholdNumber[index], thresholdColumn[index]);
 			}
 			numOrdinal++;
 		}
@@ -128,33 +127,31 @@ void omxExpectation::loadThresholds(int numCols, int *thresholdColumn, int *thre
 
 static void omxExpectationProcessDataStructures(omxExpectation* ox, SEXP rObj)
 {
-	int numCols;
-	SEXP nextMatrix, itemList, threshMatrix; 
+	int numCols=0;
 	
 	if(rObj == NULL) return;
 
-	{
-		ScopedProtect p1(nextMatrix, R_do_slot(rObj, Rf_install("dataColumns")));
-		ox->saveDataColumnsInfo(nextMatrix);
-	}
-
-	if(OMX_DEBUG) {
-		mxPrintMat("Variable mapping", ox->getDataColumns());
-	}
-	
-	auto dc = ox->getDataColumns();
-	numCols = dc.size();
 	omxData *data = ox->data;
-	for (int cx=0; cx < numCols; ++cx) {
-		int var = dc[cx];
-		data->assertColumnIsData(var);
+	bool isRaw = strEQ(omxDataType(data), "raw");
+	if (isRaw || omxDataHasMatrix(data)) {
+		ProtectedSEXP Rdc(R_do_slot(rObj, Rf_install("dataColumns")));
+		numCols = Rf_length(Rdc);
+		ox->saveDataColumnsInfo(Rdc);
+		if(OMX_DEBUG) mxPrintMat("Variable mapping", ox->getDataColumns());
+		if (isRaw) {
+			auto dc = ox->getDataColumns();
+			for (int cx=0; cx < numCols; ++cx) {
+				int var = dc[cx];
+				data->assertColumnIsData(var);
+			}
+		}
 	}
 
 	if (R_has_slot(rObj, Rf_install("thresholds"))) {
 		if(OMX_DEBUG) {
 			mxLog("Accessing Threshold matrix.");
 		}
-		ScopedProtect p1(threshMatrix, R_do_slot(rObj, Rf_install("thresholds")));
+		ProtectedSEXP threshMatrix(R_do_slot(rObj, Rf_install("thresholds")));
 
 		if(INTEGER(threshMatrix)[0] != NA_INTEGER) {
 			if(OMX_DEBUG) {
@@ -168,12 +165,11 @@ static void omxExpectationProcessDataStructures(omxExpectation* ox, SEXP rObj)
 			 * and fill all the thresholds with {NULL, 0, 0}.
 			 * However the current path does not have a lot of overhead. */
 			int* thresholdColumn, *thresholdNumber;
-			{ScopedProtect pc(nextMatrix, R_do_slot(rObj, Rf_install("thresholdColumns")));
-				thresholdColumn = INTEGER(nextMatrix);
-			}
-			{ScopedProtect pi(itemList, R_do_slot(rObj, Rf_install("thresholdLevels")));
-				thresholdNumber = INTEGER(itemList);
-			}
+			ProtectedSEXP nextMatrix (R_do_slot(rObj, Rf_install("thresholdColumns")));
+			thresholdColumn = INTEGER(nextMatrix);
+			ProtectedSEXP itemList(R_do_slot(rObj, Rf_install("thresholdLevels")));
+			thresholdNumber = INTEGER(itemList);
+
 			ox->loadThresholds(numCols, thresholdColumn, thresholdNumber);
 		} else {
 			if (OMX_DEBUG) {

src/omxExpectation.h
@@ -75,11 +75,10 @@ class omxExpectation {					// An Expectation
 		numDataColumns = Rf_length(vec);
 		dataColumnsPtr = INTEGER(vec);
 	}
-	const Eigen::Map<Eigen::VectorXi> getDataColumnsInternal() {
-		return Eigen::Map<Eigen::VectorXi>(dataColumnsPtr, numDataColumns);
-	}
-	const Eigen::Map<Eigen::VectorXi> getDataColumns() {
-		return Eigen::Map<Eigen::VectorXi>(this->dataColumnFun(this), numDataColumns);
+
+	typedef Eigen::Matrix<int, Eigen::Dynamic, 1> DataColumnType;
+	const Eigen::Map<DataColumnType> getDataColumns() {
+		return Eigen::Map<DataColumnType>(this->dataColumnFun(this), numDataColumns);
 	}
 	std::vector< omxThresholdColumn > &getThresholdInfo() {
 		return this->thresholdInfoFun(this);

commit cd9a95ca304f9b21d18bbfa393cf2bd10e6e9c41

RAM: Permute covariance data correctly to match model
Bug introduced 07 Jan 2017 in 34868e55073

R/MxExpectationLISREL.R
@@ -445,7 +445,7 @@ setMethod("genericExpFunConvert", signature("MxExpectationLISREL"),
 		#
 		# Raw data error checking
 		#  Set the canonical order of observed variable names.
-		translatedNames <- c(dimnames(lyMatrix)[[1]], dimnames(lxMatrix)[[1]]) #fMatrixTranslateNames(fMatrix, modelname) #Rearrange the rownames of F to match the order of the columns
+		translatedNames <- c(dimnames(lyMatrix)[[1]], dimnames(lxMatrix)[[1]]) #modelManifestNames(fMatrix, modelname) #Rearrange the rownames of F to match the order of the columns
 		.Object@depth <- generateLISRELDepth(flatModel, beMatrix2, model@options) #Find out how many iterations of I + BE + BE^2 + ... are need until nilpotency.
 		if (dataIsRawish(mxDataObject)) {
 			threshName <- .Object@thresholds
@@ -809,7 +809,7 @@ generateLISRELDepth <- function(flatModel, aMatrixName, modeloptions) {
 
 
 #
-#fMatrixTranslateNames <- function(fMatrix, modelName) {
+#modelManifestNames <- function(fMatrix, modelName) {
 #	retval <- character()
 #	colNames <- dimnames(fMatrix)[[2]]
 #	for(i in 1:nrow(fMatrix)) {

R/MxExpectationRAM.R
@@ -197,7 +197,7 @@ setMethod("genericExpFunConvert", signature("MxExpectationRAM"),
 				stop(msg, call. = FALSE)
 			}
 		}
-		translatedNames <- fMatrixTranslateNames(fMatrix, modelname)
+		translatedNames <- modelManifestNames(fMatrix, modelname)
 		.Object@depth <- generateRAMDepth(flatModel, aMatrix, model@options)
 		if (length(translatedNames)) {
 			.Object@dataColumnNames <- translatedNames
@@ -346,7 +346,7 @@ generateDepthHelper <- function(aValues, currentProduct, depth, maxdepth) {
 	}
 }
 
-fMatrixTranslateNames <- function(fMatrix, modelName) {
+modelManifestNames <- function(fMatrix, modelName) {
 	retval <- character()
 	if (length(fMatrix) == 0) return(retval)
 	colNames <- dimnames(fMatrix)[[2]]

R/MxRAMtoML.R
@@ -87,7 +87,7 @@ convertRAMtoMLModel <- function(model, job, namespace) {
 		model <- mxModel(model, algebra)
 	}
 
-    translatedNames <- fMatrixTranslateNames(job[[fName]]@values, modelname)
+    translatedNames <- modelManifestNames(job[[fName]]@values, modelname)
 	dataset <- job[[modelname]]@data
 	if (dataset@type == 'raw') {
 		objectiveType <- as.symbol('mxFIMLObjective')

inst/models/passing/tangleF.R
@@ -1,9 +1,41 @@
 library(OpenMx)
+library(testthat)
 
 # NPSOL trips up on WLS for some reason
 if (mxOption(NULL,"Default optimizer") == 'NPSOL') stop("SKIP")
 
 mxOption(NULL, "Standard Errors", "No")
+
+data(demoOneFactor)
+
+mkModel <- function() {
+  manifests <- sample(names(demoOneFactor), ncol(demoOneFactor))
+  mxModel("One Factor", type="RAM",
+          manifestVars = manifests,
+          latentVars = c("G"),
+          mxPath(from="G", to=manifests,
+                 labels=paste0("loading_",manifests)),
+          mxPath(from=manifests, arrows=2),
+          mxPath(from="G", arrows=2, free=FALSE, values=1.0),
+          mxPath(from="one", to=manifests,
+                 labels=paste0("mean_",manifests)),
+          mxData(cov(demoOneFactor), type="cov", numObs=500,
+                 means=colMeans(demoOneFactor)))
+}
+
+interest <- c(paste0("loading_x",1:5),paste0("mean_x",1:5))
+c1 <- c()
+for (rep in 1:5) {
+  fit <- mxRun(mkModel())
+  if (is.null(c1)) {
+    c1 <- coef(fit)[interest]
+  } else {
+    expect_equivalent(c1 - coef(fit)[interest],
+                      rep(0,10), tolerance=1e-6)
+  }
+}
+
+# -------
 
 numManifestsPerLatent <- 5
 

src/omxMLFitFunction.cpp
@@ -405,10 +405,10 @@ void MLFitState::init()
 		newObj->observedMeans = omxCreateCopyOfMatrix(newObj->observedMeans, oo->matrix->currentState);
 		Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic, int> pm(dc);
 		EigenMatrixAdaptor Ecov(newObj->observedCov);
-		Ecov.derived() = (pm * Ecov * pm.transpose()).eval();
+		Ecov.derived() = (pm.transpose() * Ecov * pm).eval();
 		if (newObj->observedMeans) {
 			EigenVectorAdaptor Emean(newObj->observedMeans);
-			Emean.derived() = (pm * Emean).eval();
+			Emean.derived() = (pm.transpose() * Emean).eval();
 		}
 	}
 