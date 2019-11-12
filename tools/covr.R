054a13610759dde1d26c47fbd67f762676d98ebf
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2019-11-12 11:01:55 -0500

    Update covr script

---
+++ tools/covr.R
@@ -0,0 +1,22 @@
+library(devtools)
+devtools::install_github("jpritikin/covr")
+library(covr)
+library(roxygen2)
+options(covr.gcov = "gcov")
+withr::with_makevars(getOption("covr.flags"), assignment = "+=", {
+  utils::install.packages(
+    ".", repos = NULL, type = "source",
+    INSTALL_opts = c("--example",  "--install-tests", "--with-keep.source",
+      "--with-keep.parse.data", "--no-multiarch")
+  )
+})
+roxygenize('.', roclets=c('rd'))
+options(digits=15)
+c1 <- covr::package_coverage(type=c("tests","examples"), quiet=TRUE, pre_clean=FALSE)
+pct <- percent_coverage(c1, by="line")
+print(pct)
+if (pct < 61.1) {
+  print(paste("Coverage dropped to", pct))
+  q(status=1)
+}
+covr::codecov(coverage = c1)
