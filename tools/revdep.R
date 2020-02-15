328e164d3cb451347d4999556bff192cb032ca62
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-02-15 10:05:06 -0500

    Adjust revdep script again

--- tools/revdep.R
+++ tools/revdep.R
@@ -1,7 +1,7 @@
 library(revdepcheck)
 
-revdep_reset()
 revdep_env_vars(force_suggests = FALSE)
-revdep_check(dependencies = c("Depends", "Imports", "LinkingTo"),
+revdep_reset('OpenMx')
+revdep_check('OpenMx',
+             dependencies = c("Depends", "Imports", "LinkingTo"),
              timeout=as.difftime(15, units = "mins"))
-
