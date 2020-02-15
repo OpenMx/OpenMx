37cb16014ccb680bb3b7318fc5cc62427561806f
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-02-15 09:36:16 -0500

    Adjust revdep script

--- tools/revdep.R
+++ tools/revdep.R
@@ -2,6 +2,6 @@ library(revdepcheck)
 
 revdep_reset()
 revdep_env_vars(force_suggests = FALSE)
-revdep_check(dependencies = c("Depends", "Imports", "LinkingTo"), quiet=FALSE,
+revdep_check(dependencies = c("Depends", "Imports", "LinkingTo"),
              timeout=as.difftime(15, units = "mins"))
-revdep_reset()
+
