e190fc3a7ec9e891cb6729538cd14bfb510d8d1d
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-02-15 14:05:11 -0500

    Adjust revdep script again #2

--- tools/revdep.R
+++ tools/revdep.R
@@ -1,7 +1,9 @@
 library(revdepcheck)
 
+args <- commandArgs(trailingOnly = TRUE)
+
 revdep_env_vars(force_suggests = FALSE)
-revdep_reset('OpenMx')
-revdep_check('OpenMx',
+revdep_reset(args[1])
+revdep_check(args[1],
              dependencies = c("Depends", "Imports", "LinkingTo"),
              timeout=as.difftime(15, units = "mins"))
