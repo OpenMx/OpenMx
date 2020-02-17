193f0409fcb23778e93562803fbc279e56cf519c
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-02-17 11:48:55 -0500

    Continue tweaking revdep script

--- tools/revdep.R
+++ tools/revdep.R
@@ -2,8 +2,9 @@ library(revdepcheck)
 
 args <- commandArgs(trailingOnly = TRUE)
 
+print(args[1])
+
 revdep_env_vars(force_suggests = FALSE)
-revdep_reset(args[1])
 revdep_check(args[1],
              dependencies = c("Depends", "Imports", "LinkingTo"),
              timeout=as.difftime(15, units = "mins"))
