8fa4d8b67437e584eaea2ecaa013bae917371780
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-02-17 21:30:28 -0500

    Continue revdep adjustment

--- tools/revdep.R
+++ tools/revdep.R
@@ -6,7 +6,12 @@ print(getwd())
 print(args[1])
 print(dir('build'))
 
-revdep_env_vars(force_suggests = FALSE)
+e <- c('LD_PRELOAD', paste0('NAMESERVER',1:2), 'FORCE_DNS_OVER_TCP',
+  'PATH', 'LD_LIBRARY_PATH', 'PKG_CONFIG_PATH', 'IMX_SAFE',
+  'INSTALLMAKEFLAGS', 'MAKEFLAGS', 'R_LIBS_USER')
+
 revdep_check(args[1],
              dependencies = c("Depends", "Imports", "LinkingTo"),
-             timeout=as.difftime(15, units = "mins"))
+             timeout=as.difftime(15, units = "mins"),
+             env=c(revdep_env_vars(force_suggests = FALSE),
+                   sapply(e, Sys.getenv)))
