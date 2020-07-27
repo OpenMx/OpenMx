993e44c2309d3d0d5667b89a127413897c2ed846
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-07-27 13:40:13 -0400

    Skip on CSOLNP

--- inst/models/passing/AnalyticConstraintJacobians--Eq_and_Ineq.R
+++ inst/models/passing/AnalyticConstraintJacobians--Eq_and_Ineq.R
@@ -4,9 +4,9 @@
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,8 +16,7 @@
 library(OpenMx)
 #mxOption(NULL,"Default optimizer","SLSQP")
 
-#CSOLNP still fails in a platform-specific manner:
-if(mxOption(NULL,"Default optimizer")=="CSOLNP" && .Platform$OS.type=="windows" && .Platform$r_arch=="i386"){stop("SKIP")}
+if (mxOption(NULL,"Default optimizer") == 'CSOLNP') stop("SKIP")
 
 library(mvtnorm)
 
@@ -172,7 +171,7 @@ m3 <- mxModel(
 	mxExpectationNormal(covariance="Sigma",means="Mu",dimnames=c("y1","y2","y3"),thresholds="Tau",threshnames=c("y1","y2","y3")),
 	safeT,#safeT2,
 	mxConstraint(diag2vec(Sigma)==ONE,name="identifying",jac="eqJac"),
-	eqjsub, taueqjac, eqjac#, 
+	eqjsub, taueqjac, eqjac#,
 	#tauineqjac, ineqjsub, ineqjac, sgn
 )
 m4 <- mxRun(m3)
@@ -209,7 +208,7 @@ m5 <- mxModel(
 	mxExpectationNormal(covariance="Sigma",means="Mu",dimnames=c("y1","y2","y3"),thresholds="Tau",threshnames=c("y1","y2","y3")),
 	safeT2,
 	mxConstraint(diag2vec(Sigma)==ONE,name="identifying",jac="eqJac"),
-	eqjsub, taueqjac, eqjac, 
+	eqjsub, taueqjac, eqjac,
 	tauineqjac, ineqjsub, ineqjac, sgn
 )
 m6 <- mxRun(m5)
@@ -225,4 +224,3 @@ omxCheckCloseEnough(mxEval(Sigma,m6,T)[c(2,3,6)],c(0.5,0.5,0.5),0.05)
 omxCheckCloseEnough(diag(mxEval(Sigma,m6,T)),c(1,1,1),as.numeric(mxOption(NULL,"Feasibility tolerance")))
 omxCheckCloseEnough(mxEval(Tau,m6,T)[1,],mxEval(Tau,m2,T)[1,],5e-4)
 omxCheckCloseEnough(mxEval(Sigma,m6,T),mxEval(Sigma,m2,T),5e-4)
-
