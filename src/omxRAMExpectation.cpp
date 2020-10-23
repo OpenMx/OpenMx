OpenMx/OpenMx

commit 89b4d50c1250101b8a027dd4ff1d982f6c24f232

Clarify state cloning, OpenMP negotiation

src/omxRAMExpectation.cpp
@@ -416,7 +416,8 @@ void omxRAMExpectation::init()
 		pcalc.setAlgo(0, hasProductNodes, useSparse);
 
 		currentState->restoreParam(estSave);
-	} else {
+	}
+  if (!currentState->isTopState()) {
     auto pex = (omxRAMExpectation*) currentState->getParent(this);
     if (pex->slope) {
       numExoPred = pex->numExoPred;
@@ -432,6 +433,7 @@ void omxRAMExpectation::studyExoPred()
 
 	Eigen::VectorXd estSave;
 	currentState->setFakeParam(estSave);
+  if (!currentState->isTopState()) OOPS; // can data->defVars.erase only once
 	omxRecompute(A, 0);
 
 	EigenMatrixAdaptor eA(A);

src/Compute.cpp
@@ -663,6 +663,7 @@ void FitContext::init()
 	openmpUser = false;
 	ordinalRelativeError = 0;
 	computeCount = 0;
+  permitParallel = false;
 
 	hess.resize(numParam, numParam);  // TODO why needed?
 	ihess.resize(numParam, numParam);  // TODO why needed?
@@ -1408,7 +1409,7 @@ void FitContext::createChildren1()
 
 	for(int ii = 0; ii < numThreads; ii++) {
 		FitContext *kid = new FitContext(this, varGroup);
-		kid->state = new omxState(state);
+		kid->state = new omxState(state, openmpUser);
 		kid->state->initialRecalc(kid);
 		childList.push_back(kid);
 	}

src/Compute.h
@@ -208,8 +208,8 @@ class FitContext {
 
 	FitContext(omxState *_state);
 	FitContext(FitContext *parent, FreeVarGroup *group);
-  bool permitParallel; // whether openmpUser is permitted
 	bool openmpUser;  // whether some fitfunction/expectation uses OpenMP
+  bool permitParallel; // whether openmpUser is permitted
   void createChildren(omxMatrix *alg=0, bool permitParallel=false);
   int numOptimizerThreads() { return (childList.size() && !openmpUser)? childList.size() : 1; }
 	void destroyChildren();

src/omxFIMLFitFunction.cpp
@@ -850,13 +850,10 @@ void omxFIMLFitFunction::compute(int want, FitContext *fc)
     } else {
 			omxMatrix *pfitMat = fc->getParentState()->getMatrixFromIndex(off->matrix);
       auto *pff = (omxFIMLFitFunction*) pfitMat->fitFunction;
-      if (pff->openmpUser) {
-        parent = pff;
-        elapsed.resize(ELAPSED_HISTORY_SIZE);
-        curElapsed = NA_INTEGER;
-      } else {
-        if (!indexVector.size()) sortData(off);
-      }
+      if (!pff->openmpUser) OOPS;
+      parent = pff;
+      elapsed.resize(ELAPSED_HISTORY_SIZE);
+      curElapsed = NA_INTEGER;
 		}
 		builtCache = true;
 	}

src/omxLISRELExpectation.cpp
@@ -580,7 +580,7 @@ void omxLISRELExpectation::init()
 	} else LISobj->means  = 	NULL;
 	//TODO: Adjust means processing to allow only Xs or only Ys
 
-	if (currentState->isClone()) {
+	if (!currentState->isTopState()) {
     auto pex = (omxLISRELExpectation*) currentState->getParent(this);
     if (pex->slope) {
       numExoPred = pex->numExoPred;

src/omxState.cpp
@@ -407,7 +407,8 @@ void omxState::loadDefinitionVariables(bool start)
 	}
 }
 
-omxState::omxState(omxState *src) : wantStage(0), parent(src), hasFakeParam(false)
+omxState::omxState(omxState *src, bool isTeam) :
+  wantStage(0), parent(src), workBoss(isTeam? src : 0), hasFakeParam(false)
 {
 	init();
 

src/omxState.h
@@ -320,13 +320,15 @@ class omxState {
 	static int nextId;
 	int stateId;
 	int wantStage; // hack because omxRecompute doesn't take 'want' as a parameter TODO
-	omxState *parent;
+	omxState *parent;     // for read-only access to shared state
+	omxState *workBoss; // for OpenMP when multiple omxState want to cooperate
 	bool hasFakeParam;
  public:
 	int getWantStage() const { return wantStage; }
 	void setWantStage(int stage);
 	int getId() const { return stateId; }
-	bool isClone() const { return parent != 0; }
+	bool isClone() const { return workBoss != 0; } // rename to isWorkBoss TODO
+  bool isTopState() const { return parent == 0; }
 
 	std::vector< omxMatrix* > matrixList;
 	std::vector< omxMatrix* > algebraList;
@@ -335,8 +337,8 @@ class omxState {
 	std::vector< omxConstraint* > conListX;
   void constraintPreeval(FitContext *fc) { for (auto c1 : conListX) c1->preeval(fc); }
 
-	omxState() : wantStage(0), parent(0), hasFakeParam(false) { init(); };
-	omxState(omxState *src);
+	omxState() : wantStage(0), parent(0), workBoss(0), hasFakeParam(false) { init(); };
+	omxState(omxState *src, bool isTeam);
 	void initialRecalc(FitContext *fc);
 	void omxProcessMxMatrixEntities(SEXP matList);
 	void omxProcessFreeVarList(SEXP varList);

src/omxWLSFitFunction.cpp
@@ -194,7 +194,7 @@ void omxWLSFitFunction::prepData()
 
 	omxData* dataMat = oo->expectation->data;
 
-	if (!matrix->currentState->isClone()) {
+	if (matrix->currentState->isTopState()) {
 		std::vector<int> exoPred;
 		expectation->getExogenousPredictors(exoPred);
 		// how to prohibit def vars? TODO

commit cfdb88770742f8200d19c007ebca0062895d75d1

Fix WLS regression introduced in 89b4d50c125010

src/omxRAMExpectation.cpp
@@ -453,11 +453,11 @@ void omxRAMExpectation::init()
 
 void omxRAMExpectation::studyExoPred()
 {
+  if (!currentState->isTopState()) return; // done in init(); defVar already modified
 	if (data->defVars.size() == 0 || !M || !M->isSimple() || !S->isSimple()) return;
 
 	Eigen::VectorXd estSave;
 	currentState->setFakeParam(estSave);
-  if (!currentState->isTopState()) OOPS; // can data->defVars.erase only once
 	omxRecompute(A, 0);
 
 	EigenMatrixAdaptor eA(A);