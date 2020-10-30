93fa290788e6a62da5463f62b0020198f225251c
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-10-29 23:20:02 -0400

    Remove tests that no longer make sense

--- inst/models/passing/DefaultComputePlanTest.R
+++ inst/models/passing/DefaultComputePlanTest.R
@@ -18,24 +18,3 @@ expect_equivalent(
          no=plan$steps$CI$plan$fitfunction),
 	"foo.fitfunction")
 omxCheckEquals(plan$steps$ND$fitfunction,"foo.fitfunction")
-
-ol <- options()$mxOption
-ol$"Gradient algorithm" <- "forward"
-ol$"Gradient iterations" <- 2L
-ol$"Gradient step size" <- 1e-3
-ol$"Calculate Hessian" <- "No"
-ol$"Standard Errors" <- "No"
-plan <- omxDefaultComputePlan(intervals=TRUE,optionList=ol)
-omxCheckEquals(names(plan$steps),c("GD","CI","RD","RE"))
-omxCheckEquals(plan$steps$GD$gradientAlgo,"forward")
-omxCheckEquals(plan$steps$GD$gradientIterations,2L)
-omxCheckEquals(plan$steps$GD$gradientStepSize,0.001)
-if(mxOption(NULL,"Default optimizer")=="SLSQP"){
-	omxCheckEquals(plan$steps$CI$plan$plan$gradientAlgo,"forward")
-	omxCheckEquals(plan$steps$CI$plan$plan$gradientIterations,2L)
-	omxCheckEquals(plan$steps$CI$plan$plan$gradientStepSize,0.001)
-} else{
-	omxCheckEquals(plan$steps$CI$plan$gradientAlgo,"forward")
-	omxCheckEquals(plan$steps$CI$plan$gradientIterations,2L)
-	omxCheckEquals(plan$steps$CI$plan$gradientStepSize,0.001)
-}
--- inst/models/passing/Simple--CheckCode6.R
+++ inst/models/passing/Simple--CheckCode6.R
@@ -4,9 +4,9 @@
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,9 +33,3 @@ if (0) {
 omxCheckError(mxOption(m1, "Gradient algorithm", "forward"), "'Gradient algorithm' is a global option and cannot be set on models.
 To change 'Gradient algorithm' globally, use, e.g.:
 mxOption(NULL, 'Gradient algorithm', 'forward')")
-
-mxOption(NULL, "Gradient algorithm", "central")
-omxCheckEquals(mxComputeGradientDescent()$gradientAlgo, "central")
-
-mxOption(NULL, "Gradient algorithm", "forward")
-omxCheckEquals(mxComputeGradientDescent()$gradientAlgo, "forward")
