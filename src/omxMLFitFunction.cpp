3c5c0c3057daafe3d09279a1bf3c9bcdd3b1c782
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2021-01-01 12:38:39 -0500

    Update copyright

--- src/omxMLFitFunction.cpp
+++ src/omxMLFitFunction.cpp
@@ -1,5 +1,5 @@
  /*
- *  Copyright 2007-2019 by the individuals mentioned in the source code history
+ *  Copyright 2007-2020 by the individuals mentioned in the source code history
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
