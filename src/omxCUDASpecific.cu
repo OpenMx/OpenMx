02c0a9cbac4c355d4d7e06806db170c7826d0961
rtm9zc <rtm9zc@virginia.edu>
2021-11-04 21:18:58 -0400

    CUDA integration with R package build!

--- src/omxCUDASpecific.cu
+++ src/omxCUDASpecific.cu
@@ -15,3 +15,53 @@
  *
  */
 #include "omxCUDASpecific.h"
+#include <stdio.h>
+
+// Squares each cell value in an array of integers
+__global__ void square_array(int *array, int arrayCount)
+{
+ int idx = threadIdx.x + blockIdx.x * blockDim.x;
+ if (idx < arrayCount)
+ {
+   array[idx] *= array[idx];
+ }
+}
+
+void callHelloWorld(){
+  printf("Hello, World from .cu!\n");
+
+  int n = 1024;
+
+  int *array;
+  size_t bytes = n*sizeof(int);
+  array = (int*)malloc(bytes);
+
+  for (int i =0; i <n; i++){
+    array[i] = i;
+  }
+
+  int *d_a;
+
+  cudaMalloc(&d_a, n*sizeof(int));
+
+  int blockSize;
+  int minGridSize;
+  int gridSize;
+
+  cudaOccupancyMaxPotentialBlockSize( &minGridSize, &blockSize,
+                                      square_array);
+  // Round up according to array size
+  gridSize = (n + blockSize - 1) / blockSize;
+
+  cudaMemcpy(d_a, array, n*sizeof(int), cudaMemcpyHostToDevice);
+
+  square_array<<< gridSize, blockSize >>>(d_a, n);
+
+  cudaDeviceSynchronize();
+
+  cudaMemcpy(array, d_a, n*sizeof(int), cudaMemcpyDeviceToHost);
+
+  for (int i =0; i <n; i+=100){
+    printf("Square of %d is %d\n", i, array[i]);
+  }
+}
