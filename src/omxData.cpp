e831cf0e932ed261bf850ba24324f8daa1338b74
mhunter1 <mhunter.ou@gmail.com>
2022-05-07 13:49:04 -0400

    Remove missing obs from denominator in WLS summary stats

--- src/omxData.cpp
+++ src/omxData.cpp
@@ -1778,16 +1778,16 @@ void OLSRegression::setResponse(ColumnData &cd, WLSVarData &pv,
 		resid = ycol - pred * beta;
 	} else {
     predCov.resize(1, 1);
-    predCov(0,0) = 1 / totalWeight;
+    predCov(0,0) = 1 / (totalWeight - naCount);
 		beta.resize(1);
-		beta[0] = (ycolF.array() * rowMultF).sum() / totalWeight;
+		beta[0] = (ycolF.array() * rowMultF).sum() / (totalWeight - naCount);
 		resid = ycol.array() - beta[0];
 	}
 	subsetVectorStore(resid, [&](int rx){ return !std::isfinite(ycol[rx]); }, 0.);
   double residSqSum = (resid.square() * rowMult).sum();
-  double sigma2 = residSqSum / (totalWeight - beta.size());
+  double sigma2 = residSqSum / (totalWeight - naCount - beta.size());
   vcov = sigma2 * predCov.selfadjointView<Eigen::Lower>();
-	var = residSqSum / totalWeight;
+	var = residSqSum / (totalWeight - naCount);
 }
 
 void OLSRegression::calcScores()
@@ -2142,7 +2142,7 @@ struct PolyserialCor : NewtonRaphsonObjective {
 		double den = 0;
 		for (int tx=0; tx < numThr; ++tx) den += Rf_dnorm4(ov.theta[tx], 0., 1., 0);
 		double rho = (zeeF * ycolF.cast<double>().array() * rowMultF).sum() /
-			(totalWeight * sqrt(var) * den);
+			((totalWeight - naCount) * sqrt(var) * den);
 		if (!std::isfinite(rho)) mxThrow("PolyserialCor starting value not finite");
 		if (fabs(rho) >= 1.0) rho = 0;
 		if (data.verbose >= 3) mxLog("starting ps rho = %f", rho);
@@ -2299,8 +2299,8 @@ struct PolychoricCor : NewtonRaphsonObjective {
 		subsetVector(y1, notMissingF, y1F);
 		subsetVector(y2, notMissingF, y2F);
 		subsetVector(rowMult, notMissingF, rowMultF);
-		Eigen::ArrayXd y1c = y1F.cast<double>() - (y1F.cast<double>() * rowMultF).sum() / totalWeight;
-		Eigen::ArrayXd y2c = y2F.cast<double>() - (y2F.cast<double>() * rowMultF).sum() / totalWeight;
+		Eigen::ArrayXd y1c = y1F.cast<double>() - (y1F.cast<double>() * rowMultF).sum() / (totalWeight - naCount);
+		Eigen::ArrayXd y2c = y2F.cast<double>() - (y2F.cast<double>() * rowMultF).sum() / (totalWeight - naCount);
 		double rho = (y1c * y2c * rowMultF).sum() /
 			(sqrt((y1c*y1c*rowMultF).sum() * (y2c*y2c*rowMultF).sum()));
 		if (fabs(rho) >= 1.0) rho = 0;
@@ -2476,14 +2476,14 @@ struct PearsonCor {
 	{
 		int rows = pv1.resid.size();
 
-		rho = 2.*(pv1.resid * pv2.resid * rowMult).sum() /
-			((pv1.resid.square() * rowMult).sum()+(pv2.resid.square() * rowMult).sum());
-		double R = (1 - rho*rho);
-		double i2r = 1./(2.*R);
 		double var_y1 = pv1.theta[pv1.theta.size()-1];
 		double sd_y1 = sqrt(var_y1);
 		double var_y2 = pv2.theta[pv2.theta.size()-1];
 		double sd_y2 = sqrt(var_y2);
+		rho = 2.*(pv1.resid * pv2.resid * rowMult).sum() /
+			((pv1.resid.square() * rowMult).sum()+(pv2.resid.square() * rowMult).sum());
+		double R = (1 - rho*rho);
+		double i2r = 1./(2.*R);
 
 		int numPred = pred1.size() + pred2.size();
 		scores.resize(rows, 4 + numPred + 1);
