e08c4cc5a6896d7af6a59b6ce94d21d40bf3085b
RMKirkpatrick <rkirkpatrick2@vcu.edu>
2024-12-17 10:01:04 -0600

    Make optimizer-specific parts of new test script finer-grained; set RNG seed for `mxTryHard()`.

--- inst/models/passing/Active_bounds_and_Hessian_test.R
+++ inst/models/passing/Active_bounds_and_Hessian_test.R
@@ -36,68 +36,72 @@ summary(factorModelFit <- mxRun(factorModel))
 omxCheckEquals(factorModelFit$output$status$code,0)
 omxCheckTrue(factorModelFit$output$infoDefinite)
 
-if (mxOption(NULL, "Default optimizer") == "SLSQP") { #<--TODO
-	
-	factorModel2 <- mxModel(
-		"One Factor",
-		# Notice the lbound:
-		mxMatrix("Full", 5, 1, values=0.8, lbound=c(0.8,rep(NA,4)), free=TRUE, name="A"), 
-		mxMatrix("Symm", 1, 1, values=1, free=FALSE, name="L"),
-		mxMatrix("Diag", 5, 5, values=1, free=TRUE, name="U"),
-		mxAlgebra(A %*% L %*% t(A) + U, name="R"),
-		mxExpectationNormal(covariance = "R", dimnames = names(demoOneFactor)),
-		mxFitFunctionML(),
-		mxData(cov(demoOneFactor), type="cov", numObs=500)
-	)
-	fmf2 <- mxRun(factorModel2)
-	summary(fmf2)
-	omxCheckEquals(fmf2$output$status$code,0)
+factorModel2 <- mxModel(
+	"One Factor",
+	# Notice the lbound:
+	mxMatrix("Full", 5, 1, values=0.8, lbound=c(0.8,rep(NA,4)), free=TRUE, name="A"), 
+	mxMatrix("Symm", 1, 1, values=1, free=FALSE, name="L"),
+	mxMatrix("Diag", 5, 5, values=1, free=TRUE, name="U"),
+	mxAlgebra(A %*% L %*% t(A) + U, name="R"),
+	mxExpectationNormal(covariance = "R", dimnames = names(demoOneFactor)),
+	mxFitFunctionML(),
+	mxData(cov(demoOneFactor), type="cov", numObs=500)
+)
+fmf2 <- mxRun(factorModel2)
+summary(fmf2)
+omxCheckTrue(fmf2$output$status$code!=5)
+if(mxOption(NULL,"Default optimizer")!="CSOLNP"){
 	omxCheckTrue(fmf2$output$infoDefinite)
-	
-	#Custom compute plan:
-	plan <- omxDefaultComputePlan()
-	plan$steps <- list(GD=plan$steps$GD,RE=plan$steps$RE)
-	factorModel3 <- mxModel(
-		"One Factor",
-		plan,
-		mxMatrix("Full", 5, 1, values=0.8, lbound=c(0.8,rep(NA,4)), free=TRUE, name="A"),
-		mxMatrix("Symm", 1, 1, values=1, free=FALSE, name="L"),
-		mxMatrix("Diag", 5, 5, values=1, free=TRUE, name="U"),
-		mxAlgebra(A %*% L %*% t(A) + U, name="R"),
-		mxExpectationNormal(covariance = "R", dimnames = names(demoOneFactor)),
-		mxFitFunctionML(),
-		mxData(cov(demoOneFactor), type="cov", numObs=500)
-	)
-	fmf3 <- mxRun(factorModel3)
-	summary(fmf3)
-	omxCheckEquals(fmf3$output$status$code,0)
+}
+
+#Custom compute plan:
+plan <- omxDefaultComputePlan()
+plan$steps <- list(GD=plan$steps$GD,RE=plan$steps$RE)
+factorModel3 <- mxModel(
+	"One Factor",
+	plan,
+	mxMatrix("Full", 5, 1, values=0.8, lbound=c(0.8,rep(NA,4)), free=TRUE, name="A"),
+	mxMatrix("Symm", 1, 1, values=1, free=FALSE, name="L"),
+	mxMatrix("Diag", 5, 5, values=1, free=TRUE, name="U"),
+	mxAlgebra(A %*% L %*% t(A) + U, name="R"),
+	mxExpectationNormal(covariance = "R", dimnames = names(demoOneFactor)),
+	mxFitFunctionML(),
+	mxData(cov(demoOneFactor), type="cov", numObs=500)
+)
+fmf3 <- mxRun(factorModel3)
+summary(fmf3)
+omxCheckTrue(fmf3$output$status$code!=5)
+if(mxOption(NULL,"Default optimizer")=="SLSQP"){
 	omxCheckTrue(!length(fmf3$output$infoDefinite))
 	#^^^Because there was no MxComputeHessianQuality step in the compute plan.
-	
-	factorModel4 <- mxModel(
-		"One Factor",
-		# Note first loading fixed to 0.8:
-		mxMatrix("Full", 5, 1, values=0.8, free=c(F,rep(T,4)), name="A"),
-		mxMatrix("Symm", 1, 1, values=1, free=FALSE, name="L"),
-		mxMatrix("Diag", 5, 5, values=1, free=TRUE, name="U"),
-		mxAlgebra(A %*% L %*% t(A) + U, name="R"),
-		mxExpectationNormal(covariance = "R", dimnames = names(demoOneFactor)),
-		mxFitFunctionML(),
-		mxData(cov(demoOneFactor), type="cov", numObs=500)
-	)
-	fmf4 <- mxRun(factorModel4)
-	summary(fmf4)
-	fmf4$output$standardErrors
-	#^^^The standard errors should NOT be considered valid. 
-	#It's "cheating" to fix an unknown parameter that, when freed, has an active bound at the MLE.
-	
-	fmf2.a <- mxTryHard(model=factorModel2,greenOK=T,checkHess=T)
-	summary(fmf2.a)
-	omxCheckEquals(fmf2.a$output$status$code,0)
-	omxCheckTrue(fmf2.a$output$infoDefinite)
-	
-	fmf3.a <- mxTryHard(model=factorModel3,greenOK=T,checkHess=T)
-	omxCheckEquals(fmf3.a$output$status$code,0)
-	omxCheckTrue(!length(fmf3.a$output$infoDefinite))
-	
 }
+
+factorModel4 <- mxModel(
+	"One Factor",
+	# Note first loading fixed to 0.8:
+	mxMatrix("Full", 5, 1, values=0.8, free=c(F,rep(T,4)), name="A"),
+	mxMatrix("Symm", 1, 1, values=1, free=FALSE, name="L"),
+	mxMatrix("Diag", 5, 5, values=1, free=TRUE, name="U"),
+	mxAlgebra(A %*% L %*% t(A) + U, name="R"),
+	mxExpectationNormal(covariance = "R", dimnames = names(demoOneFactor)),
+	mxFitFunctionML(),
+	mxData(cov(demoOneFactor), type="cov", numObs=500)
+)
+fmf4 <- mxRun(factorModel4)
+summary(fmf4)
+fmf4$output$standardErrors
+#^^^The standard errors should NOT be considered valid. 
+#It's "cheating" to fix an unknown parameter that, when freed, has an active bound at the MLE.
+
+set.seed(241217); fmf2.a <- mxTryHard(model=factorModel2,greenOK=T,checkHess=T)
+summary(fmf2.a)
+omxCheckEquals(fmf2.a$output$status$code,0)
+omxCheckTrue(fmf2.a$output$infoDefinite)
+
+set.seed(241217); fmf3.a <- mxTryHard(model=factorModel3,greenOK=T,checkHess=T)
+omxCheckEquals(fmf3.a$output$status$code,0)
+if(mxOption(NULL,"Default optimizer")=="SLSQP"){
+	omxCheckTrue(!length(fmf3.a$output$infoDefinite))
+}
+
+
