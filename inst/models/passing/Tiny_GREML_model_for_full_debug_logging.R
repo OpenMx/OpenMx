f6b477f3be0d3ad77ec551da75c7330db9aef8e5
RMKirkpatrick <rkirkpatrick2@vcu.edu>
2025-03-13 15:00:50 -0500

    Add test of underidentification with GREML expectation.

--- inst/models/passing/Tiny_GREML_model_for_full_debug_logging.R
+++ inst/models/passing/Tiny_GREML_model_for_full_debug_logging.R
@@ -45,7 +45,7 @@ testmod <- mxModel(
 
 xpec <- mxGetExpected(testmod,c("covariance","means"))
 omxCheckEquals(xpec$covariance,diag(2,4))
-omxCheckEquals(xpec$means,matrix(-0.5477233,4,1))
+omxCheckEquals(xpec$means,matrix(-0.5477233,4,1),1e-7)
 
 check <- mxCheckIdentification(testmod,T)
 omxCheckTrue(check$status)
@@ -82,3 +82,19 @@ summary(testrun2)
 omxCheckEquals(mxEval(nrow(I),testrun2,T), 5)
 omxCheckEquals(mxEval(ncol(I),testrun2,T), 5)
 
+
+testmod3 <- mxModel(
+	"GREMLtest",
+	mxData(observed = dat, type="raw", sort=FALSE),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=0.1,labels="foo",name="Foo"),
+	mxMatrix(type = "Full", nrow = 1, ncol=1, free=T, values = 2, labels = "ve", lbound = 0.0001, name = "Ve"),
+	mxMatrix(type="Unit", nrow=5,ncol=1,name="Uni"),
+	mxAlgebra(vec2diag(Uni), name="I"),
+	mxAlgebra( vec2diag(Uni%x%Ve), name="V"),
+	ge,
+	gff,
+	plan
+)
+check3 <- mxCheckIdentification(testmod3)
+omxCheckTrue(!check3$status)
+omxCheckEquals(check3$non_identified_parameters,"foo")
